86daa3fb872486dfd6d2fad179bcde47482536be
Daniel Sandler <dsandler@google.com>
2009-09-28 16:03:50 -0400

    Live wallpaper improvements.
    Now draws something a little nicer if the live wallpaper
    doesn't have a preview.
    
    Also: move Launcher2 back into "shared" so it has permission
    to set the wallpaper.

--- Android.mk
+++ Android.mk
@@ -38,7 +38,7 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_PACKAGE_NAME := Launcher2
 LOCAL_CERTIFICATE := platform
-LOCAL_CERTIFICATE := shared
+# LOCAL_CERTIFICATE := shared
 
 LOCAL_OVERRIDES_PACKAGES := Launcher
 
