9415d87eda0cf28b8df1eccde39b0ca1646be3b9
Adam Cohen <adamcohen@google.com>
2010-09-13 14:49:43 -0700

    -Added 3D effect to home screen scrolling -Added background outline fade in / out -Modified the feel of scrolling: now using quintic  interpolator and modified the influence of  scroll velocity
    Change-Id: Ifddcab5223ac20be7d9f800ccf09442d9b4db781

--- src/com/android/launcher2/LauncherAppWidgetHost.java
+++ src/com/android/launcher2/LauncherAppWidgetHost.java
@@ -30,7 +30,7 @@ public class LauncherAppWidgetHost extends AppWidgetHost {
     public LauncherAppWidgetHost(Context context, int hostId) {
         super(context, hostId);
     }
-    
+
     @Override
     protected AppWidgetHostView onCreateView(Context context, int appWidgetId,
             AppWidgetProviderInfo appWidget) {
