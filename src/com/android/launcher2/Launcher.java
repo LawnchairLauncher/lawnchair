6e3140865d3f0def2e55934d8b0b2c1503386e54
Winson Chung <winsonc@google.com>
2011-01-27 16:46:51 -0800

    Fixes for other places where we expect CellLayoutChildren instead of CellLayout. (3392097)
    Change-Id: I9df13c413ce62a2760ce6a8ebafed1e948088de2

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -3331,7 +3331,8 @@ public final class Launcher extends Activity
         int count = workspace.getChildCount();
         for (int i = 0; i < count; i++) {
             // Use removeAllViewsInLayout() to avoid an extra requestLayout() and invalidate().
-            ((ViewGroup) workspace.getChildAt(i)).removeAllViewsInLayout();
+            final ViewGroup layout = ((CellLayout) workspace.getChildAt(i)).getChildrenLayout();
+            layout.removeAllViewsInLayout();
         }
 
         if (DEBUG_USER_INTERFACE) {
