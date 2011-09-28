90576b5f095371e1ba4fedcb775f43715adf9634
Winson Chung <winsonc@google.com>
2011-09-27 17:45:27 -0700

    Fixing some small issues.
    - Adding All Apps pressed state (5375824)
    - Ensuring the drop targets are single line (5375563)
    - Adding some checks in kb focus logic to appease some frantic monkeys (4508638)
    - Removing some unused assets
    
    Change-Id: I46702ee25c638849c716f0d8428f68c2b06c1c2e

--- src/com/android/launcher2/Hotseat.java
+++ src/com/android/launcher2/Hotseat.java
@@ -100,7 +100,7 @@ public class Hotseat extends FrameLayout {
         BubbleTextView allAppsButton = (BubbleTextView)
                 inflater.inflate(R.layout.application, mContent, false);
         allAppsButton.setCompoundDrawablesWithIntrinsicBounds(null,
-                context.getResources().getDrawable(R.drawable.ic_allapps), null, null);
+                context.getResources().getDrawable(R.drawable.all_apps_button_icon), null, null);
         // allAppsButton.setText(context.getString(R.string.all_apps_button_label));
         allAppsButton.setContentDescription(context.getString(R.string.all_apps_button_label));
         allAppsButton.setOnClickListener(new View.OnClickListener() {
