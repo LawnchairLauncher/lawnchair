6cbb4cd561aacbe5c25bf3d5cf5396d9b7c553e1
nebkat <nebkat@gmail.com>
2011-12-24 09:56:48 +0000

    Update Copyright
    Change-Id: I5ffdfd4a09bd58f8ae498c932ce07d691cfb8ab8

--- src/com/cyanogenmod/trebuchet/preference/NumberPickerPreference.java
+++ src/com/cyanogenmod/trebuchet/preference/NumberPickerPreference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Cyanogenmod Project
+ * Copyright (C) 2011 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
