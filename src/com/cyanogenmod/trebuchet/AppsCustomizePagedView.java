634cf792741753dc5d8a91972bdac30b29cc404e
nebkat <nebkat@teamhacksung.org>
2011-12-30 18:58:41 +0000

    AppsCustomize: Apps Sort Mode Cling
    Change-Id: I07a08afb1f249f226693e935ad7ebc017f9c79b0

--- src/com/cyanogenmod/trebuchet/AppsCustomizePagedView.java
+++ src/com/cyanogenmod/trebuchet/AppsCustomizePagedView.java
@@ -276,6 +276,7 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
 
     // Cling
     private boolean mHasShownAllAppsCling;
+    private boolean mHasShownAllAppsSortCling;
     private int mClingFocusedX;
     private int mClingFocusedY;
 
@@ -571,6 +572,7 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
     }
 
     void showAllAppsCling() {
+        Cling allAppsCling = (Cling) getTabHost().findViewById(R.id.all_apps_cling);
         if (!mHasShownAllAppsCling && isDataReady()) {
             mHasShownAllAppsCling = true;
             // Calculate the position for the cling punch through
@@ -582,6 +584,10 @@ public class AppsCustomizePagedView extends PagedViewWithDraggableItems implemen
                     offset[0];
             pos[1] += offset[1];
             mLauncher.showFirstRunAllAppsCling(pos);
+        } else if (!mHasShownAllAppsSortCling && isDataReady() &&
+                allAppsCling != null && allAppsCling.isDismissed()) {
+            mHasShownAllAppsSortCling = true;
+            mLauncher.showFirstRunAllAppsSortCling();
         }
     }
 
--- src/com/cyanogenmod/trebuchet/Cling.java
+++ src/com/cyanogenmod/trebuchet/Cling.java
@@ -40,6 +40,7 @@ public class Cling extends FrameLayout {
 
     static final String WORKSPACE_CLING_DISMISSED_KEY = "cling.workspace.dismissed";
     static final String ALLAPPS_CLING_DISMISSED_KEY = "cling.allapps.dismissed";
+    static final String ALLAPPS_SORT_CLING_DISMISSED_KEY = "cling.allappssort.dismissed";
     static final String FOLDER_CLING_DISMISSED_KEY = "cling.folder.dismissed";
 
     private static String WORKSPACE_PORTRAIT = "workspace_portrait";
@@ -51,9 +52,12 @@ public class Cling extends FrameLayout {
     private static String ALLAPPS_LANDSCAPE = "all_apps_landscape";
     private static String ALLAPPS_LARGE = "all_apps_large";
 
+    private static String ALLAPPS_SORT_PORTRAIT = "all_apps_sort_portrait";
+    private static String ALLAPPS_SORT_LANDSCAPE = "all_apps_sort_landscape";
     private static String FOLDER_PORTRAIT = "folder_portrait";
     private static String FOLDER_LANDSCAPE = "folder_landscape";
     private static String FOLDER_LARGE = "folder_large";
+    private static String ALLAPPS_SORT_LARGE = "all_apps_sort_large";
 
     private Launcher mLauncher;
     private boolean mIsInitialized;
@@ -66,6 +70,7 @@ public class Cling extends FrameLayout {
     private int mButtonBarHeight;
     private float mRevealRadius;
     private int[] mPositionData;
+    private boolean mDismissed;
 
     private Paint mErasePaint;
 
@@ -89,6 +94,7 @@ public class Cling extends FrameLayout {
         if (!mIsInitialized) {
             mLauncher = l;
             mPositionData = positionData;
+            mDismissed = false;
 
             Resources r = getContext().getResources();
 
@@ -108,6 +114,14 @@ public class Cling extends FrameLayout {
         }
     }
 
+    void dismiss() {
+        mDismissed = true;
+    }
+
+    boolean isDismissed() {
+        return mDismissed;
+    }
+
     void cleanup() {
         mBackground = null;
         mPunchThroughGraphic = null;
@@ -124,6 +138,10 @@ public class Cling extends FrameLayout {
             return new int[]{getMeasuredWidth() / 2, getMeasuredHeight() - (mButtonBarHeight / 2)};
         } else if (mDrawIdentifier.equals(WORKSPACE_LANDSCAPE)) {
             return new int[]{getMeasuredWidth() - (mButtonBarHeight / 2), getMeasuredHeight() / 2};
+        } else if (mDrawIdentifier.equals(ALLAPPS_SORT_PORTRAIT) ||
+                   mDrawIdentifier.equals(ALLAPPS_SORT_LANDSCAPE) ||
+                   mDrawIdentifier.equals(ALLAPPS_SORT_LARGE)) {
+            return new int[]{mButtonBarHeight / 2, mButtonBarHeight / 2};
         } else if (mDrawIdentifier.equals(WORKSPACE_LARGE)) {
             final float scale = LauncherApplication.getScreenDensity();
             final int cornerXOffset = (int) (scale * 15);
@@ -173,7 +191,10 @@ public class Cling extends FrameLayout {
             mDrawIdentifier.equals(WORKSPACE_LARGE) ||
             mDrawIdentifier.equals(ALLAPPS_PORTRAIT) ||
             mDrawIdentifier.equals(ALLAPPS_LANDSCAPE) ||
-            mDrawIdentifier.equals(ALLAPPS_LARGE)) {
+            mDrawIdentifier.equals(ALLAPPS_LARGE) ||
+            mDrawIdentifier.equals(ALLAPPS_SORT_PORTRAIT) ||
+            mDrawIdentifier.equals(ALLAPPS_SORT_LANDSCAPE) ||
+            mDrawIdentifier.equals(ALLAPPS_SORT_LARGE)) {
 
             int[] positions = getPunchThroughPositions();
             for (int i = 0; i < positions.length; i += 2) {
