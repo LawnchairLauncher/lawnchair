968ead59833700ddb168e57a22047ff2ec76ded3
nebkat <nebkat@teamhacksung.org>
2012-02-24 16:03:42 +0000

    DeleteDropTarget: Long-Hold To Uninstall
    Change-Id: I2f60040c4cc645a54e9a8e696f330a8e70ff833c

--- src/com/cyanogenmod/trebuchet/DeleteDropTarget.java
+++ src/com/cyanogenmod/trebuchet/DeleteDropTarget.java
@@ -20,12 +20,14 @@ import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.PointF;
 import android.graphics.Rect;
-import android.graphics.drawable.TransitionDrawable;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -45,10 +47,18 @@ public class DeleteDropTarget extends ButtonDropTarget {
 
     private final int mFlingDeleteMode = MODE_FLING_DELETE_ALONG_VECTOR;
 
+    private static final int MODE_DELETE = 0;
+    private static final int MODE_UNINSTALL = 1;
+    private int mMode = MODE_DELETE;
+
     private ColorStateList mOriginalTextColor;
-    private TransitionDrawable mUninstallDrawable;
-    private TransitionDrawable mRemoveDrawable;
-    private TransitionDrawable mCurrentDrawable;
+    private Drawable mUninstallActiveDrawable;
+    private Drawable mRemoveActiveDrawable;
+    private Drawable mRemoveNormalDrawable;
+    private Drawable mCurrentDrawable;
+    private boolean mUninstall;
+
+    private final Handler mHandler = new Handler();
 
     public DeleteDropTarget(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
@@ -57,6 +67,12 @@ public class DeleteDropTarget extends ButtonDropTarget {
     public DeleteDropTarget(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
+
+    private final Runnable mShowUninstaller = new Runnable() {
+        public void run() {
+            switchToUninstallTarget();
+        }
+    };
 
     @Override
     protected void onFinishInflate() {
@@ -68,16 +84,9 @@ public class DeleteDropTarget extends ButtonDropTarget {
         // Get the hover color
         Resources r = getResources();
         mHoverColor = r.getColor(R.color.delete_target_hover_tint);
-        mUninstallDrawable = (TransitionDrawable) 
-                r.getDrawable(R.drawable.uninstall_target_selector);
-        mRemoveDrawable = (TransitionDrawable) r.getDrawable(R.drawable.remove_target_selector);
-
-        mRemoveDrawable.setCrossFadeEnabled(true);
-        mUninstallDrawable.setCrossFadeEnabled(true);
-
-        // The current drawable is set to either the remove drawable or the uninstall drawable 
-        // and is initially set to the remove drawable, as set in the layout xml.
-        mCurrentDrawable = (TransitionDrawable) getCurrentDrawable();
+        mUninstallActiveDrawable = r.getDrawable(R.drawable.ic_launcher_trashcan_active_holo);
+        mRemoveActiveDrawable = r.getDrawable(R.drawable.ic_launcher_clear_active_holo);
+        mRemoveNormalDrawable = r.getDrawable(R.drawable.ic_launcher_clear_normal_holo);
 
         // Remove the text in the Phone UI in landscape
         int orientation = getResources().getConfiguration().orientation;
@@ -104,25 +113,23 @@ public class DeleteDropTarget extends ButtonDropTarget {
         }
         return false;
     }
-    private boolean isDragSourceWorkspaceOrFolder(DragObject d) {
-        return (d.dragSource instanceof Workspace) || (d.dragSource instanceof Folder);
+    private boolean isDragSourceWorkspaceOrFolder(DragSource source) {
+        return (source instanceof Workspace) || (source instanceof Folder);
     }
-    private boolean isWorkspaceOrFolderApplication(DragObject d) {
-        return isDragSourceWorkspaceOrFolder(d) && (d.dragInfo instanceof ShortcutInfo);
+    private boolean isWorkspaceOrFolderApplication(DragSource source, Object info) {
+        return isDragSourceWorkspaceOrFolder(source) && (info instanceof ShortcutInfo);
     }
-    private boolean isWorkspaceOrFolderWidget(DragObject d) {
-        return isDragSourceWorkspaceOrFolder(d) && (d.dragInfo instanceof LauncherAppWidgetInfo);
+    private boolean isWorkspaceWidget(DragSource source, Object info) {
+        return isDragSourceWorkspaceOrFolder(source) && (info instanceof LauncherAppWidgetInfo);
     }
-    private boolean isWorkspaceFolder(DragObject d) {
-        return (d.dragSource instanceof Workspace) && (d.dragInfo instanceof FolderInfo);
+    private boolean isWorkspaceFolder(DragSource source, Object info) {
+        return (source instanceof Workspace) && (info instanceof FolderInfo);
     }
 
     private void setHoverColor() {
-        mCurrentDrawable.startTransition(mTransitionDuration);
         setTextColor(mHoverColor);
     }
     private void resetHoverColor() {
-        mCurrentDrawable.resetTransition();
         setTextColor(mOriginalTextColor);
     }
 
@@ -134,49 +141,72 @@ public class DeleteDropTarget extends ButtonDropTarget {
 
     @Override
     public void onDragStart(DragSource source, Object info, int dragAction) {
-        boolean isVisible = true;
         boolean isUninstall = false;
 
-        // If we are dragging a widget from AppsCustomize, hide the delete target
-        if (isAllAppsWidget(source, info)) {
-            isVisible = false;
-        }
-
-        // If we are dragging an application from AppsCustomize, only show the control if we can
-        // delete the app (it was downloaded), and rename the string to "uninstall" in such a case
+        // If we are dragging an application from AppsCustomize, only show the uninstall control if we
+        // can delete the app (it was downloaded)
         if (isAllAppsApplication(source, info)) {
             ApplicationInfo appInfo = (ApplicationInfo) info;
             if ((appInfo.flags & ApplicationInfo.DOWNLOADED_FLAG) != 0) {
                 isUninstall = true;
-            } else {
-                isVisible = false;
+            }
+        } else if (isWorkspaceOrFolderApplication(source, info)) {
+            ShortcutInfo shortcutInfo = (ShortcutInfo) info;
+            PackageManager pm = getContext().getPackageManager();
+            ResolveInfo resolveInfo = pm.resolveActivity(shortcutInfo.intent, 0);
+            if (resolveInfo != null && (resolveInfo.activityInfo.applicationInfo.flags &
+                    android.content.pm.ApplicationInfo.FLAG_SYSTEM) == 0) {
+                isUninstall = true;
             }
         }
 
-        if (isUninstall) {
-            setCompoundDrawablesWithIntrinsicBounds(mUninstallDrawable, null, null, null);
-        } else {
-            setCompoundDrawablesWithIntrinsicBounds(mRemoveDrawable, null, null, null);
-        }
-        mCurrentDrawable = (TransitionDrawable) getCurrentDrawable();
+        setCompoundDrawablesWithIntrinsicBounds(mRemoveNormalDrawable, null, null, null);
+        mCurrentDrawable = getCompoundDrawables()[0];
 
-        mActive = isVisible;
+        mUninstall = isUninstall;
+        mActive = true;
+        mMode = MODE_DELETE;
+
+        setTextColor(mOriginalTextColor);
         resetHoverColor();
-        ((ViewGroup) getParent()).setVisibility(isVisible ? View.VISIBLE : View.GONE);
+        ((ViewGroup) getParent()).setVisibility(View.VISIBLE);
         if (getText().length() > 0) {
-            setText(isUninstall ? R.string.delete_target_uninstall_label
-                : R.string.delete_target_label);
+            setText(R.string.delete_target_label);
         }
+    }
+
+    private void switchToUninstallTarget() {
+        if (!mUninstall) {
+            return;
+        }
+
+        mMode = MODE_UNINSTALL;
+
+        if (getText().length() > 0) {
+            setText(R.string.delete_target_uninstall_label);
+        }
+
+        setCompoundDrawablesWithIntrinsicBounds(mUninstallActiveDrawable, null, null, null);
+        mCurrentDrawable = getCompoundDrawables()[0];
     }
 
     @Override
     public void onDragEnd() {
         super.onDragEnd();
+
         mActive = false;
     }
 
     public void onDragEnter(DragObject d) {
         super.onDragEnter(d);
+
+        if (mUninstall) {
+            mHandler.removeCallbacks(mShowUninstaller);
+            mHandler.postDelayed(mShowUninstaller, 1000);
+        }
+
+        setCompoundDrawablesWithIntrinsicBounds(mRemoveActiveDrawable, null, null, null);
+        mCurrentDrawable = getCompoundDrawables()[0];
 
         setHoverColor();
     }
@@ -184,7 +214,17 @@ public class DeleteDropTarget extends ButtonDropTarget {
     public void onDragExit(DragObject d) {
         super.onDragExit(d);
 
+        mHandler.removeCallbacks(mShowUninstaller);
+
         if (!d.dragComplete) {
+            mMode = MODE_DELETE;
+
+            if (getText().length() > 0) {
+                setText(R.string.delete_target_label);
+            }
+
+            setCompoundDrawablesWithIntrinsicBounds(mRemoveNormalDrawable, null, null, null);
+            mCurrentDrawable = getCompoundDrawables()[0];
             resetHoverColor();
         } else {
             // Restore the hover color if we are deleting
@@ -218,32 +258,42 @@ public class DeleteDropTarget extends ButtonDropTarget {
     private void completeDrop(DragObject d) {
         ItemInfo item = (ItemInfo) d.dragInfo;
 
-        if (isAllAppsApplication(d.dragSource, item)) {
-            // Uninstall the application if it is being dragged from AppsCustomize
-            mLauncher.startApplicationUninstallActivity((ApplicationInfo) item);
-        } else if (isWorkspaceOrFolderApplication(d)) {
-            LauncherModel.deleteItemFromDatabase(mLauncher, item);
-        } else if (isWorkspaceFolder(d)) {
-            // Remove the folder from the workspace and delete the contents from launcher model
-            FolderInfo folderInfo = (FolderInfo) item;
-            mLauncher.removeFolder(folderInfo);
-            LauncherModel.deleteFolderContentsFromDatabase(mLauncher, folderInfo);
-        } else if (isWorkspaceOrFolderWidget(d)) {
-            // Remove the widget from the workspace
-            mLauncher.removeAppWidget((LauncherAppWidgetInfo) item);
-            LauncherModel.deleteItemFromDatabase(mLauncher, item);
+        switch (mMode) {
+            case MODE_DELETE:
+                if (isWorkspaceOrFolderApplication(d.dragSource, item)) {
+                    LauncherModel.deleteItemFromDatabase(mLauncher, item);
+                } else if (isWorkspaceFolder(d.dragSource, d.dragInfo)) {
+                    // Remove the folder from the workspace and delete the contents from launcher model
+                    FolderInfo folderInfo = (FolderInfo) item;
+                    mLauncher.removeFolder(folderInfo);
+                    LauncherModel.deleteFolderContentsFromDatabase(mLauncher, folderInfo);
+                } else if (isWorkspaceWidget(d.dragSource, item)) {
+                    // Remove the widget from the workspace
+                    mLauncher.removeAppWidget((LauncherAppWidgetInfo) item);
+                    LauncherModel.deleteItemFromDatabase(mLauncher, item);
 
-            final LauncherAppWidgetInfo launcherAppWidgetInfo = (LauncherAppWidgetInfo) item;
-            final LauncherAppWidgetHost appWidgetHost = mLauncher.getAppWidgetHost();
-            if (appWidgetHost != null) {
-                // Deleting an app widget ID is a void call but writes to disk before returning
-                // to the caller...
-                new Thread("deleteAppWidgetId") {
-                    public void run() {
-                        appWidgetHost.deleteAppWidgetId(launcherAppWidgetInfo.appWidgetId);
+                    final LauncherAppWidgetInfo launcherAppWidgetInfo = (LauncherAppWidgetInfo) item;
+                    final LauncherAppWidgetHost appWidgetHost = mLauncher.getAppWidgetHost();
+                    if (appWidgetHost != null) {
+                        // Deleting an app widget ID is a void call but writes to disk before returning
+                        // to the caller...
+                        new Thread("deleteAppWidgetId") {
+                            public void run() {
+                                appWidgetHost.deleteAppWidgetId(launcherAppWidgetInfo.appWidgetId);
+                            }
+                        }.start();
                     }
-                }.start();
-            }
+                }
+                break;
+            case MODE_UNINSTALL:
+                if (isAllAppsApplication(d.dragSource, item)) {
+                    // Uninstall the application
+                    mLauncher.startApplicationUninstallActivity((ApplicationInfo) item);
+                } else if (isWorkspaceOrFolderApplication(d.dragSource, item)) {
+                    // Uninstall the shortcut
+                    mLauncher.startShortcutUninstallActivity((ShortcutInfo) item);
+                }
+                break;
         }
     }
 
