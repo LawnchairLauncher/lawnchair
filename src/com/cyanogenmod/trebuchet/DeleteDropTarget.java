6075c92995548d50f54e72d5c5f3c03ba3c31a08
nebkat <nebkat@teamhacksung.org>
2012-03-17 19:37:38 +0000

    DeleteDropTarget: "Cancel" when dragging from AllApps
    Change-Id: I4fb03b0cbffe9662bcf6c582fe5bfb5bbbcac20e

--- src/com/cyanogenmod/trebuchet/DeleteDropTarget.java
+++ src/com/cyanogenmod/trebuchet/DeleteDropTarget.java
@@ -97,6 +97,9 @@ public class DeleteDropTarget extends ButtonDropTarget {
         }
     }
 
+    private boolean isAllAppsItem(DragSource source, Object info) {
+        return isAllAppsApplication(source, info) || isAllAppsWidget(source, info);
+    }
     private boolean isAllAppsApplication(DragSource source, Object info) {
         return (source instanceof AppsCustomizePagedView) && (info instanceof ApplicationInfo);
     }
@@ -171,7 +174,11 @@ public class DeleteDropTarget extends ButtonDropTarget {
         resetHoverColor();
         ((ViewGroup) getParent()).setVisibility(View.VISIBLE);
         if (getText().length() > 0) {
-            setText(R.string.delete_target_label);
+            if (isAllAppsItem(source, info)) {
+                setText(R.string.cancel_target_label);
+            } else {
+                setText(R.string.delete_target_label);
+            }
         }
     }
 
@@ -220,7 +227,11 @@ public class DeleteDropTarget extends ButtonDropTarget {
             mMode = MODE_DELETE;
 
             if (getText().length() > 0) {
-                setText(R.string.delete_target_label);
+                if (isAllAppsItem(d.dragSource, d.dragInfo)) {
+                    setText(R.string.cancel_target_label);
+                } else {
+                    setText(R.string.delete_target_label);
+                }
             }
 
             setCompoundDrawablesWithIntrinsicBounds(mRemoveNormalDrawable, null, null, null);
