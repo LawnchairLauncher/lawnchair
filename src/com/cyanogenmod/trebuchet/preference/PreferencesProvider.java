bde2a5aff1430c5623c332c5b09ce153a03c34a4
nebkat <nebkat@teamhacksung.org>
2012-03-25 12:34:24 +0100

    Preferences: Grid Size
    Change-Id: I51bec0a0c29421fc389f7b656496ba1467758892

--- src/com/cyanogenmod/trebuchet/preference/PreferencesProvider.java
+++ src/com/cyanogenmod/trebuchet/preference/PreferencesProvider.java
@@ -25,6 +25,24 @@ public final class PreferencesProvider {
     public static final String PREFERENCES_CHANGED = "preferences_changed";
     public static class Interface {
         public static class Homescreen {
+            public static int getCellCountX(Context context, int def) {
+                final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                String[] values = preferences.getString("ui_homescreen_grid", "0|" + def).split("\\|");
+                try {
+                    return Integer.parseInt(values[1]);
+                } catch (NumberFormatException e) {
+                    return def;
+                }
+            }
+            public static int getCellCountY(Context context, int def) {
+                final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
+                String[] values = preferences.getString("ui_homescreen_grid", def + "|0").split("\\|");;
+                try {
+                    return Integer.parseInt(values[0]);
+                } catch (NumberFormatException e) {
+                    return def;
+                }
+            }
             public static boolean getShowSearchBar(Context context) {
                 final SharedPreferences preferences = context.getSharedPreferences(PREFERENCES_KEY, 0);
                 return preferences.getBoolean("ui_homescreen_general_search", true);
