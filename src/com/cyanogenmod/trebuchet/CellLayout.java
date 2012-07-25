ec05469652d9f257f79e71720511cf0b60f4c829
Adam Cohen <adamcohen@google.com>
2012-07-24 22:34:36 -0700

    Ensuring that restoreInstanceState is being called promptly for synchronously bound page
    Change-Id: I0e71c29f553ad360ec42a6a0b2529d16cbddd437

--- src/com/cyanogenmod/trebuchet/CellLayout.java
+++ src/com/cyanogenmod/trebuchet/CellLayout.java
@@ -19,6 +19,7 @@ package com.cyanogenmod.trebuchet;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.animation.ValueAnimator.AnimatorUpdateListener;
