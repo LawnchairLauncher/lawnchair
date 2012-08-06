CyanogenMod/android_packages_apps_Trebuchet

Preferences: Resize any widget
Change-Id: I7ef5546f57543664d45a25eb5b9d07f303c9db0b

src/com/android/launcher2/AppWidgetResizeFrame.java
@@ -13,6 +13,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.android.launcher.R;
+import com.android.launcher2.preference.PreferencesProvider;
 
 public class AppWidgetResizeFrame extends FrameLayout {
 
@@ -116,6 +117,11 @@ public class AppWidgetResizeFrame extends FrameLayout {
         mWidgetPaddingTop = p.top;
         mWidgetPaddingRight = p.right;
         mWidgetPaddingBottom = p.bottom;
+
+        // Resize any widget
+        if (PreferencesProvider.Interface.Homescreen.getResizeAnyWidget(context)) {
+            mResizeMode = AppWidgetProviderInfo.RESIZE_BOTH;
+        }
 
         if (mResizeMode == AppWidgetProviderInfo.RESIZE_HORIZONTAL) {
             mTopHandle.setVisibility(GONE);

LawnchairLauncher/lawnchair

src/com/cyanogenmod/trebuchet/AppWidgetResizeFrame.java
@@ -14,6 +14,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.cyanogenmod.trebuchet.R;
+import com.cyanogenmod.trebuchet.preference.PreferencesProvider;
 
 public class AppWidgetResizeFrame extends FrameLayout {
     private LauncherAppWidgetHostView mWidgetView;
@@ -117,6 +118,11 @@ public class AppWidgetResizeFrame extends FrameLayout {
         mWidgetPaddingTop = p.top;
         mWidgetPaddingRight = p.right;
         mWidgetPaddingBottom = p.bottom;
+
+        // Resize any widget
+        if (PreferencesProvider.Interface.Homescreen.getResizeAnyWidget(context)) {
+            mResizeMode = AppWidgetProviderInfo.RESIZE_BOTH;
+        }
 
         if (mResizeMode == AppWidgetProviderInfo.RESIZE_HORIZONTAL) {
             mTopHandle.setVisibility(GONE);