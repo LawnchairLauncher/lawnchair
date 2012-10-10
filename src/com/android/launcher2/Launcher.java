b86d516bccffc56af9716a39f3d217e928d02d04
Michael Jurka <mikejurka@google.com>
2012-10-10 22:20:47 +0200

    Update custom cling params
    Bug: 7318080
    
    Change-Id: I77d73d81a60efa3886f088e82c6ba3466f6ac331

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -55,7 +55,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
