3c1681d5f7d56af5b25126d1c2b2ba7aa799deca
Michael Jurka <mikejurka@google.com>
2012-11-15 14:11:41 -0800

    Cleaning up unused code

--- src/com/android/launcher2/AppsCustomizePagedView.java
+++ src/com/android/launcher2/AppsCustomizePagedView.java
@@ -51,7 +51,6 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateInterpolator;
