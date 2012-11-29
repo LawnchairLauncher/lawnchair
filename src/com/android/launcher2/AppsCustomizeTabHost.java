7819abd2d191a170cbc9d4bd190001c465187a9d
Winson Chung <winsonc@google.com>
2012-11-29 14:29:38 -0800

    Fixing issue where the cling is offset. (Bug 7633074)
    - Also fixing issue where the workspace cling was showing on top of AllApps when you entered it for the first time.
    
    Change-Id: I7d59ad153de192bc1d69537af1b4af632465138b

--- src/com/android/launcher2/AppsCustomizeTabHost.java
+++ src/com/android/launcher2/AppsCustomizeTabHost.java
@@ -418,11 +418,9 @@ public class AppsCustomizeTabHost extends TabHost implements LauncherTransitiona
         }
 
         if (!toWorkspace) {
-            // Going from Workspace -> All Apps
-            setVisibilityOfSiblingsWithLowerZOrder(INVISIBLE);
-
-            // Dismiss the workspace cling and show the all apps cling (if not already shown)
+            // Dismiss the workspace cling
             l.dismissWorkspaceCling(null);
+            // Show the all apps cling (if not already shown)
             mAppsCustomizePane.showAllAppsCling();
             // Make sure adjacent pages are loaded (we wait until after the transition to
             // prevent slowing down the animation)
@@ -431,6 +429,11 @@ public class AppsCustomizeTabHost extends TabHost implements LauncherTransitiona
             if (!LauncherApplication.isScreenLarge()) {
                 mAppsCustomizePane.hideScrollingIndicator(false);
             }
+
+            // Going from Workspace -> All Apps
+            // NOTE: We should do this at the end since we check visibility state in some of the
+            // cling initialization/dismiss code above.
+            setVisibilityOfSiblingsWithLowerZOrder(INVISIBLE);
         }
     }
 
