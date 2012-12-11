04f9c23295e5b23e3ee813839e236e81f4556fd2
nebkat <nebkat@teamhacksung.org>
2012-12-11 21:30:54 +0000

    Workspace: Quad interpolator
    Change-Id: I995a311c60ed775e818c4b17393964111214ff7d

--- src/com/cyanogenmod/trebuchet/PagedView.java
+++ src/com/cyanogenmod/trebuchet/PagedView.java
@@ -58,9 +58,9 @@ public abstract class PagedView extends ViewGroup implements ViewGroup.OnHierarc
     // the min drag distance for a fling to register, to prevent random page shifts
     private static final int MIN_LENGTH_FOR_FLING = 25;
 
-    protected static final int PAGE_SNAP_ANIMATION_DURATION = 550;
-    protected static final int MAX_PAGE_SNAP_DURATION = 750;
-    protected static final int SLOW_PAGE_SNAP_ANIMATION_DURATION = 950;
+    protected static final int PAGE_SNAP_ANIMATION_DURATION = 400;
+    protected static final int MAX_PAGE_SNAP_DURATION = 550;
+    protected static final int SLOW_PAGE_SNAP_ANIMATION_DURATION = 750;
     protected static final float NANOTIME_DIV = 1000000000.0f;
 
     private static final float OVERSCROLL_ACCELERATE_FACTOR = 2;
@@ -234,7 +234,7 @@ public abstract class PagedView extends ViewGroup implements ViewGroup.OnHierarc
     protected void init() {
         mDirtyPageContent = new ArrayList<Boolean>();
         mDirtyPageContent.ensureCapacity(32);
-        mScroller = new Scroller(getContext(), new ScrollInterpolator());
+        mScroller = new Scroller(getContext(), getScrollInterpolator());
         mCurrentPage = 0;
         mCenterPagesVertically = true;
 
@@ -1441,14 +1441,28 @@ public abstract class PagedView extends ViewGroup implements ViewGroup.OnHierarc
         snapToPage(getPageNearestToCenterOfScreen(), PAGE_SNAP_ANIMATION_DURATION);
     }
 
-    private static class ScrollInterpolator implements Interpolator {
-        public ScrollInterpolator() {
+    public static class QuintInterpolator implements Interpolator {
+        public QuintInterpolator() {
         }
 
         public float getInterpolation(float t) {
             t -= 1.0f;
             return t*t*t*t*t + 1;
         }
+    }
+
+    public static class QuadInterpolator implements Interpolator {
+        public QuadInterpolator() {
+        }
+
+        public float getInterpolation(float t) {
+            t -= 1.0f;
+            return -(t*t*t*t - 1);
+        }
+    }
+
+    protected Interpolator getScrollInterpolator() {
+        return new QuintInterpolator();
     }
 
     // We want the duration of the page snap animation to be influenced by the distance that
--- src/com/cyanogenmod/trebuchet/Workspace.java
+++ src/com/cyanogenmod/trebuchet/Workspace.java
@@ -56,6 +56,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
+import android.view.animation.Interpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -1114,6 +1115,11 @@ public class Workspace extends SmoothPagedView
         } else {
             mWallpaperScrollRatio = 1f;
         }
+    }
+
+    @Override
+    protected Interpolator getScrollInterpolator() {
+        return new PagedView.QuadInterpolator();
     }
 
     class WallpaperOffsetInterpolator {
