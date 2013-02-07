f0d45bdac841709ea9021a19cea2a6cf0eddd9ae
Michael Jurka <mikejurka@google.com>
2013-02-07 14:37:41 +0100

    Fix crash when creating preview cache table

--- src/com/android/launcher2/WidgetPreviewLoader.java
+++ src/com/android/launcher2/WidgetPreviewLoader.java
@@ -274,7 +274,7 @@ public class WidgetPreviewLoader {
                     COLUMN_SIZE + " TEXT NOT NULL, " +
                     COLUMN_PREVIEW_BITMAP + " BLOB NOT NULL, " +
                     "PRIMARY KEY (" + COLUMN_NAME + ", " + COLUMN_SIZE + ") " +
-                    ");");
+                    ") IF NOT EXISTS;");
         }
 
         @Override
