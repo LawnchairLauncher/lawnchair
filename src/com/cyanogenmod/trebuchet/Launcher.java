0827ce7a634435f515aee66cd128c9a1684d17e2
Jorge Ruesga <jorge@ruesga.com>
2013-06-20 23:51:35 +0200

    Trebuchet: Revert animation end
    Since moveToDefaultScreen is protected by mIsSwitchingState, no animation is triggered when
    the home screen is double tapped. Now, cancel the animation is causing icons to disappear.
    
    Change-Id: I3838a02d7afc4a40343c593ea743d1773bbc3cfd
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/cyanogenmod/trebuchet/Launcher.java
+++ src/com/cyanogenmod/trebuchet/Launcher.java
@@ -1567,7 +1567,6 @@ public final class Launcher extends Activity
                     if (alreadyOnHome && mState == State.WORKSPACE && !mWorkspace.isTouchActive() &&
                             openFolder == null) {
                         if (mStateAnimation != null) {
-                            mStateAnimation.end();
                             mStateAnimation = null;
                         }
                         mWorkspace.moveToDefaultScreen(true);
