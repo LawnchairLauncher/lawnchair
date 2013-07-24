83d5e013d2e6cf2d715bdeeb407975ee4abdbb68
Kaiyi Zhang <kaiyiz@codeaurora.org>
2013-07-24 13:09:59 +0800

    Launcher: Return item to folder when failing to drag it out
    After animation, when add icons to screen, use the old icon list which
    is created during animation.
    
    When add icons after animation, refresh the icon list.
    
    Change-Id: I2553ed41b80fac4cca21591a0e52c7e1b6af8f6e
    CRs-Fixed: 494966

--- src/com/cyanogenmod/trebuchet/Folder.java
+++ src/com/cyanogenmod/trebuchet/Folder.java
@@ -994,6 +994,7 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
         mFolderIcon.requestFocus();
 
         if (mRearrangeOnClose) {
+            mItemsInvalidated = true;
             setupContentForNumItems(getItemCount());
             mRearrangeOnClose = false;
         }
