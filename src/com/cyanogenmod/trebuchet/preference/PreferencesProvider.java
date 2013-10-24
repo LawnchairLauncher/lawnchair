a39739b8104131f098c0fc1680b063fd7d12bad7
Danesh M <daneshm90@gmail.com>
2013-10-24 20:16:50 +0000

    Revert "Trebuchet : Fix dynamic padding"
    This reverts commit efa4b8d18459243abf379bb0135ee0a89165a50a.
    
    Change-Id: Ibc3857b7cb94cce395cbaa2671b34cced3a538b9

--- src/com/cyanogenmod/trebuchet/preference/PreferencesProvider.java
+++ src/com/cyanogenmod/trebuchet/preference/PreferencesProvider.java
@@ -93,6 +93,9 @@ public final class PreferencesProvider {
                     return def;
                 }
             }
+            public static boolean getStretchScreens() {
+                return getBoolean("ui_homescreen_stretch_screens", true);
+            }
             public static boolean getShowSearchBar() {
                 return getBoolean("ui_homescreen_general_search", true);
             }
