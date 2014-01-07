7cc9766d25a465093800ddc09e66f2f695d7c452
Dan Sandler <dsandler@android.com>
2014-01-07 14:12:27 -0500

    KEYCODE_MENU: hide all apps when entering overview mode
    Bug: 12165618
    Change-Id: I7ee652012c7f7b8e6fab3d9d1f99cf22484f3182

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -2149,13 +2149,7 @@ public class Launcher extends Activity
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         super.onPrepareOptionsMenu(menu);
-        if (!isAllAppsVisible()) {
-            if (!mWorkspace.isInOverviewMode()) {
-                mWorkspace.enterOverviewMode();
-            } else {
-                mWorkspace.exitOverviewMode(true);
-            }
-        }
+        showOverviewMode(true);
         return false;
     }
 
