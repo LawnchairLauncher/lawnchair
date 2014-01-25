LineageOS/android_packages_apps_Trebuchet

Settings: Initial
Change-Id: If8199313561dfba03a1aad0b49bb4036dcf586c9

src/org/cyanogenmod/trebuchet/Launcher.java
@@ -97,6 +97,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.cyanogenmod.trebuchet.DropTarget.DragObject;
+import org.cyanogenmod.trebuchet.settings.SettingsActivity;
+import org.cyanogenmod.trebuchet.settings.SettingsProvider;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -404,6 +406,9 @@ public class Launcher extends Activity
         mIconCache.flushInvalidIcons(grid);
         mDragController = new DragController(this);
         mInflater = getLayoutInflater();
+
+        // Load all settings
+        SettingsProvider.load(this);
 
         mStats = new Stats(this);
 
@@ -832,6 +837,10 @@ public class Launcher extends Activity
         }
         super.onResume();
 
+        if (settingsChanged()) {
+            finish();
+        }
+
         // Restore the previous launcher state
         if (mOnResumeState == State.WORKSPACE) {
             showWorkspace(false);
@@ -977,6 +986,13 @@ public class Launcher extends Activity
     }
 
     protected void startSettings() {
+        Intent settings = new Intent().setClass(this, SettingsActivity.class);
+        settings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        startActivity(settings);
+        if (mWorkspace.isInOverviewMode()) {
+            mWorkspace.exitOverviewMode(false);
+        }
     }
 
     public interface QSBScroller {
@@ -4377,6 +4393,22 @@ public class Launcher extends Activity
         Cling cling = (Cling) findViewById(R.id.folder_cling);
         dismissCling(cling, null, Cling.FOLDER_CLING_DISMISSED_KEY,
                 DISMISS_CLING_DURATION, true);
+    }
+
+    /**
+     * To avoid managing preference change listeners for various parts of the
+     * launcher we simply kill the process and let it reload from scratch.
+     */
+    public boolean settingsChanged() {
+        SharedPreferences prefs =
+                getSharedPreferences(SettingsProvider.SETTINGS_KEY, Context.MODE_PRIVATE);
+        boolean settingsChanged = prefs.getBoolean(SettingsProvider.SETTINGS_CHANGED, false);
+        if (settingsChanged) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(SettingsProvider.SETTINGS_CHANGED, false);
+            editor.commit();
+        }
+        return settingsChanged;
     }
 
     /**

src/org/cyanogenmod/trebuchet/settings/SettingsActivity.java
@@ -1,0 +1,225 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.trebuchet.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.*;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+import org.cyanogenmod.trebuchet.R;
+
+import java.util.List;
+
+public class SettingsActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String TAG = "Trebuchet.SettingsActivity";
+
+    private SharedPreferences mSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSettings = getSharedPreferences(SettingsProvider.SETTINGS_KEY,
+                Context.MODE_PRIVATE);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSettings.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSettings.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                finish();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preferences_headers, target);
+        updateHeaders(target);
+    }
+
+    private void updateHeaders(List<Header> headers) {
+        int i = 0;
+        while (i < headers.size()) {
+            Header header = headers.get(i);
+
+            // Version preference
+            if (header.id == R.id.preferences_application_version) {
+                header.title = getString(R.string.application_name) + " " + getString(R.string.application_version);
+            }
+
+            // Increment if not removed
+            if (headers.get(i) == header) {
+                i++;
+            }
+        }
+    }
+
+    @Override
+    public void setListAdapter(ListAdapter adapter) {
+        if (adapter == null) {
+            super.setListAdapter(null);
+        } else {
+            super.setListAdapter(new HeaderAdapter(this, getHeaders()));
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putBoolean(SettingsProvider.SETTINGS_CHANGED, true);
+        editor.commit();
+    }
+
+    private static class HeaderAdapter extends ArrayAdapter<Header> {
+        private static final int HEADER_TYPE_NORMAL = 0;
+        private static final int HEADER_TYPE_CATEGORY = 1;
+
+        private static final int HEADER_TYPE_COUNT = HEADER_TYPE_CATEGORY + 1;
+
+        private static class HeaderViewHolder {
+            ImageView icon;
+            TextView title;
+            TextView summary;
+        }
+
+        private LayoutInflater mInflater;
+
+        static int getHeaderType(Header header) {
+            if (header.id == R.id.preferences_application_section) {
+                return HEADER_TYPE_CATEGORY;
+            } else {
+                return HEADER_TYPE_NORMAL;
+            }
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            Header header = getItem(position);
+            return getHeaderType(header);
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false; // because of categories
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return getItemViewType(position) != HEADER_TYPE_CATEGORY;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return HEADER_TYPE_COUNT;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        public HeaderAdapter(Context context, List<Header> objects) {
+            super(context, 0, objects);
+
+            mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            HeaderViewHolder holder;
+            Header header = getItem(position);
+            int headerType = getHeaderType(header);
+            View view = null;
+
+            if (convertView == null) {
+                holder = new HeaderViewHolder();
+                switch (headerType) {
+                    case HEADER_TYPE_CATEGORY:
+                        view = new TextView(getContext(), null,
+                                android.R.attr.listSeparatorTextViewStyle);
+                        holder.title = (TextView) view;
+                        break;
+
+                    case HEADER_TYPE_NORMAL:
+                        view = mInflater.inflate(
+                                R.layout.preference_header_item, parent,
+                                false);
+                        holder.icon = (ImageView) view.findViewById(android.R.id.icon);
+                        holder.title = (TextView)
+                                view.findViewById(android.R.id.title);
+                        holder.summary = (TextView)
+                                view.findViewById(android.R.id.summary);
+                        break;
+                }
+                view.setTag(holder);
+            } else {
+                view = convertView;
+                holder = (HeaderViewHolder) view.getTag();
+            }
+
+            // All view fields must be updated every time, because the view may be recycled
+            switch (headerType) {
+                case HEADER_TYPE_CATEGORY:
+                    holder.title.setText(header.getTitle(getContext().getResources()));
+                    break;
+
+                case HEADER_TYPE_NORMAL:
+                    holder.icon.setImageResource(header.iconRes);
+                    holder.title.setText(header.getTitle(getContext().getResources()));
+                    CharSequence summary = header.getSummary(getContext().getResources());
+                    if (!TextUtils.isEmpty(summary)) {
+                        holder.summary.setVisibility(View.VISIBLE);
+                        holder.summary.setText(summary);
+                    } else {
+                        holder.summary.setVisibility(View.GONE);
+                    }
+                    break;
+            }
+
+            return view;
+        }
+    }
+}

src/org/cyanogenmod/trebuchet/settings/SettingsProvider.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.trebuchet.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Map;
+
+public final class SettingsProvider {
+    public static final String SETTINGS_KEY = "org.cyanogenmod.trebuchet_preferences";
+
+    public static final String SETTINGS_CHANGED = "settings_changed";
+
+    private static Map<String, ?> sKeyValues;
+
+    public static void load(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(SETTINGS_KEY, 0);
+        sKeyValues = preferences.getAll();
+    }
+
+    private static int getInt(String key, int def) {
+        return sKeyValues.containsKey(key) && sKeyValues.get(key) instanceof Integer ?
+                (Integer) sKeyValues.get(key) : def;
+    }
+
+    private static boolean getBoolean(String key, boolean def) {
+        return sKeyValues.containsKey(key) && sKeyValues.get(key) instanceof Boolean ?
+                (Boolean) sKeyValues.get(key) : def;
+    }
+
+    private static String getString(String key, String def) {
+        return sKeyValues.containsKey(key) && sKeyValues.get(key) instanceof String ?
+                (String) sKeyValues.get(key) : def;
+    }
+}

LawnchairLauncher/lawnchair

src/com/android/launcher3/Launcher.java
@@ -98,6 +98,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.launcher3.DropTarget.DragObject;
+import com.android.launcher3.settings.SettingsActivity;
+import com.android.launcher3.settings.SettingsProvider;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -405,6 +407,9 @@ public class Launcher extends Activity
         mIconCache.flushInvalidIcons(grid);
         mDragController = new DragController(this);
         mInflater = getLayoutInflater();
+
+        // Load all settings
+        SettingsProvider.load(this);
 
         mStats = new Stats(this);
 
@@ -833,6 +838,10 @@ public class Launcher extends Activity
         }
         super.onResume();
 
+        if (settingsChanged()) {
+            finish();
+        }
+
         // Restore the previous launcher state
         if (mOnResumeState == State.WORKSPACE) {
             showWorkspace(false);
@@ -978,6 +987,13 @@ public class Launcher extends Activity
     }
 
     protected void startSettings() {
+        Intent settings = new Intent().setClass(this, SettingsActivity.class);
+        settings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+        startActivity(settings);
+        if (mWorkspace.isInOverviewMode()) {
+            mWorkspace.exitOverviewMode(false);
+        }
     }
 
     public interface QSBScroller {
@@ -4378,6 +4394,22 @@ public class Launcher extends Activity
         Cling cling = (Cling) findViewById(R.id.folder_cling);
         dismissCling(cling, null, Cling.FOLDER_CLING_DISMISSED_KEY,
                 DISMISS_CLING_DURATION, true);
+    }
+
+    /**
+     * To avoid managing preference change listeners for various parts of the
+     * launcher we simply kill the process and let it reload from scratch.
+     */
+    public boolean settingsChanged() {
+        SharedPreferences prefs =
+                getSharedPreferences(SettingsProvider.SETTINGS_KEY, Context.MODE_PRIVATE);
+        boolean settingsChanged = prefs.getBoolean(SettingsProvider.SETTINGS_CHANGED, false);
+        if (settingsChanged) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(SettingsProvider.SETTINGS_CHANGED, false);
+            editor.commit();
+        }
+        return settingsChanged;
     }
 
     /**

src/com/android/launcher3/settings/SettingsActivity.java
@@ -1,0 +1,225 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.*;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+import com.android.launcher3.R;
+
+import java.util.List;
+
+public class SettingsActivity extends PreferenceActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private static final String TAG = "Launcher3.SettingsActivity";
+
+    private SharedPreferences mSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSettings = getSharedPreferences(SettingsProvider.SETTINGS_KEY,
+                Context.MODE_PRIVATE);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSettings.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSettings.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                finish();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preferences_headers, target);
+        updateHeaders(target);
+    }
+
+    private void updateHeaders(List<Header> headers) {
+        int i = 0;
+        while (i < headers.size()) {
+            Header header = headers.get(i);
+
+            // Version preference
+            if (header.id == R.id.preferences_application_version) {
+                header.title = getString(R.string.application_name) + " " + getString(R.string.application_version);
+            }
+
+            // Increment if not removed
+            if (headers.get(i) == header) {
+                i++;
+            }
+        }
+    }
+
+    @Override
+    public void setListAdapter(ListAdapter adapter) {
+        if (adapter == null) {
+            super.setListAdapter(null);
+        } else {
+            super.setListAdapter(new HeaderAdapter(this, getHeaders()));
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putBoolean(SettingsProvider.SETTINGS_CHANGED, true);
+        editor.commit();
+    }
+
+    private static class HeaderAdapter extends ArrayAdapter<Header> {
+        private static final int HEADER_TYPE_NORMAL = 0;
+        private static final int HEADER_TYPE_CATEGORY = 1;
+
+        private static final int HEADER_TYPE_COUNT = HEADER_TYPE_CATEGORY + 1;
+
+        private static class HeaderViewHolder {
+            ImageView icon;
+            TextView title;
+            TextView summary;
+        }
+
+        private LayoutInflater mInflater;
+
+        static int getHeaderType(Header header) {
+            if (header.id == R.id.preferences_application_section) {
+                return HEADER_TYPE_CATEGORY;
+            } else {
+                return HEADER_TYPE_NORMAL;
+            }
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            Header header = getItem(position);
+            return getHeaderType(header);
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false; // because of categories
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return getItemViewType(position) != HEADER_TYPE_CATEGORY;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return HEADER_TYPE_COUNT;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        public HeaderAdapter(Context context, List<Header> objects) {
+            super(context, 0, objects);
+
+            mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            HeaderViewHolder holder;
+            Header header = getItem(position);
+            int headerType = getHeaderType(header);
+            View view = null;
+
+            if (convertView == null) {
+                holder = new HeaderViewHolder();
+                switch (headerType) {
+                    case HEADER_TYPE_CATEGORY:
+                        view = new TextView(getContext(), null,
+                                android.R.attr.listSeparatorTextViewStyle);
+                        holder.title = (TextView) view;
+                        break;
+
+                    case HEADER_TYPE_NORMAL:
+                        view = mInflater.inflate(
+                                R.layout.preference_header_item, parent,
+                                false);
+                        holder.icon = (ImageView) view.findViewById(android.R.id.icon);
+                        holder.title = (TextView)
+                                view.findViewById(android.R.id.title);
+                        holder.summary = (TextView)
+                                view.findViewById(android.R.id.summary);
+                        break;
+                }
+                view.setTag(holder);
+            } else {
+                view = convertView;
+                holder = (HeaderViewHolder) view.getTag();
+            }
+
+            // All view fields must be updated every time, because the view may be recycled
+            switch (headerType) {
+                case HEADER_TYPE_CATEGORY:
+                    holder.title.setText(header.getTitle(getContext().getResources()));
+                    break;
+
+                case HEADER_TYPE_NORMAL:
+                    holder.icon.setImageResource(header.iconRes);
+                    holder.title.setText(header.getTitle(getContext().getResources()));
+                    CharSequence summary = header.getSummary(getContext().getResources());
+                    if (!TextUtils.isEmpty(summary)) {
+                        holder.summary.setVisibility(View.VISIBLE);
+                        holder.summary.setText(summary);
+                    } else {
+                        holder.summary.setVisibility(View.GONE);
+                    }
+                    break;
+            }
+
+            return view;
+        }
+    }
+}

src/com/android/launcher3/settings/SettingsProvider.java
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.launcher3.settings;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Map;
+
+public final class SettingsProvider {
+    public static final String SETTINGS_KEY = "com.android.launcher3_preferences";
+
+    public static final String SETTINGS_CHANGED = "settings_changed";
+
+    private static Map<String, ?> sKeyValues;
+
+    public static void load(Context context) {
+        SharedPreferences preferences = context.getSharedPreferences(SETTINGS_KEY, 0);
+        sKeyValues = preferences.getAll();
+    }
+
+    private static int getInt(String key, int def) {
+        return sKeyValues.containsKey(key) && sKeyValues.get(key) instanceof Integer ?
+                (Integer) sKeyValues.get(key) : def;
+    }
+
+    private static boolean getBoolean(String key, boolean def) {
+        return sKeyValues.containsKey(key) && sKeyValues.get(key) instanceof Boolean ?
+                (Boolean) sKeyValues.get(key) : def;
+    }
+
+    private static String getString(String key, String def) {
+        return sKeyValues.containsKey(key) && sKeyValues.get(key) instanceof String ?
+                (String) sKeyValues.get(key) : def;
+    }
+}