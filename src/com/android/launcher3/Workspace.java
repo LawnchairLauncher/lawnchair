d027815092005de1a63d24ab0c06ea0f982b9843
Flamefire <alex@grundis.de>
2014-04-21 17:58:55 +0200

    Fix NPE if a moved cell does not have a parent
    This NPE should actually never happen. It did however and crashed
    Trebuchet.
    Symptom that will lead to this NPE: Drag Shortcut from homescreen
    to somewhere else. Old icon will not get removed while dragging.
    This should also be fixed!
    
    Change-Id: I5b5070c93077e69f0f63a26d337fb9aee83ec6d2

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -2878,7 +2878,9 @@ public class Workspace extends SmoothPagedView
                     final ItemInfo info = (ItemInfo) cell.getTag();
                     if (hasMovedLayouts) {
                         // Reparent the view
-                        getParentCellLayoutForView(cell).removeView(cell);
+                        CellLayout parent = getParentCellLayoutForView(cell);
+                        if (parent != null)
+                            parent.removeView(cell);
                         addInScreen(cell, container, screenId, mTargetCell[0], mTargetCell[1],
                                 info.spanX, info.spanY);
                     }
