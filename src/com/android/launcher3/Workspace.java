LawnchairLauncher/lawnchair

commit 31cc95a06e7a8bbbf7e9978d03adbba219f8bd2d

Smooth out slide in/out animation for the overview panel
Change-Id: I742d2248b7cf70767f9e776d88db6fc1aaa767ce

src/com/android/launcher3/Workspace.java
@@ -56,6 +56,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityManager;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
@@ -2321,14 +2322,14 @@ public class Workspace extends SmoothPagedView
         float finalBackgroundAlpha = (stateIsSpringLoaded || stateIsOverview) ? 1.0f : 0f;
         float finalBackgroundAlphaMultiplier = (stateIsSpringLoaded || stateIsOverview || mShowOutlines) ? 1.0f : 0f;
         float finalHotseatAndPageIndicatorAlpha = (stateIsOverview || stateIsSmall) ? 0f : 1f;
-        float finalOverviewPanelAlpha = stateIsOverview ? 1f : 0f;
+        final float finalOverviewPanelAlpha = stateIsOverview ? 1f : 0f;
         float finalSearchBarAlpha = !stateIsNormal ? 0f : 1f;
         float finalWorkspaceTranslationY = stateIsOverview ? getOverviewModeTranslationY() : 0;
 
         boolean workspaceToAllApps = (oldStateIsNormal && stateIsSmall);
         boolean allAppsToWorkspace = (oldStateIsSmall && stateIsNormal);
-        boolean workspaceToOverview = (oldStateIsNormal && stateIsOverview);
-        boolean overviewToWorkspace = (oldStateIsOverview && stateIsNormal);
+        final boolean workspaceToOverview = (oldStateIsNormal && stateIsOverview);
+        final boolean overviewToWorkspace = (oldStateIsOverview && stateIsNormal);
 
         mNewScale = 1.0f;
 
@@ -2486,26 +2487,56 @@ public class Workspace extends SmoothPagedView
                 .alpha(finalSearchBarAlpha).withLayer();
             if (mShowSearchBar) searchBarAlpha.addListener(new AlphaUpdateListener(searchBar));
 
-            Animator overviewPanelAlpha = new LauncherViewPropertyAnimator(overviewPanel)
-                .alpha(finalOverviewPanelAlpha).withLayer();
-            overviewPanelAlpha.addListener(new AlphaUpdateListener(overviewPanel));
-
             if (workspaceToOverview) {
                 pageIndicatorAlpha.setInterpolator(new DecelerateInterpolator(2));
                 hotseatAlpha.setInterpolator(new DecelerateInterpolator(2));
-                overviewPanelAlpha.setInterpolator(null);
             } else if (overviewToWorkspace) {
                 pageIndicatorAlpha.setInterpolator(null);
                 hotseatAlpha.setInterpolator(null);
-                overviewPanelAlpha.setInterpolator(new DecelerateInterpolator(2));
             }
             searchBarAlpha.setInterpolator(null);
 
-            overviewPanel.setAlpha(finalOverviewPanelAlpha);
-            AlphaUpdateListener.updateVisibility(overviewPanel);
-            Animation animation = AnimationUtils.loadAnimation(mLauncher, R.anim.drop_down);
-            overviewPanel.startAnimation(animation);
-            anim.play(hotseatAlpha);
+            float mOverviewPanelSlideScale = 1.0f;
+
+            if (overviewToWorkspace) {
+                overviewPanel.setScaleY(1.0f);
+                mOverviewPanelSlideScale = 2.0f;
+            } else if (workspaceToOverview) {
+                overviewPanel.setScaleY(2.0f);
+                mOverviewPanelSlideScale = 1.0f;
+            }
+            LauncherViewPropertyAnimator overviewPanelScale = new LauncherViewPropertyAnimator(overviewPanel);
+            overviewPanelScale.scaleY(mOverviewPanelSlideScale)
+                    .alpha(finalOverviewPanelAlpha)
+                    .setInterpolator(new AccelerateDecelerateInterpolator());
+            overviewPanelScale.addListener(new AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+                    if (workspaceToOverview) {
+                        overviewPanel.setAlpha(finalOverviewPanelAlpha);
+                        AlphaUpdateListener.updateVisibility(overviewPanel);
+                    }
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    if (overviewToWorkspace) {
+                        overviewPanel.setAlpha(finalOverviewPanelAlpha);
+                        AlphaUpdateListener.updateVisibility(overviewPanel);
+                    }
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    overviewPanel.setAlpha(finalOverviewPanelAlpha);
+                    AlphaUpdateListener.updateVisibility(overviewPanel);
+                }
+                @Override
+                public void onAnimationRepeat(Animator animation) {}
+            });
+            anim.play(overviewPanelScale);
+
             if (mShowSearchBar) anim.play(searchBarAlpha);
             anim.play(pageIndicatorAlpha);
             anim.setStartDelay(delay);

res/anim/drop_down.xml
@@ -1,13 +1,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<set xmlns:android="http://schemas.android.com/apk/res/android"
-        android:interpolator="@android:interpolator/accelerate_decelerate">
-    <scale
-       android:fromXScale="1.0"
-       android:toXScale="1.0"
-       android:fromYScale="2.5"
-       android:toYScale="1.0"
-       android:pivotX="50%"
-       android:pivotY="50%"
-       android:duration="300"
-       android:fillBefore="false" />
-</set>

commit 7a812e245677db527b863060821e47b0ba3b0105

Fixing issues introduced in 31cc95a0:  - HotSeat needs go visible/invisible when transitioning in and out of overview mode  - Animation listener for setting alphas needs to remove itself when the animation finishes
Change-Id: I654c0e700c9bc3f77692ed653f8158cec1f2430b

src/com/android/launcher3/Workspace.java
@@ -2495,6 +2495,7 @@ public class Workspace extends SmoothPagedView
                 hotseatAlpha.setInterpolator(null);
             }
             searchBarAlpha.setInterpolator(null);
+            anim.play(hotseatAlpha);
 
             float mOverviewPanelSlideScale = 1.0f;
 
@@ -2505,7 +2506,7 @@ public class Workspace extends SmoothPagedView
                 overviewPanel.setScaleY(2.0f);
                 mOverviewPanelSlideScale = 1.0f;
             }
-            LauncherViewPropertyAnimator overviewPanelScale = new LauncherViewPropertyAnimator(overviewPanel);
+            final LauncherViewPropertyAnimator overviewPanelScale = new LauncherViewPropertyAnimator(overviewPanel);
             overviewPanelScale.scaleY(mOverviewPanelSlideScale)
                     .alpha(finalOverviewPanelAlpha)
                     .setInterpolator(new AccelerateDecelerateInterpolator());
@@ -2525,12 +2526,14 @@ public class Workspace extends SmoothPagedView
                         overviewPanel.setAlpha(finalOverviewPanelAlpha);
                         AlphaUpdateListener.updateVisibility(overviewPanel);
                     }
+                    overviewPanelScale.removeAllListeners();
                 }
 
                 @Override
                 public void onAnimationCancel(Animator animation) {
                     overviewPanel.setAlpha(finalOverviewPanelAlpha);
                     AlphaUpdateListener.updateVisibility(overviewPanel);
+                    overviewPanelScale.removeAllListeners();
                 }
                 @Override
                 public void onAnimationRepeat(Animator animation) {}