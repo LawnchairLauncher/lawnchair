b2f186e293253dd81283785e1d4903f77c8c18a2
Raj Yengisetty <rajesh@cyngn.com>
2014-07-10 10:01:32 -0700

    Fix Overview Panel glitch when animating Workspace into overview mode.
    Change-Id: I12cd8e6e96cdebadfa42590152c4c16e644aded3

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -2502,9 +2502,9 @@ public class Workspace extends SmoothPagedView
 
             if (overviewToWorkspace) {
                 overviewPanel.setScaleY(1.0f);
-                mOverviewPanelSlideScale = 2.0f;
+                mOverviewPanelSlideScale = 2.5f;
             } else if (workspaceToOverview) {
-                overviewPanel.setScaleY(2.0f);
+                overviewPanel.setScaleY(2.5f);
                 mOverviewPanelSlideScale = 1.0f;
             }
 
@@ -2519,7 +2519,6 @@ public class Workspace extends SmoothPagedView
                         overviewPanel.setAlpha(finalOverviewPanelAlpha);
                         AlphaUpdateListener.updateVisibility(overviewPanel);
                     }
-
                 }
 
                 @Override
