LineageOS/android_packages_apps_Trebuchet

Add support for CMHome (1/2)
Add permission for DashClock extensions.
Change Trebuchet to allow viewing additional screen.
Add setting that to configure Google Now / CMHome.

Change-Id: I63286a2cce87455ed411bd4c77680200eec89be7

src/com/android/launcher3/Launcher.java
@@ -56,7 +56,6 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -243,8 +242,6 @@ public class Launcher extends Activity
     private static int NEW_APPS_ANIMATION_INACTIVE_TIMEOUT_SECONDS = 5;
     private static int NEW_APPS_ANIMATION_DELAY = 500;
 
-    private boolean mGelIntegrationEnabled = false;
-
     private final BroadcastReceiver mCloseSystemDialogsReceiver
             = new CloseSystemDialogsIntentReceiver();
     private final ContentObserver mWidgetObserver = new AppWidgetResetObserver();
@@ -378,6 +375,34 @@ public class Launcher extends Activity
 
     private BubbleTextView mWaitingForResume;
 
+    public enum CustomContentMode {
+        DISABLED(0),
+        GEL(1),
+        CUSTOM_HOME(2);
+
+        private final int mValue;
+        private CustomContentMode(int value) {
+            mValue = value;
+        }
+
+        public int getValue() {
+            return mValue;
+        }
+
+        public static CustomContentMode getModeForValue(int value) {
+            switch (value) {
+            case 0:
+                return DISABLED;
+            case 1:
+                return GEL;
+            default :
+                return CUSTOM_HOME;
+            }
+        }
+    }
+
+    private CustomContentMode mCustomContentMode = CustomContentMode.CUSTOM_HOME;
+
     // Preferences
     private boolean mHideIconLabels;
 
@@ -492,7 +517,7 @@ public class Launcher extends Activity
         mSavedState = savedInstanceState;
         restoreState(mSavedState);
 
-        restoreGelSetting();
+        restoreCustomContentMode();
 
         if (PROFILE_STARTUP) {
             android.os.Debug.stopMethodTracing();
@@ -541,10 +566,11 @@ public class Launcher extends Activity
                 "cyanogenmod.permission.PROTECTED_APP", null);
     }
 
-    public void restoreGelSetting() {
-        mGelIntegrationEnabled = SettingsProvider.getBoolean(this,
-                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                R.bool.preferences_interface_homescreen_search_screen_left_default);
+    public void restoreCustomContentMode() {
+        mCustomContentMode = CustomContentMode.getModeForValue(
+                SettingsProvider.getIntCustomDefault(this,
+                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_PANEL_LEFT,
+                CustomContentMode.CUSTOM_HOME.getValue()));
     }
 
     void initializeDynamicGrid() {
@@ -555,7 +581,7 @@ public class Launcher extends Activity
                 SettingsProvider.SETTINGS_UI_HOMESCREEN_HIDE_ICON_LABELS,
                 R.bool.preferences_interface_homescreen_hide_icon_labels_default);
 
-        restoreGelSetting();
+        restoreCustomContentMode();
 
         // Determine the dynamic grid properties
         Point smallestSize = new Point();
@@ -589,7 +615,23 @@ public class Launcher extends Activity
     }
 
     protected boolean hasCustomContentToLeft() {
-       return isGelIntegrationSupported() && isGelIntegrationEnabled();
+        switch(getCustomContentMode()) {
+            case GEL:
+                return isGelIntegrationSupported();
+            case CUSTOM_HOME:
+                return isCustomHomeActive();
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Returns true if the custom home application is initialized and ready
+     * for the user to scroll to it. To be implemented by subclasses.
+     * @return True if the custom home view is initialized.
+     */
+    protected boolean isCustomHomeActive() {
+        return false;
     }
 
     public boolean isGelIntegrationSupported() {
@@ -602,12 +644,16 @@ public class Launcher extends Activity
         return globalSearchActivity != null && isCM();
     }
 
-    public boolean isGelIntegrationEnabled() {
-        return mGelIntegrationEnabled;
+    public CustomContentMode getCustomContentMode() {
+        return mCustomContentMode;
+    }
+
+    public void setCustomContentMode(CustomContentMode customContentMode) {
+        mCustomContentMode = customContentMode;
     }
 
     public void onCustomContentLaunch() {
-        if(isGelIntegrationEnabled() && isGelIntegrationSupported()) {
+        if(isCustomContentModeGel() && isGelIntegrationSupported()) {
             GelIntegrationHelper.getInstance().registerSwipeBackGestureListenerAndStartGel(this);
         }
     }
@@ -1060,7 +1106,7 @@ public class Launcher extends Activity
         }
         super.onResume();
 
-        if(isGelIntegrationEnabled() && isGelIntegrationSupported()) {
+        if(isCustomContentModeGel() && isGelIntegrationSupported()) {
             GelIntegrationHelper.getInstance().handleGelResume();
         }
 
@@ -1216,6 +1262,10 @@ public class Launcher extends Activity
     public void resetQSBScroll() {
         mSearchDropTargetBar.animate().translationY(0).start();
         getQsbBar().animate().translationY(0).start();
+    }
+
+    protected boolean isCustomContentModeGel() {
+        return mCustomContentMode == CustomContentMode.GEL;
     }
 
     public interface CustomContentCallbacks {

src/com/android/launcher3/list/SettingsPinnedHeaderAdapter.java
@@ -124,15 +124,7 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
             ((TextView) v.findViewById(R.id.item_state)).setText(state);
         } else if (title.equals(res
                 .getString(R.string.search_screen_left_text))) {
-            boolean current = SettingsProvider
-                    .getBoolean(
-                            mContext,
-                            SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                            R.bool.preferences_interface_homescreen_search_screen_left_default);
-            String state = current ? res.getString(
-                    R.string.setting_state_on) : res.getString(
-                    R.string.setting_state_off);
-            ((TextView) v.findViewById(R.id.item_state)).setText(state);
+            updateSearchPanelItem(v);
         } else if (title.equals(res.getString(R.string.scrolling_wallpaper))) {
             boolean current = SettingsProvider
                     .getBoolean(
@@ -181,6 +173,24 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
             case InstallTime:
                 state = mLauncher.getResources().getString(
                         R.string.sort_mode_install_time);
+                break;
+        }
+        ((TextView) v.findViewById(R.id.item_state)).setText(state);
+    }
+
+    public void updateSearchPanelItem(View v) {
+        String state = "";
+        switch (mLauncher.getCustomContentMode()) {
+            case DISABLED:
+                state = mLauncher.getResources().getString(
+                        R.string.setting_state_off);
+                break;
+            case GEL:
+                state = mLauncher.getResources().getString(R.string.search_panel_gel);
+                break;
+            default:
+                state = mLauncher.getResources().getString(
+                        R.string.search_panel_custom_home);
                 break;
         }
         ((TextView) v.findViewById(R.id.item_state)).setText(state);
@@ -296,23 +306,8 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
             } else if (value.equals(res
                     .getString(R.string.search_screen_left_text)) &&
                     ((Integer)v.getTag() == OverviewSettingsPanel.HOME_SETTINGS_POSITION)) {
-
-                boolean current = SettingsProvider.getBoolean(mContext,
-                        SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                        R.bool.preferences_interface_homescreen_search_screen_left_default);
-
-                // If GEL integration is not supported, do not allow the user to turn it on.
-                if(!current && !mLauncher.isGelIntegrationSupported()) {
-                    Toast.makeText(mLauncher.getApplicationContext(),
-                            res.getString(R.string.search_screen_left_unsupported_toast),
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    onSettingsBooleanChanged(
-                            v,
-                            SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                            R.bool.preferences_interface_homescreen_search_screen_left_default);
-                    mLauncher.updateDynamicGrid();
-                }
+                onClickSearchPanelButton();
+                mLauncher.updateDynamicGrid();
             } else if (value.equals(res
                     .getString(R.string.grid_size_text))) {
                 mLauncher.onClickDynamicGridSizeButton();
@@ -368,6 +363,28 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
                 AppsCustomizePagedView.SortMode.getModeForValue(sort));
 
         SettingsProvider.putInt(mLauncher, SettingsProvider.SETTINGS_UI_DRAWER_SORT_MODE, sort);
+
+        notifyDataSetChanged();
+    }
+
+    private void onClickSearchPanelButton() {
+        int searchPanelVal = SettingsProvider.getIntCustomDefault(mLauncher,
+                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_PANEL_LEFT,
+                Launcher.CustomContentMode.CUSTOM_HOME.getValue());
+
+        Launcher.CustomContentMode nextCCMode =
+            Launcher.CustomContentMode.getModeForValue(searchPanelVal + 1);
+        if(nextCCMode == Launcher.CustomContentMode.GEL && !mLauncher.isGelIntegrationSupported()) {
+            // GEL is not supported, skip that option
+            searchPanelVal++;
+        }
+
+        searchPanelVal = (searchPanelVal + 1) % Launcher.CustomContentMode.values().length;
+        mLauncher.setCustomContentMode(Launcher.CustomContentMode.getModeForValue(searchPanelVal));
+
+        SettingsProvider.putInt(mLauncher,
+                                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_PANEL_LEFT,
+                                searchPanelVal);
 
         notifyDataSetChanged();
     }

LawnchairLauncher/lawnchair

src/com/android/launcher3/Launcher.java
@@ -56,7 +56,6 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -243,8 +242,6 @@ public class Launcher extends Activity
     private static int NEW_APPS_ANIMATION_INACTIVE_TIMEOUT_SECONDS = 5;
     private static int NEW_APPS_ANIMATION_DELAY = 500;
 
-    private boolean mGelIntegrationEnabled = false;
-
     private final BroadcastReceiver mCloseSystemDialogsReceiver
             = new CloseSystemDialogsIntentReceiver();
     private final ContentObserver mWidgetObserver = new AppWidgetResetObserver();
@@ -380,6 +377,34 @@ public class Launcher extends Activity
 
     private BubbleTextView mWaitingForResume;
 
+    public enum CustomContentMode {
+        DISABLED(0),
+        GEL(1),
+        CUSTOM_HOME(2);
+
+        private final int mValue;
+        private CustomContentMode(int value) {
+            mValue = value;
+        }
+
+        public int getValue() {
+            return mValue;
+        }
+
+        public static CustomContentMode getModeForValue(int value) {
+            switch (value) {
+            case 0:
+                return DISABLED;
+            case 1:
+                return GEL;
+            default :
+                return CUSTOM_HOME;
+            }
+        }
+    }
+
+    private CustomContentMode mCustomContentMode = CustomContentMode.CUSTOM_HOME;
+
     // Preferences
     private boolean mHideIconLabels;
 
@@ -492,7 +517,7 @@ public class Launcher extends Activity
         mSavedState = savedInstanceState;
         restoreState(mSavedState);
 
-        restoreGelSetting();
+        restoreCustomContentMode();
 
         if (PROFILE_STARTUP) {
             android.os.Debug.stopMethodTracing();
@@ -541,10 +566,11 @@ public class Launcher extends Activity
                 "cyanogenmod.permission.PROTECTED_APP", null);
     }
 
-    public void restoreGelSetting() {
-        mGelIntegrationEnabled = SettingsProvider.getBoolean(this,
-                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                R.bool.preferences_interface_homescreen_search_screen_left_default);
+    public void restoreCustomContentMode() {
+        mCustomContentMode = CustomContentMode.getModeForValue(
+                SettingsProvider.getIntCustomDefault(this,
+                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_PANEL_LEFT,
+                CustomContentMode.DISABLED.getValue()));
     }
 
     void initializeDynamicGrid() {
@@ -555,7 +581,7 @@ public class Launcher extends Activity
                 SettingsProvider.SETTINGS_UI_HOMESCREEN_HIDE_ICON_LABELS,
                 R.bool.preferences_interface_homescreen_hide_icon_labels_default);
 
-        restoreGelSetting();
+        restoreCustomContentMode();
 
         // Determine the dynamic grid properties
         Point smallestSize = new Point();
@@ -589,7 +615,23 @@ public class Launcher extends Activity
     }
 
     protected boolean hasCustomContentToLeft() {
-       return isGelIntegrationSupported() && isGelIntegrationEnabled();
+        switch(getCustomContentMode()) {
+            case GEL:
+                return isGelIntegrationSupported();
+            case CUSTOM_HOME:
+                return isCustomHomeActive();
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Returns true if the custom home application is initialized and ready
+     * for the user to scroll to it. To be implemented by subclasses.
+     * @return True if the custom home view is initialized.
+     */
+    protected boolean isCustomHomeActive() {
+        return false;
     }
 
     public boolean isGelIntegrationSupported() {
@@ -602,12 +644,16 @@ public class Launcher extends Activity
         return globalSearchActivity != null && isCM();
     }
 
-    public boolean isGelIntegrationEnabled() {
-        return mGelIntegrationEnabled;
+    public CustomContentMode getCustomContentMode() {
+        return mCustomContentMode;
+    }
+
+    public void setCustomContentMode(CustomContentMode customContentMode) {
+        mCustomContentMode = customContentMode;
     }
 
     public void onCustomContentLaunch() {
-        if(isGelIntegrationEnabled() && isGelIntegrationSupported()) {
+        if(isCustomContentModeGel() && isGelIntegrationSupported()) {
             GelIntegrationHelper.getInstance().registerSwipeBackGestureListenerAndStartGel(this, mWorkspace.isLayoutRtl());
         }
     }
@@ -1060,10 +1106,9 @@ public class Launcher extends Activity
         }
         super.onResume();
 
-
         updateGridIfNeeded();
 
-        if(isGelIntegrationEnabled() && isGelIntegrationSupported()) {
+        if(isCustomContentModeGel() && isGelIntegrationSupported()) {
             GelIntegrationHelper.getInstance().handleGelResume();
         }
 
@@ -1219,6 +1264,10 @@ public class Launcher extends Activity
         if (isSearchBarEnabled()) {
             getQsbBar().animate().translationY(0).start();
         }
+    }
+
+    protected boolean isCustomContentModeGel() {
+        return mCustomContentMode == CustomContentMode.GEL;
     }
 
     public interface CustomContentCallbacks {

src/com/android/launcher3/list/SettingsPinnedHeaderAdapter.java
@@ -124,15 +124,7 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
             ((TextView) v.findViewById(R.id.item_state)).setText(state);
         } else if (title.equals(res
                 .getString(R.string.search_screen_left_text))) {
-            boolean current = SettingsProvider
-                    .getBoolean(
-                            mContext,
-                            SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                            R.bool.preferences_interface_homescreen_search_screen_left_default);
-            String state = current ? res.getString(
-                    R.string.setting_state_on) : res.getString(
-                    R.string.setting_state_off);
-            ((TextView) v.findViewById(R.id.item_state)).setText(state);
+            updateSearchPanelItem(v);
         } else if (title.equals(res.getString(R.string.scrolling_wallpaper))) {
             boolean current = SettingsProvider
                     .getBoolean(
@@ -181,6 +173,24 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
             case InstallTime:
                 state = mLauncher.getResources().getString(
                         R.string.sort_mode_install_time);
+                break;
+        }
+        ((TextView) v.findViewById(R.id.item_state)).setText(state);
+    }
+
+    public void updateSearchPanelItem(View v) {
+        String state = "";
+        switch (mLauncher.getCustomContentMode()) {
+            case DISABLED:
+                state = mLauncher.getResources().getString(
+                        R.string.setting_state_off);
+                break;
+            case GEL:
+                state = mLauncher.getResources().getString(R.string.search_panel_gel);
+                break;
+            default:
+                state = mLauncher.getResources().getString(
+                        R.string.search_panel_custom_home);
                 break;
         }
         ((TextView) v.findViewById(R.id.item_state)).setText(state);
@@ -293,25 +303,11 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
             } else if (value.equals(res
                     .getString(R.string.search_screen_left_text)) &&
                     ((Integer)v.getTag() == OverviewSettingsPanel.HOME_SETTINGS_POSITION)) {
-
-                boolean current = SettingsProvider.getBoolean(mContext,
-                        SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                        R.bool.preferences_interface_homescreen_search_screen_left_default);
-
-                // If GEL integration is not supported, do not allow the user to turn it on.
-                if(!current && !mLauncher.isGelIntegrationSupported()) {
-                    Toast.makeText(mLauncher.getApplicationContext(),
-                            res.getString(R.string.search_screen_left_unsupported_toast),
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    onSettingsBooleanChanged(
-                            v,
-                            SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_SCREEN_LEFT,
-                            R.bool.preferences_interface_homescreen_search_screen_left_default);
-                    mLauncher.restoreGelSetting();
-                    mLauncher.getWorkspace().updatePageScrollForCustomPage(!current);
-                    mLauncher.setUpdateDynamicGrid();
-                }
+                onClickSearchPanelButton();
+                boolean customContentEnabled =
+                        mLauncher.getCustomContentMode() != Launcher.CustomContentMode.DISABLED;
+                mLauncher.getWorkspace().updatePageScrollForCustomPage(customContentEnabled);
+                mLauncher.setUpdateDynamicGrid();
             } else if (value.equals(res
                     .getString(R.string.grid_size_text))) {
                 mLauncher.onClickDynamicGridSizeButton();
@@ -367,6 +363,28 @@ public class SettingsPinnedHeaderAdapter extends PinnedHeaderListAdapter {
                 AppsCustomizePagedView.SortMode.getModeForValue(sort));
 
         SettingsProvider.putInt(mLauncher, SettingsProvider.SETTINGS_UI_DRAWER_SORT_MODE, sort);
+
+        notifyDataSetChanged();
+    }
+
+    private void onClickSearchPanelButton() {
+        int searchPanelVal = SettingsProvider.getIntCustomDefault(mLauncher,
+                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_PANEL_LEFT,
+                Launcher.CustomContentMode.DISABLED.getValue());
+
+        Launcher.CustomContentMode nextCCMode =
+            Launcher.CustomContentMode.getModeForValue(searchPanelVal + 1);
+        if(nextCCMode == Launcher.CustomContentMode.GEL && !mLauncher.isGelIntegrationSupported()) {
+            // GEL is not supported, skip that option
+            searchPanelVal++;
+        }
+
+        searchPanelVal = (searchPanelVal + 1) % Launcher.CustomContentMode.values().length;
+        mLauncher.setCustomContentMode(Launcher.CustomContentMode.getModeForValue(searchPanelVal));
+
+        SettingsProvider.putInt(mLauncher,
+                                SettingsProvider.SETTINGS_UI_HOMESCREEN_SEARCH_PANEL_LEFT,
+                                searchPanelVal);
 
         notifyDataSetChanged();
     }