87cf88393cedb66633bbcf51c283d525ef88932f
Adam Cohen <adamcohen@google.com>
2015-02-18 17:26:22 -0800

    Mark WIDGET_CATEGORY_HOME and WIDGET_CATEGORY_SEARCHBOX on the search widget
    Bug 17334589
    
    Change-Id: I0e14381cfc19822451e68eb7f070d94525e126c3

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -3910,8 +3910,11 @@ public class Launcher extends Activity
             }
 
             Bundle opts = new Bundle();
+            // We indicate that this widget is both acting as a search box affordance as well
+            // as being present on the home screen.
             opts.putInt(AppWidgetManager.OPTION_APPWIDGET_HOST_CATEGORY,
-                    AppWidgetProviderInfo.WIDGET_CATEGORY_SEARCHBOX);
+                    AppWidgetProviderInfo.WIDGET_CATEGORY_SEARCHBOX |
+                    AppWidgetProviderInfo.WIDGET_CATEGORY_HOME_SCREEN);
 
             SharedPreferences sp = getSharedPreferences(
                     LauncherAppState.getSharedPreferencesKey(), MODE_PRIVATE);
