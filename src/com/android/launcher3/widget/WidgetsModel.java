a23dab9c646f2f59b503bc4436183d477b8ad492
Hyunyoung Song <hyunyoungs@google.com>
2015-04-09 16:54:31 -0700

    WidgetTray UI improvement
    - Long application title should be ellipsized
    - Disable the drawable when the view is no longer attached to the window
    - Save the PackageItemInfo to the WidgetModel once full res icon bitmap is loaded
    
    b/19897708
    
    Change-Id: I96f260f9d165b206905a127fcee9879556c3d0c4

--- src/com/android/launcher3/widget/WidgetsModel.java
+++ src/com/android/launcher3/widget/WidgetsModel.java
@@ -133,4 +133,8 @@ public class WidgetsModel {
             }
         });
     }
+
+    public void setPackageItemInfo(String packageName, PackageItemInfo infoOut) {
+        mPackageItemInfoList.put(packageName, infoOut);
+    }
 }
