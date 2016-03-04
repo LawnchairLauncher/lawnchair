3650019856ad9bd44ad8ecb591917f7f2520e8d6
Hyunyoung Song <hyunyoungs@google.com>
2016-03-04 12:00:25 -0800

    Trim down the logging class
    b/26494415
    
    Change-Id: I9035f515e4eb54af8f8ad2f29ab294d2ac9760e8

--- src/com/android/launcher3/logging/UserEventLogger.java
+++ src/com/android/launcher3/logging/UserEventLogger.java
@@ -1,7 +1,4 @@
 package com.android.launcher3.logging;
 
-import com.android.launcher3.userevent.nano.LauncherLogProto;
-
 public abstract class UserEventLogger {
-    public void processEvent(LauncherLogProto proto) {}
 }
