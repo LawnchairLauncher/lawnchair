ea045f973a90781e15a0f2d8e2dff994b828613b
huiwan <huiwan@codeaurora.org>
2016-08-16 10:16:31 +0800

    Fix requestFocus()' on a null object reference
    Runtime exception when resume the Launcher, the requestFocus called
    on null pointer reference.
    
    Change-Id: I9cad959bb6ae1228ddf576720316a30e22cc8222

--- app/src/main/java/ch/deletescape/lawnchair/Launcher.java
+++ app/src/main/java/ch/deletescape/lawnchair/Launcher.java
@@ -3618,7 +3618,8 @@ public class Launcher extends Activity
         }
         if (mSavedState != null) {
             if (!mWorkspace.hasFocus()) {
-                mWorkspace.getChildAt(mWorkspace.getCurrentPage()).requestFocus();
+                View v = mWorkspace.getChildAt(mWorkspace.getCurrentPage());
+                if (v != null) v.requestFocus();
             }
 
             mSavedState = null;
