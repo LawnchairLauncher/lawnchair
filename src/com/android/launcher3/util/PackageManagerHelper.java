b4810a3c2074107c55c808daec44930b2a8af76e
Sunny Goyal <sunnygoyal@google.com>
2016-09-08 14:32:06 -0700

    Preventing a shortcut which requires permissions from being added to homescreen
    A shortcut can be added by any app as INSTALL_SHORTCUT is a normal
    level permission. But the intent is actually launched by the launcher
    app which can have other permission as well.
    
    > When adding a shortcut from the broadcast, verify that the intent does
    not require any permission
    > When adding a shortcut using the two-step drop process, verify that
    the source app also has the permission to create such a shortcut
    
    Bug: 30778130
    Change-Id: I33a391bc0af81248aaff4459aaa79d1adc77926b
    (cherry picked from commit fb5096d07bb3bb59fd4b5db6b68613030169b4bd)
    (cherry picked from commit 116d34bc634cfe12ffa2f291df286629d5abdcfd)

--- src/com/android/launcher3/util/PackageManagerHelper.java
+++ src/com/android/launcher3/util/PackageManagerHelper.java
@@ -16,8 +16,15 @@
 
 package com.android.launcher3.util;
 
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
+import android.os.Build;
+import android.text.TextUtils;
 
 import com.android.launcher3.Utilities;
 
@@ -67,5 +74,54 @@ public class PackageManagerHelper {
         } else {
             return false;
         }
+    }
+
+    /**
+     * Returns true if {@param srcPackage} has the permission required to start the activity from
+     * {@param intent}. If {@param srcPackage} is null, then the activity should not need
+     * any permissions
+     */
+    public static boolean hasPermissionForActivity(Context context, Intent intent,
+            String srcPackage) {
+        PackageManager pm = context.getPackageManager();
+        ResolveInfo target = pm.resolveActivity(intent, 0);
+        if (target == null) {
+            // Not a valid target
+            return false;
+        }
+        if (TextUtils.isEmpty(target.activityInfo.permission)) {
+            // No permission is needed
+            return true;
+        }
+        if (TextUtils.isEmpty(srcPackage)) {
+            // The activity requires some permission but there is no source.
+            return false;
+        }
+
+        // Source does not have sufficient permissions.
+        if(pm.checkPermission(target.activityInfo.permission, srcPackage) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return false;
+        }
+
+        if (!Utilities.ATLEAST_MARSHMALLOW) {
+            // These checks are sufficient for below M devices.
+            return true;
+        }
+
+        // On M and above also check AppOpsManager for compatibility mode permissions.
+        if (TextUtils.isEmpty(AppOpsManager.permissionToOp(target.activityInfo.permission))) {
+            // There is no app-op for this permission, which could have been disabled.
+            return true;
+        }
+
+        // There is no direct way to check if the app-op is allowed for a particular app. Since
+        // app-op is only enabled for apps running in compatibility mode, simply block such apps.
+
+        try {
+            return pm.getApplicationInfo(srcPackage, 0).targetSdkVersion >= Build.VERSION_CODES.M;
+        } catch (NameNotFoundException e) { }
+
+        return false;
     }
 }
