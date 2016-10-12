0fed55bf031087474a3833a3e28f4263c8dc7959
Tony Wickham <twickham@google.com>
2016-10-12 14:55:46 -0700

    Add feature flag to disable light status bar
    Bug: 32085545
    Change-Id: I4860145b00893c20f7e37817e9659ccd90409966

--- src_config/com/android/launcher3/config/FeatureFlags.java
+++ src_config/com/android/launcher3/config/FeatureFlags.java
@@ -37,4 +37,6 @@ public final class FeatureFlags {
     public static final boolean NO_ALL_APPS_ICON = true;
     // When enabled fling down gesture on the first workspace triggers search.
     public static final boolean PULLDOWN_SEARCH = false;
+    // When enabled the status bar may show dark icons based on the top of the wallpaper.
+    public static final boolean LIGHT_STATUS_BAR = false;
 }
