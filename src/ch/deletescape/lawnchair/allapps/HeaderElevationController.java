9a7fb53e5fb3b6228af9a2f5d67a7ddc00408594
deletescape <deletescape@outlook.com>
2017-03-22 11:57:05 +0100

    Delete unused compat class

--- src/ch/deletescape/lawnchair/allapps/HeaderElevationController.java
+++ src/ch/deletescape/lawnchair/allapps/HeaderElevationController.java
@@ -36,46 +36,7 @@ public abstract class HeaderElevationController extends RecyclerView.OnScrollLis
     public void updateBackgroundPadding(Rect bgPadding) { }
 
     abstract void onScroll(int scrollY);
-
-    public static class ControllerV16 extends HeaderElevationController {
-
-        private final View mShadow;
-        private final float mScrollToElevation;
-
-        public ControllerV16(View header) {
-            Resources res = header.getContext().getResources();
-            mScrollToElevation = res.getDimension(R.dimen.all_apps_header_scroll_to_elevation);
-
-            mShadow = new View(header.getContext());
-            mShadow.setBackground(new GradientDrawable(
-                    GradientDrawable.Orientation.TOP_BOTTOM, new int[] {0x1E000000, 0x00000000}));
-            mShadow.setAlpha(0);
-
-            FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
-                    FrameLayout.LayoutParams.MATCH_PARENT,
-                    res.getDimensionPixelSize(R.dimen.all_apps_header_shadow_height));
-            lp.topMargin = ((FrameLayout.LayoutParams) header.getLayoutParams()).height;
-
-            ((ViewGroup) header.getParent()).addView(mShadow, lp);
-        }
-
-        @Override
-        public void onScroll(int scrollY) {
-            float elevationPct = Math.min(scrollY, mScrollToElevation) /
-                    mScrollToElevation;
-            mShadow.setAlpha(elevationPct);
-        }
-
-        @Override
-        public void updateBackgroundPadding(Rect bgPadding) {
-            FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) mShadow.getLayoutParams();
-            lp.leftMargin = bgPadding.left;
-            lp.rightMargin = bgPadding.right;
-            mShadow.requestLayout();
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    
     public static class ControllerVL extends HeaderElevationController {
 
         private final View mHeader;
