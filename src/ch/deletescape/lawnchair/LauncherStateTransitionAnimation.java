LawnchairLauncher/lawnchair

commit f01a51d4ec7d98b687e7c51ac81d09aa262f9f79

Clean Up some code / get rid of useless logging stuff

src/ch/deletescape/lawnchair/LauncherStateTransitionAnimation.java
@@ -164,7 +164,6 @@ public class LauncherStateTransitionAnimation {
 
             @Override
             void onTransitionComplete() {
-                mLauncher.getUserEventDispatcher().resetElapsedContainerMillis();
                 if (startSearchAfterTransition) {
                     toView.startAppsSearch();
                 }
@@ -182,13 +181,7 @@ public class LauncherStateTransitionAnimation {
         final WidgetsContainerView toView = mLauncher.getWidgetsView();
         final View buttonView = mLauncher.getWidgetsButton();
         startAnimationToOverlay(
-                Workspace.State.OVERVIEW_HIDDEN, buttonView, toView, animated, CIRCULAR_REVEAL,
-                new PrivateTransitionCallbacks(FINAL_REVEAL_ALPHA_FOR_WIDGETS) {
-                    @Override
-                    void onTransitionComplete() {
-                        mLauncher.getUserEventDispatcher().resetElapsedContainerMillis();
-                    }
-                });
+                Workspace.State.OVERVIEW_HIDDEN, buttonView, toView, animated, CIRCULAR_REVEAL, null);
     }
 
     /**
@@ -530,11 +523,6 @@ public class LauncherStateTransitionAnimation {
                     }
                 };
             }
-
-            @Override
-            void onTransitionComplete() {
-                mLauncher.getUserEventDispatcher().resetElapsedContainerMillis();
-            }
         };
         // Only animate the search bar if animating to spring loaded mode from all apps
         startAnimationToWorkspaceFromOverlay(fromWorkspaceState, toWorkspaceState,
@@ -560,11 +548,6 @@ public class LauncherStateTransitionAnimation {
                                 revealView.setVisibility(View.INVISIBLE);
                             }
                         };
-                    }
-
-                    @Override
-                    void onTransitionComplete() {
-                        mLauncher.getUserEventDispatcher().resetElapsedContainerMillis();
                     }
                 };
         startAnimationToWorkspaceFromOverlay(

build.gradle
@@ -13,7 +13,6 @@ buildscript {
 
 apply plugin: 'com.android.application'
 apply plugin: 'com.google.firebase.firebase-perf'
-apply plugin: 'com.google.protobuf'
 
 android {
     compileSdkVersion 25
@@ -55,7 +54,6 @@ android {
             res.srcDirs = ['res']
             java.srcDirs = ['src', 'src_config']
             manifest.srcFile 'AndroidManifest-common.xml'
-            proto.srcDirs 'protos/'
         }
 
         androidTest {
@@ -80,7 +78,6 @@ dependencies {
     compile 'com.android.support:support-v4:25.3.1'
     compile 'com.android.support:recyclerview-v7:25.3.1'
     compile 'com.android.support:palette-v7:25.3.1'
-    compile 'com.google.protobuf.nano:protobuf-javanano:3.0.0-alpha-2'
     compile 'com.google.firebase:firebase-core:10.2.6'
     compile 'com.google.firebase:firebase-crash:10.2.6'
     compile 'com.google.firebase:firebase-perf:10.2.6'
@@ -89,23 +86,5 @@ dependencies {
     androidTestCompile 'com.android.support.test:runner:0.5'
     androidTestCompile 'com.android.support.test.uiautomator:uiautomator-v18:2.1.2'
     androidTestCompile 'com.android.support:support-annotations:25.3.1'
-}
-
-protobuf {
-    // Configure the protoc executable
-    protoc {
-        artifact = 'com.google.protobuf:protoc:3.0.0-alpha-3'
-
-        generateProtoTasks {
-            all().each { task ->
-                task.builtins {
-                    remove java
-                    javanano {
-                        option 'ignore_services=false'
-                    }
-                }
-            }
-        }
-    }
 }
 apply plugin: 'com.google.gms.google-services'

protos/launcher_log.proto
@@ -1,138 +1,0 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-syntax = "proto2";
-
-option java_package = "ch.deletescape.lawnchair.userevent.nano";
-option java_outer_classname = "LauncherLogProto";
-
-package userevent;
-
-message Target {
-  enum Type {
-    NONE = 0;
-    ITEM = 1;
-    CONTROL = 2;
-    CONTAINER = 3;
-  }
-
-  optional Type type = 1;
-
-  // For container type and item type
-  // Used mainly for ContainerType.FOLDER, ItemType.*
-  optional int32 page_index = 2;
-  optional int32 rank = 3;
-  optional int32 grid_x = 4;
-  optional int32 grid_y = 5;
-
-  // For container types only
-  optional ContainerType container_type = 6;
-  optional int32 cardinality = 7;
-
-  // For control types only
-  optional ControlType control_type = 8;
-
-  // For item types only
-  optional ItemType item_type = 9;
-  optional int32 package_name_hash = 10;
-  optional int32 component_hash = 11;      // Used for ItemType.WIDGET
-  optional int32 intent_hash = 12;         // Used for ItemType.SHORTCUT
-  optional int32 span_x = 13 [default = 1];// Used for ItemType.WIDGET
-  optional int32 span_y = 14 [default = 1];// Used for ItemType.WIDGET
-}
-
-// Used to define what type of item a Target would represent.
-enum ItemType {
-  DEFAULT_ITEMTYPE = 0;
-  APP_ICON = 1;
-  SHORTCUT = 2;
-  WIDGET = 3;
-  FOLDER_ICON = 4;
-  DEEPSHORTCUT = 5;
-  SEARCHBOX = 6;
-}
-
-// Used to define what type of container a Target would represent.
-enum ContainerType {
-  DEFAULT_CONTAINERTYPE = 0;
-  WORKSPACE = 1;
-  HOTSEAT = 2;
-  FOLDER = 3;
-  ALLAPPS = 4;
-  WIDGETS = 5;
-  OVERVIEW = 6;
-  SEARCHRESULT = 8;
-  DEEPSHORTCUTS = 9;
-}
-
-// Used to define what type of control a Target would represent.
-enum ControlType {
-  DEFAULT_CONTROLTYPE = 0;
-  ALL_APPS_BUTTON = 1;
-  WIDGETS_BUTTON = 2;
-  WALLPAPER_BUTTON = 3;
-  SETTINGS_BUTTON = 4;
-  REMOVE_TARGET = 5;
-  UNINSTALL_TARGET = 6;
-  APPINFO_TARGET = 7;
-  RESIZE_HANDLE = 8;
-  VERTICAL_SCROLL = 9;
-  // HOME, BACK, GO_TO_PLAYSTORE
-}
-
-// Used to define the action component of the LauncherEvent.
-message Action {
-  enum Type {
-    TOUCH = 0;
-    AUTOMATED = 1;
-    // SOFT_KEYBOARD, HARD_KEYBOARD, ASSIST
-  }
-  enum Touch {
-    TAP = 0;
-    LONGPRESS = 1;
-    DRAGDROP = 2;
-    SWIPE = 3;
-    FLING = 4;
-    PINCH = 5;
-  }
- enum Direction {
-    NONE = 0;
-    UP = 1;
-    DOWN = 2;
-    LEFT = 3;
-    RIGHT = 4;
-  }
-  optional Type type = 1;
-  optional Touch touch = 2;
-  optional Direction dir = 3;
-}
-
-//
-// Context free grammar of typical user interaction:
-//         Action (Touch) + Target
-//         Action (Touch) + Target + Target
-//
-message LauncherEvent {
-
-  required Action action = 1;
-
-  // List of targets that touch actions can be operated on.
-  repeated Target src_target = 2;
-  repeated Target dest_target = 3;
-
-  optional int64 action_duration_millis = 4;
-  optional int64 elapsed_container_millis = 5;
-  optional int64 elapsed_session_millis = 6;
-}

src/ch/deletescape/lawnchair/DragSource.java
@@ -19,12 +19,11 @@ package ch.deletescape.lawnchair;
 import android.view.View;
 
 import ch.deletescape.lawnchair.DropTarget.DragObject;
-import ch.deletescape.lawnchair.logging.UserEventDispatcher.LogContainerProvider;
 
 /**
  * Interface defining an object that can originate a drag.
  */
-public interface DragSource extends LogContainerProvider {
+public interface DragSource {
 
     /**
      * @return whether items dragged from this source supports

src/ch/deletescape/lawnchair/Hotseat.java
@@ -27,17 +27,12 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.ColorUtils;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
 import android.view.ViewDebug;
 import android.widget.FrameLayout;
 
 import ch.deletescape.lawnchair.dynamicui.ExtractedColors;
-import ch.deletescape.lawnchair.logging.UserEventDispatcher;
-import ch.deletescape.lawnchair.userevent.nano.LauncherLogProto;
-import ch.deletescape.lawnchair.userevent.nano.LauncherLogProto.Target;
 
-public class Hotseat extends FrameLayout
-        implements UserEventDispatcher.LogContainerProvider {
+public class Hotseat extends FrameLayout {
 
     private CellLayout mContent;
 
@@ -117,13 +112,6 @@ public class Hotseat extends FrameLayout
         // the normal state or an accessible drag is in progress.
         return mLauncher.getWorkspace().workspaceInModalState() &&
                 !mLauncher.getAccessibilityDelegate().isInAccessibleDrag();
-    }
-
-    @Override
-    public void fillInLogContainerData(View v, ItemInfo info, Target target, Target targetParent) {
-        target.gridX = info.cellX;
-        target.gridY = info.cellY;
-        targetParent.containerType = LauncherLogProto.HOTSEAT;
     }
 
     public void updateColor(ExtractedColors extractedColors, boolean animate) {

src/ch/deletescape/lawnchair/Launcher.java
@@ -105,13 +105,10 @@ import ch.deletescape.lawnchair.dynamicui.ExtractedColors;
 import ch.deletescape.lawnchair.folder.Folder;
 import ch.deletescape.lawnchair.folder.FolderIcon;
 import ch.deletescape.lawnchair.keyboard.ViewGroupFocusHelper;
-import ch.deletescape.lawnchair.logging.UserEventDispatcher;
 import ch.deletescape.lawnchair.model.WidgetsModel;
-import ch.deletescape.lawnchair.pageindicators.PageIndicator;
 import ch.deletescape.lawnchair.shortcuts.DeepShortcutManager;
 import ch.deletescape.lawnchair.shortcuts.DeepShortcutsContainer;
 import ch.deletescape.lawnchair.shortcuts.ShortcutKey;
-import ch.deletescape.lawnchair.userevent.nano.LauncherLogProto;
 import ch.deletescape.lawnchair.util.ActivityResultInfo;
 import ch.deletescape.lawnchair.util.ComponentKey;
 import ch.deletescape.lawnchair.util.ItemInfoMatcher;
@@ -218,7 +215,7 @@ public class Launcher extends Activity
     Hotseat mHotseat;
     private ViewGroup mOverviewPanel;
 
-    private View mAllAppsButton;
+    private View mAllAppsHandle;
     private View mWidgetsButton;
 
     private DropTargetBar mDropTargetBar;
@@ -325,8 +322,6 @@ public class Launcher extends Activity
      */
     private PendingRequestArgs mPendingRequestArgs;
 
-    private UserEventDispatcher mUserEventDispatcher;
-
     public ViewGroupFocusHelper mFocusHandler;
 
     @Override
@@ -432,16 +427,6 @@ public class Launcher extends Activity
 
     @Override
     public void onLauncherProviderChange() {
-    }
-
-    public UserEventDispatcher getUserEventDispatcher() {
-        // Logger object is a singleton and does not have to be coupled with the foreground
-        // activity. Since most user event logging is done on the UI, the object is retrieved
-        // from the callback for convenience.
-        if (mUserEventDispatcher == null) {
-            mUserEventDispatcher = new UserEventDispatcher();
-        }
-        return mUserEventDispatcher;
     }
 
     public boolean isDraggingEnabled() {
@@ -736,7 +721,6 @@ public class Launcher extends Activity
     @Override
     protected void onResume() {
         super.onResume();
-        getUserEventDispatcher().resetElapsedSessionMillis();
 
         // Restore the previous launcher state
         if (mOnResumeState == State.WORKSPACE) {
@@ -1045,11 +1029,10 @@ public class Launcher extends Activity
     }
 
     /**
-     * Sets the all apps button. This method is called from {@link Hotseat}.
-     * TODO: Get rid of this.
+     * Sets the all apps handle.
      */
-    public void setAllAppsButton(View allAppsButton) {
-        mAllAppsButton = allAppsButton;
+    public void setAllAppsHandle(View allAppsHandle) {
+        mAllAppsHandle = allAppsHandle;
     }
 
     public View getStartViewForAllAppsRevealAnimation() {
@@ -1920,9 +1903,8 @@ public class Launcher extends Activity
             if (v instanceof FolderIcon) {
                 onClickFolderIcon(v);
             }
-        } else if ((v instanceof PageIndicator) ||
-                v == mAllAppsButton) {
-            onClickAllAppsButton();
+        } else if (v == mAllAppsHandle) {
+            onClickAllAppsHandle();
         } else if (tag instanceof AppInfo) {
             startAppShortcutOrInfoActivity(v);
         } else if (tag instanceof LauncherAppWidgetInfo) {
@@ -1995,25 +1977,16 @@ public class Launcher extends Activity
                 info.appWidgetId, this, mAppWidgetHost, REQUEST_RECONFIGURE_APPWIDGET);
     }
 
-    /**
-     * Event handler for the "grid" button that appears on the home screen, which
-     * enters all apps mode.
-     *
-     */
-    protected void onClickAllAppsButton() {
-        mFirebaseAnalytics.logEvent("click_allappsbutton", null);
+    protected void onClickAllAppsHandle() {
+        mFirebaseAnalytics.logEvent("click_allappshandle", null);
         if (!isAppsViewVisible()) {
-            getUserEventDispatcher().logActionOnControl(LauncherLogProto.Action.TAP,
-                    LauncherLogProto.ALL_APPS_BUTTON);
             showAppsView(true /* animated */, false /* focusSearchBar */);
         }
     }
 
-    protected void onLongClickAllAppsButton() {
-        mFirebaseAnalytics.logEvent("longclick_allappsbutton", null);
+    protected void onLongClickAllAppsHandle() {
+        mFirebaseAnalytics.logEvent("longclick_allappshandle", null);
         if (!isAppsViewVisible()) {
-            getUserEventDispatcher().logActionOnControl(LauncherLogProto.Action.LONGPRESS,
-                    LauncherLogProto.ALL_APPS_BUTTON);
             showAppsView(true /* animated */, true /* focusSearchBar */);
         }
     }
@@ -2099,7 +2072,6 @@ public class Launcher extends Activity
             throw new IllegalArgumentException("Input must have a valid intent");
         }
         boolean success = startActivitySafely(v, intent, item);
-        getUserEventDispatcher().logAppLaunch(v, intent);
 
         if (success && v instanceof BubbleTextView) {
             mWaitingForResume = (BubbleTextView) v;
@@ -2543,9 +2515,8 @@ public class Launcher extends Activity
         if (isWorkspaceLocked()) return false;
         if (mState != State.WORKSPACE) return false;
 
-        if ((v instanceof PageIndicator) ||
-                (v == mAllAppsButton && mAllAppsButton != null)) {
-            onLongClickAllAppsButton();
+        if (v == mAllAppsHandle && mAllAppsHandle != null) {
+            onLongClickAllAppsHandle();
             return true;
         }
 
@@ -2668,8 +2639,8 @@ public class Launcher extends Activity
                     Workspace.State.NORMAL, animated, onCompleteRunnable);
 
             // Set focus to the AppsCustomize button
-            if (mAllAppsButton != null) {
-                mAllAppsButton.requestFocus();
+            if (mAllAppsHandle != null) {
+                mAllAppsHandle.requestFocus();
             }
         }
 

src/ch/deletescape/lawnchair/Workspace.java
@@ -73,8 +73,6 @@ import ch.deletescape.lawnchair.dynamicui.ExtractedColors;
 import ch.deletescape.lawnchair.folder.Folder;
 import ch.deletescape.lawnchair.folder.FolderIcon;
 import ch.deletescape.lawnchair.graphics.DragPreviewProvider;
-import ch.deletescape.lawnchair.userevent.nano.LauncherLogProto;
-import ch.deletescape.lawnchair.userevent.nano.LauncherLogProto.Target;
 import ch.deletescape.lawnchair.util.ItemInfoMatcher;
 import ch.deletescape.lawnchair.util.LongArrayMap;
 import ch.deletescape.lawnchair.util.Thunk;
@@ -3768,20 +3766,6 @@ public class Workspace extends PagedView
         }
         return getContext().getString(R.string.workspace_scroll_format,
                 page + 1, nScreens);
-    }
-
-    @Override
-    public void fillInLogContainerData(View v, ItemInfo info, Target target, Target targetParent) {
-        target.gridX = info.cellX;
-        target.gridY = info.cellY;
-        target.pageIndex = getCurrentPage();
-        targetParent.containerType = LauncherLogProto.WORKSPACE;
-        if (info.container == LauncherSettings.Favorites.CONTAINER_HOTSEAT) {
-            target.rank = info.rank;
-            targetParent.containerType = LauncherLogProto.HOTSEAT;
-        } else if (info.container >= 0) {
-            targetParent.containerType = LauncherLogProto.FOLDER;
-        }
     }
 
     /**

src/ch/deletescape/lawnchair/allapps/AllAppsContainerView.java
@@ -58,7 +58,6 @@ import ch.deletescape.lawnchair.folder.Folder;
 import ch.deletescape.lawnchair.graphics.TintedDrawableSpan;
 import ch.deletescape.lawnchair.keyboard.FocusedItemDecorator;
 import ch.deletescape.lawnchair.shortcuts.DeepShortcutsContainer;
-import ch.deletescape.lawnchair.userevent.nano.LauncherLogProto.Target;
 import ch.deletescape.lawnchair.util.ComponentKey;
 
 
@@ -596,11 +595,6 @@ public class AllAppsContainerView extends BaseContainerView implements DragSourc
         mSearchQueryBuilder.clear();
         mSearchQueryBuilder.clearSpans();
         Selection.setSelection(mSearchQueryBuilder, 0);
-    }
-
-    @Override
-    public void fillInLogContainerData(View v, ItemInfo info, Target target, Target targetParent) {
-        targetParent.containerType = mAppsRecyclerView.getContainerType();
     }
 
     public boolean shouldRestoreImeState() {

commit 1b0d694fa6ee7aebbec02a5b5307b2f912ad504b

Fix crash when trying to open widgets view (caused by f01a51d4ec7d98b687e7c51ac81d09aa262f9f79)

src/ch/deletescape/lawnchair/LauncherStateTransitionAnimation.java
@@ -181,7 +181,7 @@ public class LauncherStateTransitionAnimation {
         final WidgetsContainerView toView = mLauncher.getWidgetsView();
         final View buttonView = mLauncher.getWidgetsButton();
         startAnimationToOverlay(
-                Workspace.State.OVERVIEW_HIDDEN, buttonView, toView, animated, CIRCULAR_REVEAL, null);
+                Workspace.State.OVERVIEW_HIDDEN, buttonView, toView, animated, CIRCULAR_REVEAL, new PrivateTransitionCallbacks(FINAL_REVEAL_ALPHA_FOR_WIDGETS));
     }
 
     /**