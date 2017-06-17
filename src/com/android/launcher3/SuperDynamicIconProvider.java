3970b854099f6552c49e1585ca9228a52bb1d133
Amir Zaidi <azaidi@live.nl>
2017-06-17 19:38:26 +0200

    Revert 5ac500c7daa1cb141497d9f463341e75e8e61d40

--- src/com/android/launcher3/SuperDynamicIconProvider.java
+++ src/com/android/launcher3/SuperDynamicIconProvider.java
@@ -69,25 +69,6 @@ public class SuperDynamicIconProvider extends IconProvider
         return "com.google.android.calendar".equals(s);
     }
 
-    private Drawable getNormalIcon(String packageName, int iconDpi) {
-        try {
-            Resources resourcesForApplication = mPackageManager.getResourcesForApplication(packageName);
-            AssetManager assets = resourcesForApplication.getAssets();
-            XmlResourceParser parseXml = assets.openXmlResourceParser("AndroidManifest.xml");
-            int eventType;
-            while ((eventType = parseXml.nextToken()) != XmlPullParser.END_DOCUMENT)
-                if (eventType == XmlPullParser.START_TAG && parseXml.getName().equals("application"))
-                    for (int i = 0; i < parseXml.getAttributeCount(); i++)
-                        if (parseXml.getAttributeName(i).equals("icon"))
-                            return resourcesForApplication.getDrawableForDensity(Integer.parseInt(parseXml.getAttributeValue(i).substring(1)), iconDpi);
-            parseXml.close();
-        }
-        catch (Exception ex) {
-            Log.w("getNormalIcon", ex);
-        }
-        return null;
-    }
-
     private Drawable getRoundIcon(String packageName, int iconDpi) {
         try {
             Resources resourcesForApplication = mPackageManager.getResourcesForApplication(packageName);
@@ -111,9 +92,6 @@ public class SuperDynamicIconProvider extends IconProvider
     public Drawable getIcon(final LauncherActivityInfoCompat launcherActivityInfoCompat, int iconDpi) {
         String packageName = launcherActivityInfoCompat.getApplicationInfo().packageName;
         Drawable drawable = getRoundIcon(packageName, iconDpi);
-        if (drawable == null) {
-            drawable = getNormalIcon(packageName, iconDpi); //can clear overlays
-        }
 
         if (isCalendar(packageName)) {
             try {
