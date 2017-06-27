d28f3a401eba5b5e2adcc5eb14fb1b0c806c2711
Jozsef Kiraly <fonix232@gmail.com>
2017-06-27 14:34:09 +0200

    Move uploads to separate scripts

---
+++ scripts/deploy-hockeyapp.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+./deploy-telegram.sh
+
+BRANCH=$0
+
+curl \
+    -F "status=2" \
+    -F "notify=1" \
+    -F "tags=$BRANCH" \
+    -F "dsym=@proguard-$TRAVIS_BUILD_NUMBER.txt" \
+    -F "ipa=@Lawnchair-$TRAVIS_BUILD_NUMBER.apk" \
+    -H "X-HockeyAppToken: $HOCKEYAPP_TOKEN" \
+    https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APPID/app_versions/upload
