2de2d6781bcdf2e58da59c1eef353e35ff4cb5bb
Mario Bertschler <bmario@google.com>
2017-06-30 18:51:46 -0700

    Settings: only replace content with new fragment if it has no saved instance state.
    Bug: 63141698
    Change-Id: Ifdaca42224199cfd8fdd2fc01c3760faecf35e2d

--- src/com/android/launcher3/SettingsActivity.java
+++ src/com/android/launcher3/SettingsActivity.java
@@ -42,10 +42,12 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new LauncherSettingsFragment())
-                .commit();
+        if (savedInstanceState == null) {
+            // Display the fragment as the main content.
+            getFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new LauncherSettingsFragment())
+                    .commit();
+        }
     }
 
     /**
