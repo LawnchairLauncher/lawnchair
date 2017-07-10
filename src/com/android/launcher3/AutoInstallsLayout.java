decdca0763207b72d82d40f70a9ed81e4e8fce7b
paphonb <paphonb@gmail.com>
2017-07-10 10:51:26 +0700

    Fix weird issue from new Gradle build tools

--- src/com/android/launcher3/AutoInstallsLayout.java
+++ src/com/android/launcher3/AutoInstallsLayout.java
@@ -646,7 +646,7 @@ public class AutoInstallsLayout {
      */
     protected static String getAttributeValue(XmlResourceParser parser, String attribute) {
         String value = parser.getAttributeValue(
-                "http://schemas.android.com/apk/res-auto/com.android.launcher3", attribute);
+                "http://schemas.android.com/apk/res-auto", attribute);
         if (value == null) {
             value = parser.getAttributeValue(null, attribute);
         }
@@ -660,7 +660,7 @@ public class AutoInstallsLayout {
     protected static int getAttributeResourceValue(XmlResourceParser parser, String attribute,
             int defaultValue) {
         int value = parser.getAttributeResourceValue(
-                "http://schemas.android.com/apk/res-auto/com.android.launcher3", attribute,
+                "http://schemas.android.com/apk/res-auto", attribute,
                 defaultValue);
         if (value == defaultValue) {
             value = parser.getAttributeResourceValue(null, attribute, defaultValue);
