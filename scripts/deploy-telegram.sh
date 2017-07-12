5b6c1ddeac898193756c2315cd3cf960c124f61a
deletescape <deletescape@outlook.com>
2017-07-12 08:53:17 +0200

    derp'd

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -3,5 +3,5 @@
 cp build/outputs/apk/Lawnchair-aosp-debug.apk Lawnchair-$TRAVIS_BUILD_NUMBER.apk
 cp build/outputs/mapping/aosp/debug/mapping.txt proguard-$TRAVIS_BUILD_NUMBER.txt
 
-curl -F chat_id="lawnchairci" -F document=@"Lawnchair-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
+curl -F chat_id="@lawnchairci" -F document=@"Lawnchair-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="152559768" -F document=@"proguard-$TRAVIS_BUILD_NUMBER.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
