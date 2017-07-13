3e471d411de2062e4504f8a07b394b284d377578
Jozsef Kiraly <fonix232@gmail.com>
2017-07-13 14:49:07 +0200

    Remove initial analytics, moved to Launcher.java

--- src/ch/deletescape/lawnchair/App.java
+++ src/ch/deletescape/lawnchair/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if(!BuildConfig.MOBILE_CENTER_KEY.equalsIgnoreCase("null"))
-            MobileCenter.start(this, BuildConfig.MOBILE_CENTER_KEY, Analytics.class, Crashes.class, Distribute.class);
+        //if(!BuildConfig.MOBILE_CENTER_KEY.equalsIgnoreCase("null"))
+            //MobileCenter.start(this, BuildConfig.MOBILE_CENTER_KEY, Analytics.class, Crashes.class, Distribute.class);
     }
 }
