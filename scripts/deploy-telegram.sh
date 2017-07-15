3d7fe8a8758a23a9038db94a226793c0174f16b1
paphonb <paphonb@gmail.com>
2017-07-16 00:25:12 +0700

    Fix build error

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-cp build/outputs/apk/aosp/debug/Lawnchair-aosp-debug.apk Lawnchair-$TRAVIS_BUILD_NUMBER.apk
-cp build/outputs/mapping/aosp/debug/mapping.txt proguard-$TRAVIS_BUILD_NUMBER.txt
+cp app/build/outputs/apk/debug/app-debug.apk Lawnchair-$TRAVIS_BUILD_NUMBER.apk
+cp app/build/outputs/mapping/aosp/debug/mapping.txt proguard-$TRAVIS_BUILD_NUMBER.txt
 
 curl -F chat_id="-1001083653933" -F document=@"Lawnchair-$TRAVIS_BUILD_NUMBER.apk" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 curl -F chat_id="-1001083653933" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
