a459fb163b180acda8a8538c22c657675bcd9a21
paphonb <paphonb@gmail.com>
2017-07-24 18:48:37 +0700

    Hide google wallpaper and voice search from all apps

--- app/src/main/java/ch/deletescape/lawnchair/StringSetAppFilter.java
+++ app/src/main/java/ch/deletescape/lawnchair/StringSetAppFilter.java
@@ -2,14 +2,21 @@ package ch.deletescape.lawnchair;
 
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.SharedPreferences;
 
-public class StringSetAppFilter implements AppFilter {
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSetAppFilter extends PreferenceAppFilter {
+
+    private List<String> mHiddenPackages = new ArrayList<>();
+
+    StringSetAppFilter() {
+        mHiddenPackages.add("com.google.android.apps.wallpaper");
+        mHiddenPackages.add("com.google.android.googlequicksearchbox");
+    }
+
     @Override
     public boolean shouldShowApp(ComponentName app, Context context) {
-        if (app.getPackageName().equals(context.getPackageName()))
-            return false;
-        SharedPreferences prefs = Utilities.getPrefs(context);
-        return prefs.getBoolean("pref_showHidden", false) || !Utilities.isAppHidden(context, app.flattenToString());
+        return !mHiddenPackages.contains(app.getPackageName()) && super.shouldShowApp(app, context);
     }
 }
