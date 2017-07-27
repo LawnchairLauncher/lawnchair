b0ca054c933ca77952aed678fc10b72a2379acc4
Hassie <hassy.dev@gmail.com>
2017-07-27 12:25:26 +0100

    Cleanup imports
    Cleaned up imports causing build failure.

--- app/src/main/java/ch/deletescape/lawnchair/QsbBlockerView.java
+++ app/src/main/java/ch/deletescape/lawnchair/QsbBlockerView.java
@@ -16,9 +16,6 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
-import com.kwabenaberko.openweathermaplib.implementation.OpenWeatherMapHelper;
-import com.kwabenaberko.openweathermaplib.models.CurrentWeather;
-
 import ch.deletescape.lawnchair.config.FeatureFlags;
 import ch.deletescape.lawnchair.pixelify.OnWeatherInfoListener;
 import ch.deletescape.lawnchair.pixelify.ShadowHostView;
