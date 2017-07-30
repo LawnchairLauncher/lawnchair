38e31996cf118efac0eeedf560589061f7207f45
paphonb <paphonb@gmail.com>
2017-07-30 18:27:24 +0700

    Cleanup BitSetPreference

--- app/src/main/java/ch/deletescape/lawnchair/preferences/BitSetPreference.kt
+++ app/src/main/java/ch/deletescape/lawnchair/preferences/BitSetPreference.kt
@@ -20,27 +20,19 @@ class BitSetPreference(context: Context?, attrs: AttributeSet?) :
 
     override fun getPersistedStringSet(defaultReturnValue: MutableSet<String>?): MutableSet<String> {
         val bits = persistedInt
-        val newValues = HashSet<String>()
-        entryValues.forEach { it ->
-            it as String
-            val bit = Integer.parseInt(it)
-            if (bits and bit > 0) {
-                newValues.add(it)
-            }
-        }
-        return newValues
+        return entryValues.fold(HashSet<String>(), {acc, i -> acc.apply {
+            i as String
+            if (bits and Integer.parseInt(i) != 0)
+                acc += i
+        }})
     }
 
     fun updateSummary() {
         val bits = persistedInt
-        val newEntries = HashSet<String>()
-        entryValues.forEachIndexed { index, it ->
-            it as String
-            val bit = Integer.parseInt(it)
-            if (bits and bit > 0) {
-                newEntries.add(entries[index] as String)
-            }
-        }
+        val newEntries = entryValues.foldIndexed(HashSet<String>(), {index, acc, i -> acc.apply {
+            if (bits and Integer.parseInt(i as String) != 0)
+                acc += entries[index] as String
+        }})
         if (newEntries.size > 0) {
             summary = TextUtils.join(", ", newEntries)
         } else {
@@ -50,12 +42,7 @@ class BitSetPreference(context: Context?, attrs: AttributeSet?) :
 
     override fun persistStringSet(values: MutableSet<String>?): Boolean {
         if (values == null) return false
-        var bits = 0
-        values.forEach {
-            val bit = Integer.parseInt(it)
-            bits = bits or bit
-        }
-        persistedInt = bits
+        persistedInt = values.fold(0, {acc, i -> acc or Integer.parseInt(i)})
         updateSummary()
         return true
     }
