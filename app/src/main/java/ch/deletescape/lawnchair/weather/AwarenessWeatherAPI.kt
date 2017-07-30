e361ba2ff9b9f45cf2194cf3ec414fea8ff702f0
paphonb <paphonb@gmail.com>
2017-07-30 18:05:52 +0700

    Improve awareness api weather icon

--- app/src/main/java/ch/deletescape/lawnchair/weather/AwarenessWeatherAPI.kt
+++ app/src/main/java/ch/deletescape/lawnchair/weather/AwarenessWeatherAPI.kt
@@ -41,9 +41,7 @@ class AwarenessWeatherAPI(context: Context) : WeatherAPI(), ResultCallback<Weath
                         Units.IMPERIAL -> Weather.FAHRENHEIT
                     }
             )
-            val icon = getWeatherIcon(weatherResult.weather.conditions[0])
-            //Log.d("AWAPI", "condition: " + Arrays.toString(weatherResult.weather.conditions))
-            //val icon = "04d"
+            val icon = getWeatherIcon(weatherResult.weather.conditions)
             onWeatherData(WeatherData(
                     success = true,
                     temp = temp.toInt(),
@@ -58,18 +56,48 @@ class AwarenessWeatherAPI(context: Context) : WeatherAPI(), ResultCallback<Weath
         }
     }
 
-    private fun getWeatherIcon(condition: Int): String {
-        return when (condition) {
-            Weather.CONDITION_CLEAR -> WeatherIconProvider.CONDITION_FEW_CLOUDS
-            Weather.CONDITION_CLOUDY -> WeatherIconProvider.CONDITION_MOST_CLOUDS
-            Weather.CONDITION_FOGGY -> WeatherIconProvider.CONDITION_MIST
-            Weather.CONDITION_HAZY -> WeatherIconProvider.CONDITION_MIST
-            Weather.CONDITION_ICY -> WeatherIconProvider.CONDITION_SNOW
-            Weather.CONDITION_RAINY -> WeatherIconProvider.CONDITION_SHOWERS
-            Weather.CONDITION_SNOWY -> WeatherIconProvider.CONDITION_SNOW
-            Weather.CONDITION_STORMY -> WeatherIconProvider.CONDITION_STORM
-            Weather.CONDITION_WINDY -> WeatherIconProvider.CONDITION_CLOUDS
-            else -> WeatherIconProvider.CONDITION_UNKNOWN
-        }
+    private fun getWeatherIcon(condition: IntArray): String {
+        val conditions = condition.fold(0) { acc, i -> acc or CONDITIONS[i]}
+        if (conditions and CONDITION_STORMY != 0)
+            return WeatherIconProvider.CONDITION_STORM
+        if (conditions and CONDITION_SNOWY != 0 || conditions and CONDITION_ICY != 0)
+            return WeatherIconProvider.CONDITION_SNOW
+        if (conditions and CONDITION_RAINY != 0)
+            return WeatherIconProvider.CONDITION_SHOWERS
+        if (conditions and CONDITION_FOGGY != 0 || conditions and CONDITION_HAZY != 0)
+            return WeatherIconProvider.CONDITION_MIST
+        if (conditions and CONDITION_CLOUDY != 0 && conditions and CONDITION_CLEAR != 0)
+            return WeatherIconProvider.CONDITION_CLOUDS
+        if (conditions and CONDITION_CLOUDY != 0)
+            return WeatherIconProvider.CONDITION_MOST_CLOUDS
+        if (conditions and CONDITION_CLEAR != 0)
+            return WeatherIconProvider.CONDITION_FEW_CLOUDS
+        return WeatherIconProvider.CONDITION_UNKNOWN
+    }
+
+    companion object {
+        private const val CONDITION_UNKNOWN = 1 shl 0
+        private const val CONDITION_CLEAR = 1 shl 1
+        private const val CONDITION_CLOUDY = 1 shl 2
+        private const val CONDITION_FOGGY = 1 shl 3
+        private const val CONDITION_HAZY = 1 shl 4
+        private const val CONDITION_ICY = 1 shl 5
+        private const val CONDITION_RAINY = 1 shl 6
+        private const val CONDITION_SNOWY = 1 shl 7
+        private const val CONDITION_STORMY = 1 shl 8
+        private const val CONDITION_WINDY = 1 shl 9
+
+        private val CONDITIONS = intArrayOf(
+                AwarenessWeatherAPI.CONDITION_UNKNOWN,
+                AwarenessWeatherAPI.CONDITION_CLEAR,
+                AwarenessWeatherAPI.CONDITION_CLOUDY,
+                AwarenessWeatherAPI.CONDITION_FOGGY,
+                AwarenessWeatherAPI.CONDITION_HAZY,
+                AwarenessWeatherAPI.CONDITION_ICY,
+                AwarenessWeatherAPI.CONDITION_RAINY,
+                AwarenessWeatherAPI.CONDITION_SNOWY,
+                AwarenessWeatherAPI.CONDITION_STORMY,
+                AwarenessWeatherAPI.CONDITION_WINDY
+        )
     }
 }
