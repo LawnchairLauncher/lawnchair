62d0ba397dce94776038344a8ad06865401d7c2e
deletescape <deletescape@outlook.com>
2017-08-07 13:11:24 +0200

    Third attempt to fix LG nav bar background

--- app/src/main/java/ch/deletescape/lawnchair/Launcher.java
+++ app/src/main/java/ch/deletescape/lawnchair/Launcher.java
@@ -47,6 +47,7 @@ import android.content.res.Configuration;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
@@ -366,7 +367,7 @@ public class Launcher extends Activity
 
         setScreenOrientation();
 
-        if(!BuildConfig.MOBILE_CENTER_KEY.equalsIgnoreCase("null"))
+        if (!BuildConfig.MOBILE_CENTER_KEY.equalsIgnoreCase("null"))
             MobileCenter.start(getApplication(), BuildConfig.MOBILE_CENTER_KEY, Analytics.class, Crashes.class, Distribute.class);
 
         LauncherAppState app = LauncherAppState.getInstance();
@@ -436,13 +437,17 @@ public class Launcher extends Activity
             mSharedPrefs.edit().putBoolean("requiresIconCacheReload", false).apply();
             reloadIcons();
         }
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+
         Settings.init(this);
 
         Utilities.showChangelog(this);
     }
 
     private void setScreenOrientation() {
-        if(FeatureFlags.INSTANCE.enableScreenRotation(this)) {
+        if (FeatureFlags.INSTANCE.enableScreenRotation(this)) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         } else {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
