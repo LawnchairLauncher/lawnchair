1bf8b093d60b4fd3055c0933b119369590a821e7
deletescape <deletescape@outlook.com>
2017-08-07 14:08:56 +0200

    4th attempt

--- app/src/main/java/ch/deletescape/lawnchair/Launcher.java
+++ app/src/main/java/ch/deletescape/lawnchair/Launcher.java
@@ -73,6 +73,8 @@ import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityManager;
 import android.view.animation.OvershootInterpolator;
@@ -438,8 +440,13 @@ public class Launcher extends Activity
             reloadIcons();
         }
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        Window window = getWindow();
+        WindowManager.LayoutParams attributes = window.getAttributes();
+        attributes.systemUiVisibility |= 1792;
+        window.clearFlags(201326592);
+        window.addFlags(Integer.MIN_VALUE);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.setNavigationBarColor(Color.TRANSPARENT);
 
         Settings.init(this);
 
