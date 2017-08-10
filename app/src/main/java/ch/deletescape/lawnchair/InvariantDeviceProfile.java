f2992d81161d2c341592944bc214e33a71e80433
deletescape <deletescape@outlook.com>
2017-08-10 13:09:44 +0200

    Directly apply reverting grid customizations to default

--- app/src/main/java/ch/deletescape/lawnchair/InvariantDeviceProfile.java
+++ app/src/main/java/ch/deletescape/lawnchair/InvariantDeviceProfile.java
@@ -83,6 +83,7 @@ public class InvariantDeviceProfile {
      * Number of icons inside the hotseat area.
      */
     public int numHotseatIcons;
+    public int numHotseatIconsOriginal;
     float hotseatIconSize;
     float hotseatIconSizeOriginal;
     int defaultLayoutId;
@@ -144,8 +145,9 @@ public class InvariantDeviceProfile {
         numRowsOriginal = numRows;
         numColumns = closestProfile.numColumns;
         numColumnsOriginal = numColumns;
-        numColumnsDrawer = closestProfile.numColumns;
+        numColumnsDrawer = numColumns;
         numHotseatIcons = closestProfile.numHotseatIcons;
+        numHotseatIconsOriginal = numHotseatIcons;
         defaultLayoutId = closestProfile.defaultLayoutId;
         numFolderRows = closestProfile.numFolderRows;
         numFolderColumns = closestProfile.numFolderColumns;
@@ -201,15 +203,23 @@ public class InvariantDeviceProfile {
         String valueDefault = "default";
         if (!prefs.getString("pref_numRows", valueDefault).equals(valueDefault)) {
             numRows = Integer.valueOf(prefs.getString("pref_numRows", ""));
+        } else {
+            numRows = numRowsOriginal;
         }
         if (!prefs.getString("pref_numCols", valueDefault).equals(valueDefault)) {
             numColumns = Integer.valueOf(prefs.getString("pref_numCols", ""));
+        } else {
+            numColumns = numColumnsOriginal;
         }
         if (!prefs.getString("pref_numColsDrawer", valueDefault).equals(valueDefault)) {
             numColumnsDrawer = Integer.valueOf(prefs.getString("pref_numColsDrawer", ""));
+        } else {
+            numColumnsDrawer = numColumnsOriginal;
         }
         if (!prefs.getString("pref_numHotseatIcons", valueDefault).equals(valueDefault)) {
             numHotseatIcons = Integer.valueOf(prefs.getString("pref_numHotseatIcons", ""));
+        } else {
+            numHotseatIcons = numHotseatIconsOriginal;
         }
         if (prefs.getFloat("pref_iconScaleSB", 1f) != 1f) {
             float iconScale = prefs.getFloat("pref_iconScaleSB", 1f);
