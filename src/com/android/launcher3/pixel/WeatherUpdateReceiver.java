5d0ade58d5e3c52d88fe2506809d05e7c117812f
Amir Zaidi <azaidi@live.nl>
2017-08-22 21:18:41 +0200

    Add permissions for Pixel QSB

---
+++ src/com/android/launcher3/pixel/WeatherUpdateReceiver.java
@@ -0,0 +1,13 @@
+package com.android.launcher3.pixel;
+
+import android.widget.RemoteViews;
+import android.content.Intent;
+import android.content.Context;
+import android.content.BroadcastReceiver;
+
+public class WeatherUpdateReceiver extends BroadcastReceiver
+{
+    public void onReceive(final Context context, final Intent intent) {
+
+    }
+}
