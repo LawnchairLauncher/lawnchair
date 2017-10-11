1067665ace0b4a9681bcfe5a17b49a96f149c527
David Sn <divad.nnamtdeis@gmail.com>
2017-10-11 22:37:38 +0200

    Implement a better solution without additional libraries

--- app/src/main/java/ch/deletescape/lawnchair/util/UnicodeFilter.java
+++ app/src/main/java/ch/deletescape/lawnchair/util/UnicodeFilter.java
@@ -1,34 +1,31 @@
 package ch.deletescape.lawnchair.util;
 
-import org.apache.commons.lang3.StringUtils;
-
 import java.text.Normalizer;
 import java.util.regex.Pattern;
 
 /**
  * Attempts to replace any non-ascii characters to theirs ascii equivalents
+ * by normalizing the character into Unicode NFD form and stripping out diacritic mark characters.
  */
 public class UnicodeFilter {
     private static final Pattern DIACRITICS_PATTERN =
-            Pattern.compile("\\p{InCombiningDiacriticalMarks}");
+            Pattern.compile("\\\\p{InCombiningDiacriticalMarks}+");
 
     public static String filter(String source) {
-        StringBuilder output = new StringBuilder();
-        final int sourceLength = source.length();
+        StringBuilder output = new StringBuilder(Normalizer.normalize(source, Normalizer.Form.NFD));
 
-        for (int i = 0; i < sourceLength; i++) {
-            String s = String.valueOf(source.charAt(i));
-
-            // Try normalizing the character into Unicode NFKD form and
-            // stripping out diacritic mark characters.
-            s = Normalizer.normalize(s, Normalizer.Form.NFKD);
-            s = DIACRITICS_PATTERN.matcher(s).replaceAll("");
-
-            // Replace left over accents away using Apache's StringUtils
-            s = StringUtils.stripAccents(s);
-            output.append(s);
+        // Special case characters that don't get stripped by the above technique.
+        for (int i = 0; i < output.length(); i++) {
+            if (output.charAt(i) == '\u0141') {
+                output.deleteCharAt(i);
+                output.insert(i, 'L');
+            } else if (output.charAt(i) == '\u0142') {
+                output.deleteCharAt(i);
+                output.insert(i, 'l');
+            }
         }
 
-        return output.toString();
+        // Note that ligatures will be left as is
+        return DIACRITICS_PATTERN.matcher(output).replaceAll("");
     }
 }
