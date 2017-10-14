25ea03f8f700a1dd79b4c129b2342606f2d2b671
David Sn <divad.nnamtdeis@gmail.com>
2017-10-14 17:18:22 +0200

    Improve codacy quality score

--- scripts/s3-upload.sh
+++ scripts/s3-upload.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 # Upload files to custom S3 using curl
 # Example: ./s3-upload.sh play.minio.io example.txt bucket Q3AM3UQ867SPQQA43P2F zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG
 
@@ -7,13 +8,13 @@ FILE=$1
 BUCKET=$2
 RESOURCE="/${BUCKET}/${FILE}"
 CONTENT_TYPE=$(file --mime-type -b $FILE)
-TIMESTAMP=`date -R`
+TIMESTAMP=$(date -R)
 STRING_TO_SIGN="PUT\n\n${CONTENT_TYPE}\n${TIMESTAMP}\n${RESOURCE}"
 
 S3_KEY=$4
 S3_SECRET=$5
 
-SIGNATURE=`echo -n ${STRING_TO_SIGN} | openssl sha1 -hmac ${S3_SECRET} -binary | base64`
+SIGNATURE=$(echo -n ${STRING_TO_SIGN} | openssl sha1 -hmac ${S3_SECRET} -binary | base64)
 curl -X PUT -T "${FILE}" \
     -H "Host: ${HOST}" \
     -H "Date: ${TIMESTAMP}" \
