12b6dda90c4aa883c3de02f1986e6b83e2e300bc
David Sn <divad.nnamtdeis@gmail.com>
2017-10-14 17:00:55 +0200

    Fix derped lines

--- scripts/deploy-s3.sh
+++ scripts/deploy-s3.sh
@@ -7,11 +7,11 @@
 
 # Upload Lawnchair signed release apk
 cp app/release/app-release.apk Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk
-./scripts/s3-upload.sh Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.md5sum $S3_BUCKET $S3_HOST $S3_KEY $S3_SECRET
+./scripts/s3-upload.sh Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk $S3_BUCKET $S3_HOST $S3_KEY $S3_SECRET
 
 # Upload calculated md5 checksum for app-release.apk
 md5sum $(readlink -f app/release/app-release.apk) > Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.md5sum
-./scripts/s3-upload.sh Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk $S3_BUCKET $S3_HOST $S3_KEY $S3_SECRET
+./scripts/s3-upload.sh Lawnchair-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.md5sum $S3_BUCKET $S3_HOST $S3_KEY $S3_SECRET
 
 # Upload Lawnfeed signed debug apk
 cp lawnfeed/build/outputs/apk/debug/lawnfeed-debug.apk Lawnfeed-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.apk
