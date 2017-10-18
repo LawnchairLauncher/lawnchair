d80ca3a4376c0b7ceb376402c08611205512407b
David Sn <divad.nnamtdeis@gmail.com>
2017-10-19 00:37:29 +0200

    Fix derp in proguard file deployment
    [skip ci]

--- scripts/deploy-telegram.sh
+++ scripts/deploy-telegram.sh
@@ -7,5 +7,5 @@ curl -F chat_id="-1001083653933" -F document=@"Lawnchair-$MAJOR_MINOR.$TRAVIS_BU
 curl -F chat_id="-1001083653933" -F text="$(./scripts/changelog.sh)" -F parse_mode="HTML" https://api.telegram.org/bot$BOT_TOKEN/sendMessage
 curl -F chat_id="442800997" -F document=@"proguard-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 
-cp app/build/outputs/mapping/release/mapping.txt proguard-$MAJOR_MINOR.$TRAVIS_BUILD_NUMBER_plah.txt
+cp app/build/outputs/mapping/release/mapping.txt proguard-$MAJOR_MINOR.${TRAVIS_BUILD_NUMBER}_plah.txt
 curl -F chat_id="442800997" -F document=@"proguard-$MAJOR_MINOR.${TRAVIS_BUILD_NUMBER}_plah.txt" https://api.telegram.org/bot$BOT_TOKEN/sendDocument
