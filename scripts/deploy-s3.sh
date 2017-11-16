3fd1194cefd96720b5cbe255fa01d7afe6610e5d
David Sn <divad.nnamtdeis@gmail.com>
2017-11-16 11:44:37 +0100

    Fix not valid json when uploading version.json

--- scripts/deploy-s3.sh
+++ scripts/deploy-s3.sh
@@ -29,5 +29,5 @@ md5sum $(readlink -f lawnfeed/build/outputs/apk/debug/lawnfeed-debug.apk) > $APP
 ./scripts/s3-upload.sh $APP_VERSION/Lawnfeed-$APP_VERSION.md5sum $S3_BUCKET $S3_HOST $S3_KEY $S3_SECRET
 
 # Upload version info
-echo -e "{\"major_minor\":\""$MAJOR_MINOR"\", \"travis_build_number\":\""$TRAVIS_BUILD_NUMBER"\", \"app_version\":\""$APP_VERSION"\"}" > version.json
+echo "{\"major_minor\": \""$MAJOR_MINOR"\", \"travis_build_number\": \""$TRAVIS_BUILD_NUMBER"\", \"app_version\": \""$APP_VERSION"\"}" > version.json
 ./scripts/s3-upload.sh version.json $S3_BUCKET $S3_HOST $S3_KEY $S3_SECRET
