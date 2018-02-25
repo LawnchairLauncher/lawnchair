f7e33bd2d56d2326bc0c335d0c845d633ff53002
David Sn <divad.nnamtdeis@gmail.com>
2018-02-25 22:46:03 +0100

    Remove Telegram deploy script

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-MERGE_PREFIX="Merge pull request"
 
 if [ ! -z "$TRAVIS_TAG" ]
 then
@@ -7,10 +6,7 @@ then
 fi
 
 GIT_COMMIT_LOG="$(git log --format='%s (by %cn)' $TRAVIS_COMMIT_RANGE)"
-
-echo " <b>Changelog for build ${MAJOR_MINOR}.${TRAVIS_BUILD_NUMBER}</b>${NEWLINE}"
-
 printf '%s\n' "$GIT_COMMIT_LOG" | while IFS= read -r line
 do
-  echo "- ${line}"
+    echo "- ${line}"
 done
