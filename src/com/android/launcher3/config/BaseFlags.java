bd8c31126c3ac05bc3c46bed3a0f1b5203b1d055
paphonb <paphonb@gmail.com>
2018-03-02 11:40:27 +0700

    Hello drone

--- src/com/android/launcher3/config/BaseFlags.java
+++ src/com/android/launcher3/config/BaseFlags.java
@@ -49,7 +49,7 @@ abstract class BaseFlags {
     public static final boolean LAUNCHER3_SPRING_ICONS = true;
 
     // Feature flag to enable moving the QSB on the 0th screen of the workspace.
-    public static final boolean QSB_ON_FIRST_SCREEN = true;
+    public static boolean QSB_ON_FIRST_SCREEN = true;
     // When enabled the all-apps icon is not added to the hotseat.
     public static final boolean NO_ALL_APPS_ICON = true;
     // When enabled fling down gesture on the first workspace triggers search.
