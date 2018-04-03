47313138edd6c3d0952a08f98501d3a501177d24
Amir Zaidi <azaidi@live.nl>
2018-04-03 19:52:40 +0200

    Hardcode text color to always match AOSP

--- src/com/google/android/apps/nexuslauncher/qsb/AllAppsQsbLayout.java
+++ src/com/google/android/apps/nexuslauncher/qsb/AllAppsQsbLayout.java
@@ -177,7 +177,7 @@ public class AllAppsQsbLayout extends AbstractQsbLayout implements SearchUiManag
     }
 
     public void onExtractedColorsChanged(final WallpaperColorInfo wallpaperColorInfo) {
-        int color = Themes.getAttrBoolean(mActivity, R.attr.isMainColorDark) ? 0xEBFFFFFE : 0xCCFFFFFE;
+        int color = getResources().getColor(Themes.getAttrBoolean(mActivity, R.attr.isMainColorDark) ? R.color.qsb_background_drawer_dark : R.color.qsb_background_drawer_default);
         bz(ColorUtils.compositeColors(ColorUtils.compositeColors(color, Themes.getAttrColor(mActivity, R.attr.allAppsScrimColor)), wallpaperColorInfo.getMainColor()));
     }
 
