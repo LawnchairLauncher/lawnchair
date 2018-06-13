36f4e87843a1a5d1ee73034dcdf80ba2b762dbd1
paphonb <paphonb@gmail.com>
2018-06-13 20:33:27 +0700

    Fix Lawnchair icon visible in app drawer

--- src/ch/deletescape/lawnchair/LawnchairAppFilter.kt
+++ src/ch/deletescape/lawnchair/LawnchairAppFilter.kt
@@ -4,14 +4,13 @@ import android.content.ComponentName
 import android.content.Context
 import android.os.UserHandle
 import com.google.android.apps.nexuslauncher.NexusAppFilter
-import com.google.android.apps.nexuslauncher.NexusLauncherActivity
 
 open class LawnchairAppFilter(context: Context) : NexusAppFilter(context) {
 
     private val hideList = HashSet<ComponentName>()
 
     init {
-        hideList.add(ComponentName(context, NexusLauncherActivity::class.java.name))
+        hideList.add(ComponentName(context, LawnchairLauncher::class.java.name))
     }
 
     override fun shouldShowApp(componentName: ComponentName?, user: UserHandle?): Boolean {
