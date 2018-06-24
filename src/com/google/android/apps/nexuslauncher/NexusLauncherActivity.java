d080575c1d804d54cc305bf7359eb427288be179
paphonb <paphonb@gmail.com>
2018-06-24 21:37:29 +0700

    Don't show workspace when recreating

--- src/com/google/android/apps/nexuslauncher/NexusLauncherActivity.java
+++ src/com/google/android/apps/nexuslauncher/NexusLauncherActivity.java
@@ -1,24 +1,18 @@
 package com.google.android.apps.nexuslauncher;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
-
+import ch.deletescape.lawnchair.settings.ui.SettingsActivity;
 import com.android.launcher3.AppInfo;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.Utilities;
 import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.util.ComponentKeyMapper;
-import com.android.launcher3.util.ViewOnDrawExecutor;
 import com.google.android.libraries.gsa.launcherclient.LauncherClient;
 
 import java.util.List;
 
-import ch.deletescape.lawnchair.settings.ui.SettingsActivity;
-
 public class NexusLauncherActivity extends Launcher {
-    private final static String PREF_IS_RELOAD = "pref_reload_workspace";
     private NexusLauncher mLauncher;
-    private boolean mIsReload;
 
     public NexusLauncherActivity() {
         mLauncher = new NexusLauncher(this);
@@ -28,35 +22,18 @@ public class NexusLauncherActivity extends Launcher {
     public void onCreate(Bundle savedInstanceState) {
         FeatureFlags.QSB_ON_FIRST_SCREEN = showSmartspace();
         super.onCreate(savedInstanceState);
-
-        SharedPreferences prefs = Utilities.getPrefs(this);
-        if (mIsReload = prefs.getBoolean(PREF_IS_RELOAD, false)) {
-            prefs.edit().remove(PREF_IS_RELOAD).apply();
-            getWorkspace().setCurrentPage(0);
-            showOverviewMode(false);
-        }
     }
 
     @Override
     public void onStart() {
         super.onStart();
         if (FeatureFlags.QSB_ON_FIRST_SCREEN != showSmartspace()) {
-            Utilities.getPrefs(this).edit().putBoolean(PREF_IS_RELOAD, true).apply();
             if (Utilities.ATLEAST_NOUGAT) {
                 recreate();
             } else {
                 finish();
                 startActivity(getIntent());
             }
-        }
-    }
-
-    @Override
-    public void clearPendingExecutor(ViewOnDrawExecutor executor) {
-        super.clearPendingExecutor(executor);
-        if (mIsReload) {
-            mIsReload = false;
-            showOverviewMode(false);
         }
     }
 
