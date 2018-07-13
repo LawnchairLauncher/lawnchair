8a694000460ff7cbcc9aae935c9352c300dbfce9
paphonb <paphonb@gmail.com>
2018-07-13 20:27:21 +0700

    Remove onPreferenceChangeListener when detached

--- src/ch/deletescape/lawnchair/preferences/OWMEditTextPreference.kt
+++ src/ch/deletescape/lawnchair/preferences/OWMEditTextPreference.kt
@@ -2,7 +2,6 @@ package ch.deletescape.lawnchair.preferences
 
 import android.content.Context
 import android.support.v7.preference.EditTextPreference
-import android.support.v7.preference.PreferenceManager
 import android.text.TextUtils
 import android.util.AttributeSet
 import ch.deletescape.lawnchair.LawnchairPreferences
@@ -13,10 +12,17 @@ class OWMEditTextPreference(context: Context, attrs: AttributeSet?) : EditTextPr
     private val isApi = key == "pref_weatherApiKey"
     private val prefs = Utilities.getLawnchairPrefs(context)
 
-    override fun onAttachedToHierarchy(preferenceManager: PreferenceManager?) {
-        super.onAttachedToHierarchy(preferenceManager)
+    override fun onAttached() {
+        super.onAttached()
+
         updateSummary()
         prefs.addOnPreferenceChangeListener("pref_smartspace_widget_provider", this)
+    }
+
+    override fun onDetached() {
+        super.onDetached()
+
+        prefs.removeOnPreferenceChangeListener("pref_smartspace_widget_provider", this)
     }
 
     override fun persistString(value: String?): Boolean {
@@ -32,10 +38,10 @@ class OWMEditTextPreference(context: Context, attrs: AttributeSet?) : EditTextPr
     }
 
     private fun updateSummary() {
-        if (isApi){
-            summary = prefs.weatherApiKey.replace("[\\d\\w]".toRegex(), "*")
+        summary = if (isApi) {
+            prefs.weatherApiKey.replace("[\\d\\w]".toRegex(), "*")
         } else {
-            summary = prefs.weatherCity
+            prefs.weatherCity
         }
     }
 
