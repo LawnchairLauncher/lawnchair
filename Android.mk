dedecd8cf370f314bad6e25dcf5b6879b25b7bc3
Rajeev Kumar <rajekumar@google.com>
2018-08-06 14:32:26 -0700

    Specify proguard.flags file explicitly in the Launcher3QuickStepGo make rule
    This change makes sure that following build command doesn't error out:
    tapas Launcher3QuickStepGo
    make -j45
    
    Bug: 112274857
    Test: Built source with tapas Launcher3QuickStepGo and it built successfully.
    Change-Id: Icd788e4e119efd386bec6646627c2d9dd485af60

--- Android.mk
+++ Android.mk
@@ -193,6 +193,7 @@ LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/quickstep/res \
     $(LOCAL_PATH)/go/res
 
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_PROGUARD_ENABLED := full
 
 LOCAL_SDK_VERSION := system_current
