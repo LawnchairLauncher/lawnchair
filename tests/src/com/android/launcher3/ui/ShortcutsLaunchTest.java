dc755e718ded4a3af112b63420ea1060618a2982
Vadim Tryshev <vadimt@google.com>
2018-08-10 20:01:24 -0700

    Started converting Launcher to be gesture-stable
    All tests keep passing.
    
    WorkTabTest.workTabExists was made gesture-stable. There is no need to
    use TAPL, as the point is not to check answering to gestures, but to
    check presence of tabs, which is better to do using launcher internal
    state. (It still fails if run as a part of all tests, presumably,
    because after Launcher's start, changes in user configuration are not
    recognized, so I've commented the failing part)
    
    Bug: 110103162
    Test: Run all tests
    Change-Id: Ic30b8e8475d16cee3880332f12311a44ddfa26cb

--- tests/src/com/android/launcher3/ui/ShortcutsLaunchTest.java
+++ tests/src/com/android/launcher3/ui/ShortcutsLaunchTest.java
@@ -1,5 +1,8 @@
 package com.android.launcher3.ui;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import android.content.pm.LauncherActivityInfo;
 import android.graphics.Point;
 import android.support.test.filters.LargeTest;
@@ -12,15 +15,11 @@ import android.view.MotionEvent;
 import com.android.launcher3.R;
 import com.android.launcher3.util.Condition;
 import com.android.launcher3.util.Wait;
-import com.android.launcher3.util.rule.LauncherActivityRule;
 import com.android.launcher3.util.rule.ShellCommandRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Test for verifying that shortcuts are shown and can be launched after long pressing an app
@@ -29,7 +28,6 @@ import static org.junit.Assert.assertTrue;
 @RunWith(AndroidJUnit4.class)
 public class ShortcutsLaunchTest extends AbstractLauncherUiTest {
 
-    @Rule public LauncherActivityRule mActivityMonitor = new LauncherActivityRule();
     @Rule public ShellCommandRule mDefaultLauncherRule = ShellCommandRule.setDefaultLauncher();
 
     @Test
