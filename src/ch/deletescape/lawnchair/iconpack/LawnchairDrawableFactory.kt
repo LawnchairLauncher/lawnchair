79799fdbb0a589d69d460fb3d28222404470e953
paphonb <paphonb@gmail.com>
2018-08-14 23:10:58 +0700

    Fix per-shortcut custom icon

--- src/ch/deletescape/lawnchair/iconpack/LawnchairDrawableFactory.kt
+++ src/ch/deletescape/lawnchair/iconpack/LawnchairDrawableFactory.kt
@@ -6,6 +6,7 @@ import android.graphics.Bitmap
 import com.android.launcher3.FastBitmapDrawable
 import com.android.launcher3.ItemInfo
 import com.android.launcher3.ItemInfoWithIcon
+import com.android.launcher3.ShortcutInfo
 import com.android.launcher3.graphics.BitmapInfo
 import com.google.android.apps.nexuslauncher.DynamicDrawableFactory
 import com.google.android.apps.nexuslauncher.clock.CustomClock
@@ -16,6 +17,6 @@ class LawnchairDrawableFactory(context: Context) : DynamicDrawableFactory(contex
     val customClockDrawer = CustomClock(context)
 
     override fun newIcon(info: ItemInfoWithIcon): FastBitmapDrawable {
-        return iconPackManager.newIcon(info.iconBitmap, info, this)
+        return iconPackManager.newIcon((info as? ShortcutInfo)?.customIcon ?: info.iconBitmap, info, this)
     }
 }
