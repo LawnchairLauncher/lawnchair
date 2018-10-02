a2dc3c72119c734550caf053fc559ac84987df46
Vadim Tryshev <vadimt@google.com>
2018-10-01 17:28:53 -0700

    Disabling ShortcutsToHomeTest tests
    The bug below is for re-enabling disabled tests.
    
    For now, we'd benefit more from enabling presubmits.
    
    Bug: 117106893
    Test: will see on TAP
    Change-Id: I322deb9a64423587b329004f6cab4da1ff4f197d

--- tests/src/com/android/launcher3/ui/ShortcutsToHomeTest.java
+++ tests/src/com/android/launcher3/ui/ShortcutsToHomeTest.java
@@ -17,6 +17,7 @@ import com.android.launcher3.util.Condition;
 import com.android.launcher3.util.Wait;
 import com.android.launcher3.util.rule.ShellCommandRule;
 
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,12 +32,14 @@ public class ShortcutsToHomeTest extends AbstractLauncherUiTest {
     @Rule public ShellCommandRule mDefaultLauncherRule = ShellCommandRule.setDefaultLauncher();
 
     @Test
+    @Ignore
     public void testDragIcon_portrait() throws Throwable {
         lockRotation(true);
         performTest();
     }
 
     @Test
+    @Ignore
     public void testDragIcon_landscape() throws Throwable {
         lockRotation(false);
         performTest();
