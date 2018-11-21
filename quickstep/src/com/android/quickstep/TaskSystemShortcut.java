0072844e7fa68b4ff1daecf2c9a1cc1686f3aec0
lumark <lumark@google.com>
2018-11-21 21:05:31 +0800

    Modify TaskSystemShortcut.SplitScreen for API change.
    Using View#getDisplay().getDisplayId() into
    IWindowManager.overridePendingAppTransitionMultiThumbFuture()
    for multi-display support.
    
    Bug: 119530958
    Test: manual
    Change-Id: Icde73b2545f879771a62030c8151a86a324fa593
    (cherry picked from commit 233622d907e3c250abd25c3a616f360809850683)

--- quickstep/src/com/android/quickstep/TaskSystemShortcut.java
+++ quickstep/src/com/android/quickstep/TaskSystemShortcut.java
@@ -203,7 +203,8 @@ public class TaskSystemShortcut<T extends SystemShortcut> extends SystemShortcut
                         }
                     };
                     WindowManagerWrapper.getInstance().overridePendingAppTransitionMultiThumbFuture(
-                            future, animStartedListener, mHandler, true /* scaleUp */);
+                            future, animStartedListener, mHandler, true /* scaleUp */,
+                            v.getDisplay().getDisplayId());
                 }
             });
         }
