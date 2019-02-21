077c29e091eda63771bea79359f739e94b586870
Matthew Ng <ngmatthew@google.com>
2019-02-20 16:43:12 -0800

    Added assistant gesture swipe up right side region on nav bar
    Bug: 112934365
    Test: manual
    Change-Id: I467e71f13da4afba3a556fafb0bfdf01650ab4bf

--- src/com/android/launcher3/config/BaseFlags.java
+++ src/com/android/launcher3/config/BaseFlags.java
@@ -112,6 +112,10 @@ abstract class BaseFlags {
             "ENABLE_HINTS_IN_OVERVIEW", false,
             "Show chip hints and gleams on the overview screen");
 
+    public static final TogglableFlag ENABLE_ASSISTANT_GESTURE = new TogglableFlag(
+            "ENABLE_ASSISTANT_GESTURE", false,
+            "Enable swipe up from the bottom right corner to start assistant");
+
     public static void initialize(Context context) {
         // Avoid the disk read for user builds
         if (Utilities.IS_DEBUG_DEVICE) {
