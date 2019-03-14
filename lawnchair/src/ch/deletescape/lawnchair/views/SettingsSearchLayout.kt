7a85c6b4a9687a48fd49c8d4fcc0f697fdd29cea
Till Kottmann <me@deletescape.ch>
2019-03-14 19:32:11 +0100

    Make settings search radius follow icon shape
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- lawnchair/src/ch/deletescape/lawnchair/views/SettingsSearchLayout.kt
+++ lawnchair/src/ch/deletescape/lawnchair/views/SettingsSearchLayout.kt
@@ -18,11 +18,41 @@
 package ch.deletescape.lawnchair.views
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Rect
+import android.support.v7.widget.CardView
 import android.util.AttributeSet
+import ch.deletescape.lawnchair.folder.FolderShape
 import com.android.launcher3.InsettableFrameLayout
+import com.android.launcher3.R
+import com.android.launcher3.Utilities
+import com.android.launcher3.graphics.IconShapeOverride
 
-class SettingsSearchLayout(context: Context, attrs: AttributeSet?) : InsettableFrameLayout(context, attrs) {
+class SettingsSearchLayout(context: Context, attrs: AttributeSet?) : InsettableFrameLayout(context, attrs), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        Utilities.getDevicePrefs(context).registerOnSharedPreferenceChangeListener(this)
+        updateRadius()
+    }
+
+    fun updateRadius() {
+        val edgeRadius = FolderShape.sInstance.mAttrs.get(R.attr.qsbEdgeRadius)
+        if (edgeRadius != null) {
+            findViewById<CardView>(R.id.search_bar).radius = edgeRadius.getDimension(resources.displayMetrics)
+        }
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == IconShapeOverride.KEY_PREFERENCE) {
+            updateRadius()
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        Utilities.getDevicePrefs(context).unregisterOnSharedPreferenceChangeListener(this)
+    }
 
     override fun setInsets(insets: Rect) {
         setPadding(0, insets.top, 0, 0)
