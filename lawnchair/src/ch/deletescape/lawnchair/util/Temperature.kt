b2356ff1327ac47a9ce2c11234480dae32a23d7e
Till Kottmann <me@deletescape.ch>
2019-03-25 22:26:39 +0100

    Add remaining temperature units
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- lawnchair/src/ch/deletescape/lawnchair/util/Temperature.kt
+++ lawnchair/src/ch/deletescape/lawnchair/util/Temperature.kt
@@ -30,7 +30,12 @@ class Temperature(val value: Int, val unit: Unit) {
 
         Celsius(0f, 100f, "°C"),
         Fahrenheit(32f, 212f, "°F"),
-        Kelvin(273f, 373f, "K");
+        Kelvin(273f, 373f, "K"),
+        Rakine(491.67f, 671.64102f, "°R"),
+        Delisle(-100f, 50f, "°D"),
+        Newton(0f, 33f, "°N"),
+        Reaumur(0f, 80f, "°Ré"),
+        Romer(7.5f, 60f, "°Rø");
 
         val range = boilingPoint - freezingPoint
     }
@@ -42,6 +47,11 @@ class Temperature(val value: Int, val unit: Unit) {
                 "metric" -> Unit.Celsius
                 "imperial" -> Unit.Fahrenheit
                 "kelvin" -> Unit.Kelvin
+                "rakine" -> Unit.Rakine
+                "delisle" -> Unit.Delisle
+                "newton" -> Unit.Newton
+                "reaumur" -> Unit.Reaumur
+                "romer" -> Unit.Romer
                 else -> throw IllegalArgumentException("unknown unit $unit")
             }
         }
@@ -51,6 +61,11 @@ class Temperature(val value: Int, val unit: Unit) {
                 Unit.Celsius -> "metric"
                 Unit.Fahrenheit -> "imperial"
                 Unit.Kelvin -> "kelvin"
+                Unit.Rakine -> "rakine"
+                Unit.Delisle -> "delisle"
+                Unit.Newton -> "newton"
+                Unit.Reaumur -> "reaumur"
+                Unit.Romer -> "romer"
             }
         }
     }
