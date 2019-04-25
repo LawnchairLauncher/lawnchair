a310a80c7b01057eaa89edca99085c24706c9e73
Hyunyoung Song <hyunyoungs@google.com>
2019-04-25 13:57:16 -0700

    Add important dump logging Bug: 130851537 Bug: 119992316
    Change-Id: I3de7c1d910d1aecd80b47a85284565c79fe0e2cc

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -2345,7 +2345,8 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
         writer.println(" mPendingActivityResult=" + mPendingActivityResult);
         writer.println(" mRotationHelper: " + mRotationHelper);
         // Extra logging for b/116853349
-        mDragLayer.dumpAlpha(writer);
+        mDragLayer.dump(prefix, writer);
+        mStateManager.dump(prefix, writer);
         dumpMisc(writer);
 
         try {
