ca348407fc3f7f7cc868a6aab1d950e2165b7f28
Amir Zaidi <azaidi@live.nl>
2019-05-09 23:29:22 +0200

    Support Rootless Pixel Bridge v3

--- src/com/google/android/libraries/gsa/launcherclient/LauncherClientBridge.java
+++ src/com/google/android/libraries/gsa/launcherclient/LauncherClientBridge.java
@@ -5,38 +5,45 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.os.RemoteException;
 
-import amirz.aidlbridge.Bridge;
-import amirz.aidlbridge.BridgeCallback;
+import amirz.aidlbridge.IBridge;
+import amirz.aidlbridge.IBridgeCallback;
 
-public class LauncherClientBridge extends BridgeCallback.Stub implements ServiceConnection {
+public class LauncherClientBridge extends IBridgeCallback.Stub implements ServiceConnection {
+    private static final String INTERFACE_DESCRIPTOR = "amirz.aidlbridge.IBridge";
+
     private final BaseClientService mClientService;
+    private final int mFlags;
+    private ComponentName mConnectionName;
 
-    public LauncherClientBridge(BaseClientService launcherClientService) {
+    public LauncherClientBridge(BaseClientService launcherClientService, int flags) {
         mClientService = launcherClientService;
+        mFlags = flags;
     }
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
-        Bridge bridge = Bridge.Stub.asInterface(service);
         try {
-            bridge.setCallback(mClientService instanceof LauncherClientService ? 1 : 0, this);
+            if (INTERFACE_DESCRIPTOR.equals(service.getInterfaceDescriptor())) {
+                IBridge bridge = IBridge.Stub.asInterface(service);
+                try {
+                    bridge.bindService(this, mFlags);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                mClientService.onServiceConnected(name, service);
+                mConnectionName = name;
+            }
         } catch (RemoteException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void onBridgeConnected(IBinder service) {
-        mClientService.onServiceConnected(null, service);
-    }
-
-    @Override
-    public void onBridgeDisconnected() {
-        mClientService.onServiceDisconnected(null);
-    }
-
-    @Override
     public void onServiceDisconnected(ComponentName name) {
-        onBridgeDisconnected();
+        if (mConnectionName != null) {
+            mClientService.onServiceDisconnected(mConnectionName);
+            mConnectionName = null;
+        }
     }
 }
