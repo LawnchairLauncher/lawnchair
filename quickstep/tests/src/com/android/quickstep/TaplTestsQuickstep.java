40bbfbed0f9104b83ffe5c8c16000d726177c91b
vadimt <vadimt@google.com>
2019-05-22 19:20:57 -0700

    Reducing test wait time from 60 sec to 10 sec
    Bug: 112282235
    Bug: 121279417
    Change-Id: Id88e782145b5fdae42354f6fce830aa05b78857a

--- quickstep/tests/src/com/android/quickstep/TaplTestsQuickstep.java
+++ quickstep/tests/src/com/android/quickstep/TaplTestsQuickstep.java
@@ -127,7 +127,7 @@ public class TaplTestsQuickstep extends AbstractQuickStepTest {
         assertNotNull("OverviewTask.open returned null", task.open());
         assertTrue("Test activity didn't open from Overview", mDevice.wait(Until.hasObject(
                 By.pkg(getAppPackageName()).text("TestActivity2")),
-                LONG_WAIT_TIME_MS));
+                DEFAULT_UI_TIMEOUT));
         executeOnLauncher(launcher -> assertTrue(
                 "Launcher activity is the top activity; expecting another activity to be the top "
                         + "one",
