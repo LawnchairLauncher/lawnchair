4770e241c997557e1051bc77dd4727d474d900de
Hyunyoung Song <hyunyoungs@google.com>
2019-06-28 16:09:26 -0700

    Enable swipe down to open shade for non userdebug population
    Bug: 131356741
    Change-Id: I74c682d39c70f785777b5a0804b31a0cc2369759

--- quickstep/recents_ui_overrides/src/com/android/launcher3/uioverrides/RecentsUiFactory.java
+++ quickstep/recents_ui_overrides/src/com/android/launcher3/uioverrides/RecentsUiFactory.java
@@ -163,7 +163,7 @@ public abstract class RecentsUiFactory {
             }
         }
 
-        if (FeatureFlags.PULL_DOWN_STATUS_BAR && Utilities.IS_DEBUG_DEVICE
+        if (FeatureFlags.PULL_DOWN_STATUS_BAR
                 && !launcher.getDeviceProfile().isMultiWindowMode
                 && !launcher.getDeviceProfile().isVerticalBarLayout()) {
             list.add(new StatusBarTouchController(launcher));
