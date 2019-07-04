f95a9d5e90458279f227255395f95527e03bb3fc
Till Kottmann <me@deletescape.ch>
2019-07-04 20:55:58 +0200

    Improve adaptive icon generation again
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- src/com/android/launcher3/IconCache.java
+++ src/com/android/launcher3/IconCache.java
@@ -814,7 +814,8 @@ public class IconCache {
     }
 
     private static final class IconDB extends SQLiteCacheHelper {
-        private final static int RELEASE_VERSION = 25;
+
+        private final static int RELEASE_VERSION = 26;
 
         private final static String TABLE_NAME = "icons";
         private final static String COLUMN_ROWID = "rowid";
