b69e2018a4a6b28cb1da0de39fea17754b5dfd8d
paphonb <paphonb@gmail.com>
2019-07-07 19:43:09 +0700

    Clear icon cache when restoring settings

--- lawnchair/src/ch/deletescape/lawnchair/backup/RestoreBackupActivity.kt
+++ lawnchair/src/ch/deletescape/lawnchair/backup/RestoreBackupActivity.kt
@@ -33,6 +33,7 @@ import android.view.View
 import android.widget.*
 import ch.deletescape.lawnchair.colors.ColorEngine
 import ch.deletescape.lawnchair.settings.ui.SettingsBaseActivity
+import com.android.launcher3.LauncherAppState
 import com.android.launcher3.R
 import com.android.launcher3.Utilities
 import com.android.launcher3.provider.RestoreDbTask
@@ -196,6 +197,8 @@ class RestoreBackupActivity : SettingsBaseActivity(), LawnchairBackup.MetaLoader
                     Utilities.getLawnchairPrefs(this@RestoreBackupActivity).blockingEdit {
                         restoreSuccess = true
                     }
+                } else {
+                    LauncherAppState.getInstance(context).iconCache.clear()
                 }
 
                 if (result and LawnchairBackup.INCLUDE_HOMESCREEN != 0) {
