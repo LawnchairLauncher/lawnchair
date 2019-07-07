6a7dfafb3e79037d7300e51ef70cc18981a9f148
Till Kottmann <me@deletescape.ch>
2019-07-07 18:37:18 +0200

    Oreo Overview: Add pop-up to delete screen
    Signed-off-by: Till Kottmann <me@deletescape.ch>

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -676,6 +676,42 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
         }
     }
 
+    public void removeScreen(int index, final boolean animate) {
+        if (mLauncher.isWorkspaceLoading()) {
+            return;
+        }
+
+        int currentPage = getNextPage();
+        snapToPage(index, SNAP_OFF_EMPTY_SCREEN_DURATION);
+        long id = getScreenIdForPageIndex(index);
+        fadeAndRemoveScreen(id, SNAP_OFF_EMPTY_SCREEN_DURATION,
+                FADE_EMPTY_SCREEN_DURATION, null, false);
+
+        CellLayout cl = mWorkspaceScreens.get(id);
+        mWorkspaceScreens.remove(id);
+        mScreenOrder.remove(id);
+
+        boolean isInAccessibleDrag = mLauncher.getAccessibilityDelegate().isInAccessibleDrag();
+
+        boolean pageShift = indexOfChild(cl) < currentPage;
+
+        if (isInAccessibleDrag) {
+            cl.enableAccessibleDrag(false, CellLayout.WORKSPACE_ACCESSIBILITY_DRAG);
+        }
+
+        removeView(cl);
+
+        LauncherModel.updateWorkspaceScreenOrder(mLauncher, mScreenOrder);
+
+        if (getChildCount() == 0) {
+            addExtraEmptyScreen();
+        }
+
+        if (pageShift) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
     public void removeExtraEmptyScreen(final boolean animate, boolean stripEmptyScreens) {
         removeExtraEmptyScreenDelayed(animate, null, 0, stripEmptyScreens);
     }
@@ -723,18 +759,23 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
     private void fadeAndRemoveEmptyScreen(int delay, int duration, final Runnable onComplete,
             final boolean stripEmptyScreens) {
+        fadeAndRemoveScreen(EXTRA_EMPTY_SCREEN_ID, delay, duration, onComplete, stripEmptyScreens);
+    }
+
+    private void fadeAndRemoveScreen(long id, int delay, int duration, final Runnable onComplete,
+            final boolean stripEmptyScreens) {
         // XXX: Do we need to update LM workspace screens below?
         PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat("alpha", 0f);
         PropertyValuesHolder bgAlpha = PropertyValuesHolder.ofFloat("backgroundAlpha", 0f);
 
-        final CellLayout cl = mWorkspaceScreens.get(EXTRA_EMPTY_SCREEN_ID);
+        final CellLayout cl = mWorkspaceScreens.get(id);
 
         mRemoveEmptyScreenRunnable = new Runnable() {
             @Override
             public void run() {
                 if (hasExtraEmptyScreen()) {
-                    mWorkspaceScreens.remove(EXTRA_EMPTY_SCREEN_ID);
-                    mScreenOrder.remove(EXTRA_EMPTY_SCREEN_ID);
+                    mWorkspaceScreens.remove(id);
+                    mScreenOrder.remove(id);
                     removeView(cl);
                     if (stripEmptyScreens) {
                         stripEmptyScreens();
