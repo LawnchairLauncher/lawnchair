f2fd4a7815d617b9f523e01736dd663b082079bb
paphonb <paphonb@gmail.com>
2019-09-15 16:47:42 +0700

    Build 2 APKs for Pie and 10

--- build-ci.sh
+++ build-ci.sh
@@ -2,10 +2,27 @@
 
 if [ $DRONE_BRANCH = "beta" ]; then
     BUILD_COMMAND="assembleAospWithQuickstepPieLawnchairPlahRelease"
+    exit 1 # Not supported yet
 else
     # TODO: switch to "optimized" variant
-    BUILD_COMMAND="assembleAospWithQuickstepPieLawnchairCiDebug"
+    BUILD_COMMAND_PIE="assembleAospWithQuickstepPieLawnchairCiDebug"
+    BUILD_COMMAND_TEN="assembleAospWithQuickstepTenLawnchairCiDebug"
 fi
 
-echo "Running $BUILD_COMMAND"
-bash ./gradlew $BUILD_COMMAND
+OUT_DIR=out
+APKS_OUT_DIR=$OUT_DIR/apks
+MAPPINGS_OUT_DIR=$OUT_DIR/mappings
+mkdir -p $APKS_OUT_DIR
+mkdir -p $MAPPINGS_OUT_DIR
+
+echo "Building for Pie target"
+bash ./gradlew $BUILD_COMMAND_PIE
+
+mv build/outputs/apk/*/*/*.apk $APKS_OUT_DIR/Lawnchair-pie.apk
+mv build/outputs/mapping/*/*/mapping.txt $MAPPINGS_OUT_DIR/mapping-pie.txt
+
+echo "Building for 10 target"
+bash ./gradlew $BUILD_COMMAND_TEN
+
+mv build/outputs/apk/*/*/*.apk $APKS_OUT_DIR/Lawnchair-10.apk
+mv build/outputs/mapping/*/*/mapping.txt $MAPPINGS_OUT_DIR/mapping-ten.txt
