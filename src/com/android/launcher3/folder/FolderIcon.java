6f65162298aca3b67bc2a0476b7336800b4b8868
Till Kottmann <till.kottmann@bison-group.com>
2019-11-12 10:43:42 +0100

    FolderInfo: Prevent Nullpointer Exception

--- src/com/android/launcher3/folder/FolderIcon.java
+++ src/com/android/launcher3/folder/FolderIcon.java
@@ -784,7 +784,7 @@ public class FolderIcon extends FrameLayout implements FolderListener, OnResumeC
             return;
         }
         ((CellLayout.LayoutParams) getLayoutParams()).canReorder = true;
-        if (mInfo.container == LauncherSettings.Favorites.CONTAINER_HOTSEAT) {
+        if (mInfo != null && mInfo.container == LauncherSettings.Favorites.CONTAINER_HOTSEAT) {
             CellLayout cl = (CellLayout) getParent().getParent();
             cl.clearFolderLeaveBehind();
         }
@@ -797,7 +797,7 @@ public class FolderIcon extends FrameLayout implements FolderListener, OnResumeC
         CellLayout.LayoutParams lp = (CellLayout.LayoutParams) getLayoutParams();
         // While the folder is open, the position of the icon cannot change.
         lp.canReorder = false;
-        if (mInfo.container == LauncherSettings.Favorites.CONTAINER_HOTSEAT) {
+        if (mInfo != null && mInfo.container == LauncherSettings.Favorites.CONTAINER_HOTSEAT) {
             CellLayout cl = (CellLayout) getParent().getParent();
             cl.setFolderLeaveBehindCell(lp.cellX, lp.cellY);
         }
