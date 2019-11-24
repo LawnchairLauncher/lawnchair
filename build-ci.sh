e4362280182a778c4947bf8fcdb9c3446b466fb7
paphonb <paphonb@gmail.com>
2019-11-24 14:16:06 +0700

    Disable R8 for now

--- build-ci.sh
+++ build-ci.sh
@@ -15,4 +15,6 @@ mkdir -p $MAPPINGS_OUT_DIR
 bash ./gradlew $BUILD_COMMAND
 
 mv build/outputs/apk/*/*/*.apk $APKS_OUT_DIR/Lawnchair.apk
-mv build/outputs/mapping/*/*/mapping.txt $MAPPINGS_OUT_DIR/mapping.txt
+if [ -f build/outputs/mapping/*/*/mapping.txt ]; then
+    mv build/outputs/mapping/*/*/mapping.txt $MAPPINGS_OUT_DIR/mapping.txt
+fi
