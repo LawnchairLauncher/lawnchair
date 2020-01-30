732b1d46c5ea85f3b2ee860fbfba5a197e7be1b0
Adam Cohen <adamcohen@google.com>
2020-01-30 10:46:52 -0800

    Fix a couple issues related to drag and drop in Touch Exploration mode
    => The framework no longer sends touch events after double-click and hold, and instead only triggers a single long-press event
    => As a result of the above, there is some additional state that must be set for accessible drag and drop, and further, we no longer initiate a drag in response to long press when Touch Exploration is on.
    
    Test: manual
    
    issue b/148308758
    
    Change-Id: I26ab90b807965f31c8de93d451130410169f2fb0

--- src/com/android/launcher3/LauncherRootView.java
+++ src/com/android/launcher3/LauncherRootView.java
@@ -8,7 +8,6 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Insets;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Build;
