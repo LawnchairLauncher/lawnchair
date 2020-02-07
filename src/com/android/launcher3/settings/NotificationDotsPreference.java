4c81a9a8194190d4839a6634e1b66cda93cced58
Pinyao Ting <pinyaoting@google.com>
2020-02-07 11:39:12 -0800

    fix launcher crash when accessing Notification dots in home settings
    Bug: 148815606
    Test:
    1.  Make sure launcher doesn't have notifications permission
        (Settings -> search for "notification access" -> Disable access for Launcher)
    2.  Home settings -> Notification Dot
    3.  Verify Alert Dialog pops up without crash
    
    Change-Id: I399b6aad46e8a3e185199a2909adac9f75596b50

--- src/com/android/launcher3/settings/NotificationDotsPreference.java
+++ src/com/android/launcher3/settings/NotificationDotsPreference.java
@@ -20,7 +20,6 @@ import static com.android.launcher3.settings.SettingsActivity.EXTRA_SHOW_FRAGMEN
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -30,12 +29,13 @@ import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.fragment.app.DialogFragment;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
 import com.android.launcher3.R;
 import com.android.launcher3.notification.NotificationListener;
 import com.android.launcher3.util.SecureSettingsObserver;
-
-import androidx.preference.Preference;
-import androidx.preference.PreferenceViewHolder;
 
 /**
  * A {@link Preference} for indicating notification dots status.
