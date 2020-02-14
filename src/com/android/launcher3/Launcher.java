0dd1bd1113b12b5055b237294844ef1f56a41f00
vadimt <vadimt@google.com>
2020-02-14 14:42:35 -0800

    TAPL: Verifying activity start/stop events
    It also turned out that Pilfer event seems to come in a
    non-deterministic order relative to the events from the Main and TIS
    sequences. So I moved it to its own sequence.
    
    Change-Id: Ie4ea5865afd900bebbd8287dad2372c94dce8ad5

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -956,6 +956,8 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
                 }
             });
         }
+
+        TestLogging.recordEvent(TestProtocol.SEQUENCE_MAIN, "Activity.onStop");
     }
 
     @Override
@@ -969,6 +971,7 @@ public class Launcher extends BaseDraggingActivity implements LauncherExterns,
 
         mAppWidgetHost.setListenIfResumed(true);
         TraceHelper.INSTANCE.endSection(traceToken);
+        TestLogging.recordEvent(TestProtocol.SEQUENCE_MAIN, "Activity.onStart");
     }
 
     private void handleDeferredResume() {
