ac752e39f5d12ff02315f921269472dbd92ad227
vadimt <vadimt@google.com>
2020-03-24 17:58:03 -0700

    Debug tracing to repro a flake
    Bug: 152354290
    Change-Id: I06c9ad131ab125226dfdd548121d0aa3f26c7bc7

--- src/com/android/launcher3/BaseRecyclerView.java
+++ src/com/android/launcher3/BaseRecyclerView.java
@@ -18,6 +18,7 @@ package com.android.launcher3;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,7 @@ import android.view.ViewGroup;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.launcher3.compat.AccessibilityManagerCompat;
+import com.android.launcher3.testing.TestProtocol;
 import com.android.launcher3.views.RecyclerViewFastScroller;
 
 
@@ -176,6 +178,10 @@ public abstract class BaseRecyclerView extends RecyclerView  {
     @Override
     public void onScrollStateChanged(int state) {
         super.onScrollStateChanged(state);
+
+        if (TestProtocol.sDebugTracing) {
+            Log.d(TestProtocol.NO_SCROLL_END_WIDGETS, "onScrollStateChanged: " + state);
+        }
 
         if (state == SCROLL_STATE_IDLE) {
             AccessibilityManagerCompat.sendScrollFinishedEventToTest(getContext());
