e553e380d0dae577b690a355056cbaaacef2228b
Vinit Nayak <peanutbutter@google.com>
2020-04-15 14:13:38 -0700

    Rotate Overview Task Action Menu
    Fixes: 153371258
    Test: Tap on icon in portrait,
    landscape and seascape. Open menu
    then rotate phone.
    
    Change-Id: I38b018371561b502fe211a3c2ddea4ff7fde4274

--- quickstep/recents_ui_overrides/src/com/android/quickstep/views/TaskView.java
+++ quickstep/recents_ui_overrides/src/com/android/quickstep/views/TaskView.java
@@ -466,6 +466,7 @@ public class TaskView extends FrameLayout implements PageCallbacks, Reusable {
         int iconRotation = orientationState.getTouchRotation();
         PagedOrientationHandler orientationHandler = orientationState.getOrientationHandler();
         boolean isRtl = orientationHandler.getRecentsRtlSetting(getResources());
+        LayoutParams snapshotParams = (LayoutParams) mSnapshotView.getLayoutParams();
         int thumbnailPadding = (int) getResources().getDimension(R.dimen.task_thumbnail_top_margin);
         LayoutParams iconParams = (LayoutParams) mIconView.getLayoutParams();
         int rotation = orientationState.getTouchRotationDegrees();
@@ -473,7 +474,8 @@ public class TaskView extends FrameLayout implements PageCallbacks, Reusable {
             case Surface.ROTATION_90:
                 iconParams.gravity = (isRtl ? END : START) | CENTER_VERTICAL;
                 iconParams.rightMargin = -thumbnailPadding;
-                iconParams.leftMargin = iconParams.topMargin = iconParams.bottomMargin = 0;
+                iconParams.leftMargin = 0;
+                iconParams.topMargin = snapshotParams.topMargin / 2;
                 break;
             case Surface.ROTATION_180:
                 iconParams.gravity = BOTTOM | CENTER_HORIZONTAL;
@@ -483,17 +485,21 @@ public class TaskView extends FrameLayout implements PageCallbacks, Reusable {
             case Surface.ROTATION_270:
                 iconParams.gravity = (isRtl ? END : START) | CENTER_VERTICAL;
                 iconParams.leftMargin = -thumbnailPadding;
-                iconParams.rightMargin = iconParams.topMargin = iconParams.bottomMargin = 0;
+                iconParams.rightMargin = 0;
+                iconParams.topMargin = snapshotParams.topMargin / 2;
                 break;
             case Surface.ROTATION_0:
             default:
                 iconParams.gravity = TOP | CENTER_HORIZONTAL;
-                iconParams.leftMargin = iconParams.topMargin = iconParams.rightMargin =
-                    iconParams.bottomMargin = 0;
+                iconParams.leftMargin = iconParams.topMargin = iconParams.rightMargin = 0;
                 break;
         }
         mIconView.setLayoutParams(iconParams);
         mIconView.setRotation(rotation);
+
+        if (mMenuView != null) {
+            mMenuView.onRotationChanged();
+        }
     }
 
     private void setIconAndDimTransitionProgress(float progress, boolean invert) {
@@ -600,7 +606,10 @@ public class TaskView extends FrameLayout implements PageCallbacks, Reusable {
         }
 
         if (mMenuView != null) {
-            mMenuView.setPosition(getX() - getRecentsView().getScrollX(), getY());
+            PagedOrientationHandler pagedOrientationHandler = getPagedOrientationHandler();
+            RecentsView recentsView = getRecentsView();
+            mMenuView.setPosition(getX() - recentsView.getScrollX(),
+                    getY() - recentsView.getScrollY(), pagedOrientationHandler);
             mMenuView.setScaleX(getScaleX());
             mMenuView.setScaleY(getScaleY());
         }
@@ -872,6 +881,10 @@ public class TaskView extends FrameLayout implements PageCallbacks, Reusable {
 
     public RecentsView getRecentsView() {
         return (RecentsView) getParent();
+    }
+
+    PagedOrientationHandler getPagedOrientationHandler() {
+        return getRecentsView().mOrientationState.getOrientationHandler();
     }
 
     public void notifyTaskLaunchFailed(String tag) {
