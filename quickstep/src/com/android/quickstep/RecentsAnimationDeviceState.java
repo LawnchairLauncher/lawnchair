281193e9967c291860cebaa25bed7d65694d7c0a
Vinit Nayak <peanutbutter@google.com>
2020-05-27 12:18:30 -0700

    Move content resolver call to constructor and cache
    Changing fixed rotation enabled system setting flag
    should be followed by a restart of launcher for it
    to take effect.
    
    Bug: 149571513
    Change-Id: I84fbb654c325076571a038604fa929984d22f254

--- quickstep/src/com/android/quickstep/RecentsAnimationDeviceState.java
+++ quickstep/src/com/android/quickstep/RecentsAnimationDeviceState.java
@@ -178,7 +178,7 @@ public class RecentsAnimationDeviceState implements
     }
 
     private void setupOrientationSwipeHandler() {
-        if (!isFixedRotationTransformEnabled(mContext)) {
+        if (!isFixedRotationTransformEnabled()) {
             return;
         }
 
