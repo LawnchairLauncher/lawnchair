ba6fec3eaeb2087428c8e33826429d72ad0290cf
Becky Qiu <xuqiu@google.com>
2020-08-12 17:17:00 -0700

    [Overview Actions] Reset the modal task view when rotate from select mode.
    Test: local
    Bug: 161901771
    Change-Id: I103e59ad68874db7fcb39556764d315983ac047c

--- quickstep/recents_ui_overrides/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/recents_ui_overrides/src/com/android/quickstep/views/RecentsView.java
@@ -1718,6 +1718,7 @@ public abstract class RecentsView<T extends StatefulActivity> extends PagedView 
         // animation.
         if (mActivity.isInState(OVERVIEW_MODAL_TASK)) {
             mActivity.getStateManager().goToState(LauncherState.OVERVIEW, false);
+            resetModalVisuals();
         }
     }
 
