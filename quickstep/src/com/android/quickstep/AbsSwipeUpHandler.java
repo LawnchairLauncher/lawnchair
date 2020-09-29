9d4a96ed029fdad1e369d5eedd082938f0dc9e01
Riddle Hsu <riddlehsu@google.com>
2020-09-30 00:32:04 +0800

    Add latency metrics for recents gesture
    Pass the touch down time to RecentsAnimation#startRecentsActivity.
    
    Bug: 169221287
    Test: Enable statsd log: "adb shell cmd stats print-logs"
          Touch gesture navigation bar.
          adb logcat | grep statsd | grep "(48)"
          The line may contain 0x100000->4[I] 0x110000->20[I]
          that means 4=by recents and 20=latency 20ms.
    Change-Id: I81ee804895b7712f4d925736f5b4694c11a12cbe
    (cherry picked from commit 63623967b83edad56db58173ebb6687c685b9177)

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -1476,6 +1476,11 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<?>, Q extends
         mGestureEndCallback = gestureEndCallback;
     }
 
+    @Override
+    public long getStartTouchTime() {
+        return mTouchTimeMs;
+    }
+
     protected void linkRecentsViewScroll() {
         SurfaceTransactionApplier.create(mRecentsView, applier -> {
             mTransformParams.setSyncTransactionApplier(applier);
