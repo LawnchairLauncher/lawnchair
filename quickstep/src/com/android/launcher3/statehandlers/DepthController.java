21ed1e4ebf27e01a5b1bb772cd6ba03e31e7e6db
Danny Lin <danny@kdrag0n.dev>
2020-10-04 00:36:26 -0700

    Launcher3: Allow blur in all states except ALL_APPS and NORMAL
    We want to blur in more states for completeness:
      - Background app: inter-app quick switch
      - Hint: intermediate transition state
      - ... and likely more that I haven't encountered yet.
    
    None of the additional states above have major performance issues with
    blur, and we are allowing more states than we're blocking now, so let's
    change this to a blocklist that only contains states known to exhibit
    poor performance.
    
    Change-Id: Ie92da12f21a2dd0523769c7198fe1c1d2e2810e9

--- quickstep/src/com/android/launcher3/statehandlers/DepthController.java
+++ quickstep/src/com/android/launcher3/statehandlers/DepthController.java
@@ -227,11 +227,11 @@ public class DepthController implements StateHandler<LauncherState>,
 
         if (supportsBlur) {
             final int blur;
-            if (mLauncher.isInState(LauncherState.OVERVIEW) ||
-                    mLauncher.isInState(LauncherState.QUICK_SWITCH)) {
-                blur = (int) (mDepth * mMaxBlurRadius);
-            } else {
+            if (mLauncher.isInState(LauncherState.ALL_APPS) ||
+                    mLauncher.isInState(LauncherState.NORMAL)) {
                 blur = 0;
+            } else {
+                blur = (int) (mDepth * mMaxBlurRadius);
             }
             new TransactionCompat()
                     .setBackgroundBlurRadius(mSurface, blur)
