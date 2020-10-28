c4bef358b070b01be6fe9cfd18f8b1b0c666b395
Winson Chung <winsonc@google.com>
2020-10-28 00:13:03 -0700

    DO NOT MERGE Revert "Defer recreation until resumed"
    Bug: 171707437
    Test: Repro steps in bug
    
    This reverts commit 3f8d868110b426e40e09ece8b305fd84c1dde60c.
    
    Change-Id: I909ab701c674fe975982a7de06dc9e89dcd03aea
    (cherry picked from commit 272f4541f98e314758c1cd90c96f3d42023db6a7)

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1977,7 +1977,6 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         return result;
     }
 
-    @Override
     public void addOnResumeCallback(OnResumeCallback callback) {
         mOnResumeCallbacks.add(callback);
     }
