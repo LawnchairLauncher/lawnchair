5c3d37b37d2faf961c2ea3697d7bf5682f4f8550
[1;3C <erosky@google.com>
2021-01-29 18:20:52 -0800

    Update input-monitors to accept unrotated coordinates
    Inputflinger now internally always works in the orientation
    the display grew-up in so the coordinates of gesture-monitors
    no-longer rotate to match the display.
    
    For now, this adds some shims to achieve previous expected
    coordinates in launcher
    
    This is behind a flag:
       adb shell setprop persist.debug.per_window_input_rotation 1
    
    Bug: 179274888
    Test: use touch interactions on rotated display
    Change-Id: Iecd9d233f60b86e24070d7d9055af6b8f44f0b54

--- quickstep/src/com/android/quickstep/TouchInteractionService.java
+++ quickstep/src/com/android/quickstep/TouchInteractionService.java
@@ -38,18 +38,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.graphics.drawable.Icon;
+import android.hardware.display.DisplayManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.util.Log;
 import android.view.Choreographer;
+import android.view.Display;
 import android.view.InputEvent;
 import android.view.MotionEvent;
+import android.view.Surface;
 import android.view.accessibility.AccessibilityManager;
 
 import androidx.annotation.BinderThread;
@@ -92,6 +97,7 @@ import com.android.systemui.plugins.PluginListener;
 import com.android.systemui.shared.recents.IOverviewProxy;
 import com.android.systemui.shared.recents.ISystemUiProxy;
 import com.android.systemui.shared.system.ActivityManagerWrapper;
+import com.android.systemui.shared.system.InputChannelCompat;
 import com.android.systemui.shared.system.InputChannelCompat.InputEventReceiver;
 import com.android.systemui.shared.system.InputConsumerController;
 import com.android.systemui.shared.system.InputMonitorCompat;
@@ -121,6 +127,9 @@ public class TouchInteractionService extends Service implements PluginListener<O
      * TODO: Use AccessibilityService's corresponding global action constant in S
      */
     private static final int SYSTEM_ACTION_ID_ALL_APPS = 14;
+
+    public static final boolean ENABLE_PER_WINDOW_INPUT_ROTATION =
+            SystemProperties.getBoolean("persist.debug.per_window_input_rotation", false);
 
     private int mBackGestureNotificationCounter = -1;
     @Nullable
@@ -249,6 +258,8 @@ public class TouchInteractionService extends Service implements PluginListener<O
     private InputMonitorCompat mInputMonitorCompat;
     private InputEventReceiver mInputEventReceiver;
 
+    private DisplayManager mDisplayManager;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -262,6 +273,7 @@ public class TouchInteractionService extends Service implements PluginListener<O
         mDeviceState.addOneHandedModeChangedCallback(this::onOneHandedModeOverlayChanged);
         mDeviceState.runOnUserUnlocked(this::onUserUnlocked);
         ProtoTracer.INSTANCE.get(this).add(this);
+        mDisplayManager = getSystemService(DisplayManager.class);
 
         sConnected = true;
     }
@@ -429,6 +441,15 @@ public class TouchInteractionService extends Service implements PluginListener<O
             return;
         }
         MotionEvent event = (MotionEvent) ev;
+        if (ENABLE_PER_WINDOW_INPUT_ROTATION) {
+            final Display display = mDisplayManager.getDisplay(mDeviceState.getDisplayId());
+            int rotation = display.getRotation();
+            Point sz = new Point();
+            display.getRealSize(sz);
+            if (rotation != Surface.ROTATION_0) {
+                event.transform(InputChannelCompat.createRotationMatrix(rotation, sz.x, sz.y));
+            }
+        }
 
         TestLogging.recordMotionEvent(
                 TestProtocol.SEQUENCE_TIS, "TouchInteractionService.onInputEvent", event);
