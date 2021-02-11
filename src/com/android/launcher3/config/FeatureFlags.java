8e57cce4ee818dfceaf0f59e492e9b4e9f814d05
Andras Kloczl <andraskloczl@google.com>
2021-02-11 23:51:19 +0100

    Two panel & two page swipe & currentPage fixes
    - Pagespacing set to 0 to have similar design to figma
    - Added padding to left side of left panel and right side of right panel to match figma
    - getPageScrolls changed to support two page scrolling
    - validateNewPage changed so that only even indexed pages can be switched to
    - getPageWidthSize added for measurements, it returns half screen width when two panel is active
    - onMeasure changed to use getPageWidthSize
    - panelCount has been increased from 1 to 2 when two panel is active
    - add feature flag for two panel home
    - shouldConsumeTouch changed so not only currentpage is checked but currentpage+1 screen as well
    - add one more extra check for setDropLayoutForDragObject for currentpage + 2, next to right panel
    - using new grid option for two panel home screen
    
    Test: manual
    Bug: 174464115
    Change-Id: I1bad7eecc67f65fb833608744a5aa5aca65373b4

--- src/com/android/launcher3/config/FeatureFlags.java
+++ src/com/android/launcher3/config/FeatureFlags.java
@@ -205,6 +205,10 @@ public final class FeatureFlags {
             "ENABLE_OVERVIEW_GRID", false, "Uses grid overview layout. "
             + "Only applicable on large screen devices.");
 
+    public static final BooleanFlag ENABLE_TWO_PANEL_HOME = getDebugFlag(
+            "ENABLE_TWO_PANEL_HOME", false,
+            "Uses two panel on home screen. Only applicable on large screen devices.");
+
     public static final BooleanFlag ENABLE_SPLIT_SELECT = getDebugFlag(
             "ENABLE_SPLIT_SELECT", false, "Uses new split screen selection overview UI");
 
