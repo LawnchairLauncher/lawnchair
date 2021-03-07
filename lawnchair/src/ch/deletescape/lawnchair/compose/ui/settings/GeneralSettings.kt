2a1000e7709c21e83757ef46c779c975312f9e99
Patryk Michalik <contact@patrykmichalik.com>
2021-03-07 18:51:45 +0100

    Only show ‘Make Adaptive Icons for icon packs’ toggle on Android Oreo and higher

--- lawnchair/src/ch/deletescape/lawnchair/compose/ui/settings/GeneralSettings.kt
+++ lawnchair/src/ch/deletescape/lawnchair/compose/ui/settings/GeneralSettings.kt
@@ -1,5 +1,6 @@
 package ch.deletescape.lawnchair.compose.ui.settings
 
+import android.os.Build
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
@@ -30,15 +31,17 @@ fun GeneralSettings(navController: NavController) {
             title = stringResource(id = R.string.home_screen_rotation_label),
             subtitle = stringResource(id = R.string.home_screen_rotaton_description)
         )
-        SwitchSetting(
-            checked = generateAdaptiveIconsForIconPacks,
-            onCheckedChange = {
-                generateAdaptiveIconsForIconPacks = it
-                sharedPref.edit().putBoolean(LawnchairPreferences.WRAP_ADAPTIVE_ICONS, it).apply()
-            },
-            title = stringResource(id = R.string.make_icon_packs_adaptive_label),
-            subtitle = stringResource(id = R.string.make_icon_packs_adaptive_description)
-        )
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            SwitchSetting(
+                checked = generateAdaptiveIconsForIconPacks,
+                onCheckedChange = {
+                    generateAdaptiveIconsForIconPacks = it
+                    sharedPref.edit().putBoolean(LawnchairPreferences.WRAP_ADAPTIVE_ICONS, it).apply()
+                },
+                title = stringResource(id = R.string.make_icon_packs_adaptive_label),
+                subtitle = stringResource(id = R.string.make_icon_packs_adaptive_description)
+            )
+        }
         NavActionSetting(
             title = stringResource(id = R.string.icon_pack),
             navController = navController,
