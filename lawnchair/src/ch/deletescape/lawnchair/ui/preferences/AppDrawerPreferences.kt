07ef57acd9de594c45e603e4dab61ed98200bd6c
Patryk Michalik <contact@patrykmichalik.com>
2021-03-15 13:56:21 +0100

    Add sliders for app drawer icon and label size

--- lawnchair/src/ch/deletescape/lawnchair/ui/preferences/AppDrawerPreferences.kt
+++ lawnchair/src/ch/deletescape/lawnchair/ui/preferences/AppDrawerPreferences.kt
@@ -8,13 +8,31 @@ import com.android.launcher3.R
 @Composable
 fun AppDrawerPreferences(interactor: PreferenceInteractor) {
     Column {
-        PreferenceGroup {
+        PreferenceGroup(heading = stringResource(id = R.string.grid)) {
             SliderPreference(
                 label = stringResource(id = R.string.app_drawer_columns),
                 value = interactor.allAppsColumns.value,
                 onValueChange = { interactor.setAllAppsColumns(it) },
                 steps = 3,
                 valueRange = 3.0F..7.0F
+            )
+        }
+        PreferenceGroup(heading = stringResource(id = R.string.icons), showDivider = true) {
+            SliderPreference(
+                label = stringResource(id = R.string.icon_size),
+                value = interactor.allAppsIconSizeFactor.value,
+                onValueChange = { interactor.setAllAppsIconSizeFactor(it) },
+                steps = 9,
+                valueRange = 0.5F..1.5F,
+                showAsPercentage = true
+            )
+            SliderPreference(
+                label = stringResource(id = R.string.label_size),
+                value = interactor.allAppsTextSizeFactor.value,
+                onValueChange = { interactor.setAllAppsTextSizeFactor(it) },
+                steps = 9,
+                valueRange = 0.5F..1.5F,
+                showAsPercentage = true
             )
         }
     }
