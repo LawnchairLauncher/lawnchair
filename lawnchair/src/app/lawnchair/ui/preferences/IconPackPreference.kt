26af6371e67dea53f51776e11b44ffe9cc1ba403
Patryk Michalik <contact@patrykmichalik.com>
2021-03-24 14:14:58 +0100

    Redesign icon pack selector

--- lawnchair/src/app/lawnchair/ui/preferences/IconPackPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/IconPackPreference.kt
@@ -1,68 +1,86 @@
 package app.lawnchair.ui.preferences
 
 import android.graphics.drawable.Drawable
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
+import com.android.launcher3.R
 
 data class IconPackInfo(val name: String, val packageName: String, val icon: Drawable)
 
+@ExperimentalAnimationApi
 @Composable
 fun IconPackPreference(interactor: PreferenceInteractor) {
-    Box {
+    val iconPacks = interactor.getIconPacks().values.toList()
+
+    PreferenceGroup {
         LazyColumn(Modifier.fillMaxWidth()) {
-            items(interactor.getIconPacks().values.toList()) { iconPack ->
+            items(iconPacks) { iconPack ->
                 IconPackListItem(
                     iconPack,
                     interactor.iconPackPackage.value,
                     onSelectionChange = { interactor.setIconPackPackage(it) },
+                    showDivider = iconPacks.last().packageName != iconPack.packageName
                 )
             }
         }
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun IconPackListItem(
     iconPack: IconPackInfo,
     activeIconPackPackageName: String,
-    onSelectionChange: (String) -> Unit
+    onSelectionChange: (String) -> Unit,
+    showDivider: Boolean = true
 ) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(56.dp)
-            .clickable { onSelectionChange(iconPack.packageName) }
-            .padding(start = 16.dp, end = 16.dp)
+    PreferenceTemplate(
+        height = 52.dp,
+        showDivider = showDivider
     ) {
-        RadioButton(
-            selected = iconPack.packageName == activeIconPackPackageName,
-            { onSelectionChange(iconPack.packageName) })
-        Image(
-            iconPack.icon.toBitmap().asImageBitmap(),
-            null,
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .padding(start = 32.dp)
-                .width(36.dp)
-                .height(36.dp)
-        )
-        Text(
-            modifier = Modifier.padding(start = 16.dp),
-            text = iconPack.name,
-            style = MaterialTheme.typography.subtitle1,
-            color = MaterialTheme.colors.onBackground
-        )
+                .fillMaxSize()
+                .clickable { onSelectionChange(iconPack.packageName) }
+                .padding(start = 16.dp, end = 16.dp)
+        ) {
+            Image(
+                iconPack.icon.toBitmap().asImageBitmap(),
+                null,
+                modifier = Modifier
+                    .width(32.dp)
+                    .height(32.dp)
+            )
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = iconPack.name,
+                style = MaterialTheme.typography.subtitle1,
+                color = MaterialTheme.colors.onBackground
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            AnimatedVisibility(visible = iconPack.packageName == activeIconPackPackageName) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_tick),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(MaterialTheme.colors.primary)
+                )
+            }
+        }
     }
 }
