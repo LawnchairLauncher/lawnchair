e8d60b8477f5b27c9b6d800932a163cdf2967c8e
Patryk Michalik <contact@patrykmichalik.com>
2021-03-25 18:00:23 +0100

    Reformat `SingletonHolder`

--- lawnchair/src/app/lawnchair/util/SingletonHolder.kt
+++ lawnchair/src/app/lawnchair/util/SingletonHolder.kt
@@ -22,6 +22,7 @@ import android.content.Context
 // Source: https://medium.com/@BladeCoder/kotlin-singletons-with-argument-194ef06edd9e
 open class SingletonHolder<out T, in A>(creator: (A) -> T) {
     private var creator: ((A) -> T)? = creator
+
     @Volatile
     private var instance: T? = null
 
@@ -47,5 +48,5 @@ open class SingletonHolder<out T, in A>(creator: (A) -> T) {
     fun dangerousGetInstance() = instance
 }
 
-open class LawnchairSingletonHolder<out T>(creator: (Context) -> T)
-    : SingletonHolder<T, Context>(ensureOnMainThread(useApplicationContext(creator)))
+open class LawnchairSingletonHolder<out T>(creator: (Context) -> T) :
+    SingletonHolder<T, Context>(ensureOnMainThread(useApplicationContext(creator)))
