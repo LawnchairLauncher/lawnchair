5eff0bbe13f98cf1efcde90f60bcdc5ed39998ff
Jon Miranda <jonmiranda@google.com>
2021-04-09 10:55:49 -0400

    Remove temp logic added to migrate users to new 4x5 grid.
    Related changes:
    - I81a91b8415effbe0bf7ba722f11ab44a4169e3df
    - Ia7ae129048bf5878b216f31f949e10c71e608818
    
    Bug: 183965808
    Bug: 175329686
    Test: manual
    Change-Id: If9ae69d746f1c86db98275f0c00ae054e146b67e

--- src/com/android/launcher3/config/FeatureFlags.java
+++ src/com/android/launcher3/config/FeatureFlags.java
@@ -191,10 +191,6 @@ public final class FeatureFlags {
             "EXPANDED_SMARTSPACE", false, "Expands smartspace height to two rows. "
               + "Any apps occupying the first row will be removed from workspace.");
 
-    public static final DeviceFlag ENABLE_FOUR_COLUMNS = new DeviceFlag(
-            "ENABLE_FOUR_COLUMNS", false, "Uses 4 columns in launcher grid."
-            + "Warning: This will permanently alter your home screen items and is not reversible.");
-
     // TODO: b/172467144 Remove ENABLE_LAUNCHER_ACTIVITY_THEME_CROSSFADE feature flag.
     public static final BooleanFlag ENABLE_LAUNCHER_ACTIVITY_THEME_CROSSFADE = new DeviceFlag(
             "ENABLE_LAUNCHER_ACTIVITY_THEME_CROSSFADE", false, "Enables a "
@@ -231,12 +227,6 @@ public final class FeatureFlags {
                 flag.initialize(context);
             }
             sDebugFlags.sort((f1, f2) -> f1.key.compareToIgnoreCase(f2.key));
-        }
-    }
-
-    public static void removeFlag(DebugFlag flag) {
-        synchronized (sDebugFlags) {
-            sDebugFlags.remove(flag);
         }
     }
 
@@ -315,15 +305,6 @@ public final class FeatureFlags {
         public void initialize(Context context) {
             mCurrentValue = context.getSharedPreferences(FLAGS_PREF_NAME, Context.MODE_PRIVATE)
                     .getBoolean(key, defaultValue);
-        }
-
-        /**
-         * Resets value to default value.
-         */
-        public void reset(Context context) {
-            mCurrentValue = defaultValue;
-            context.getSharedPreferences(FLAGS_PREF_NAME, Context.MODE_PRIVATE)
-                    .edit().putBoolean(key, defaultValue).apply();
         }
 
         @Override
