2955fa8f5cb33d369cb5c39089bf11aceff0b9b3
Jon Miranda <jonmiranda@google.com>
2021-05-05 11:20:20 -0700

    Add method to check if popup supports color extraction.
    Bug: 187058092
    Test: manual
    Change-Id: I795cd3bfd04a7ab9b4730a2e7d7618830faae37e

--- src/com/android/launcher3/popup/ArrowPopup.java
+++ src/com/android/launcher3/popup/ArrowPopup.java
@@ -176,7 +176,7 @@ public abstract class ArrowPopup<T extends StatefulActivity<LauncherState>>
                         (int) argb.evaluate((i + 1) * step, primaryColor, secondaryColor);
             }
 
-            if (Utilities.ATLEAST_S) {
+            if (supportsColorExtraction()) {
                 setupColorExtraction();
             }
         }
@@ -704,6 +704,13 @@ public abstract class ArrowPopup<T extends StatefulActivity<LauncherState>>
             mColorExtractor.removeLocations();
             mColorExtractor.setListener(null);
         }
+    }
+
+    /**
+     * Returns whether color extraction is supported.
+     */
+    public boolean supportsColorExtraction() {
+        return Utilities.ATLEAST_S;
     }
 
     /**
