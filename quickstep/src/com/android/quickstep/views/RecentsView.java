09aa748b7dbd59f0fb2daaae6492ca3df815e711
Tracy Zhou <tracyzhou@google.com>
2021-05-10 13:40:04 -0700

    Check if mRecentsAnimationController is null before switching to screenshot
    Fixes: 187738762
    Test: N/A
    Change-Id: I3e24a6136a4ed6ff0b0ab810310aaf9dd9dbbb7b

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3607,6 +3607,12 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
      * capturing the snapshot at the same time.
      */
     public void switchToScreenshot(Runnable onFinishRunnable) {
+        if (mRecentsAnimationController == null) {
+            if (onFinishRunnable != null) {
+                onFinishRunnable.run();
+            }
+            return;
+        }
         switchToScreenshot(mRunningTaskId == -1 ? null
                 : mRecentsAnimationController.screenshotTask(mRunningTaskId), onFinishRunnable);
     }
