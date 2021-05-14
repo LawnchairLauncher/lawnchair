7d2dfcfb3f6194d84326ee2d36a3b182cc71a209
Jon Miranda <jonmiranda@google.com>
2021-05-14 13:07:43 -0700

    Add WorkspaceRevealAnim, successor to StaggeredWorkspaceAnim.
    - Added support for widgets for the new close animation.
    - This anim is used with the new app close.
    - This anim is not compataible with the old app close, so we need to
      keep StaggeredWorkspaceAnim in case we turn the app close flag off.
    
    - We will also use this anim in the lockscreen to launcher anim, will
      connect them in a follow up CL.
    
    Bug: 188413065
    Bug: 173107751
    Test: manual
    Change-Id: Icab3fd700f2c268bb1c4509a37466e4e6748f789

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -102,10 +102,10 @@ import com.android.quickstep.util.MotionPauseDetector;
 import com.android.quickstep.util.ProtoTracer;
 import com.android.quickstep.util.RecentsOrientedState;
 import com.android.quickstep.util.RectFSpringAnim;
-import com.android.quickstep.util.StaggeredWorkspaceAnim;
 import com.android.quickstep.util.SurfaceTransactionApplier;
 import com.android.quickstep.util.SwipePipToHomeAnimator;
 import com.android.quickstep.util.TransformParams;
+import com.android.quickstep.util.WorkspaceRevealAnim;
 import com.android.quickstep.views.RecentsView;
 import com.android.quickstep.views.TaskView;
 import com.android.systemui.shared.recents.model.ThumbnailData;
@@ -201,7 +201,7 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
             STATE_LAUNCHER_PRESENT | STATE_LAUNCHER_DRAWN | STATE_LAUNCHER_STARTED;
 
     public static final long MAX_SWIPE_DURATION = 350;
-    public static final long HOME_DURATION = StaggeredWorkspaceAnim.DURATION_MS;
+    public static final long HOME_DURATION = WorkspaceRevealAnim.DURATION_MS;
 
     public static final float MIN_PROGRESS_FOR_OVERVIEW = 0.7f;
     private static final float SWIPE_DURATION_MULTIPLIER =
@@ -1126,6 +1126,7 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
                 windowAnim.start(mContext, velocityPxPerMs);
                 mRunningWindowAnim = RunningWindowAnim.wrap(windowAnim);
             }
+            homeAnimFactory.setSwipeVelocity(velocityPxPerMs.y);
             homeAnimFactory.playAtomicAnimation(velocityPxPerMs.y);
             mLauncherTransitionController = null;
 
