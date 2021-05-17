2fe85a9aa5035f9b8d274f01fbf02536bd95c56d
Patryk Michalik <contact@patrykmichalik.com>
2021-05-17 21:18:41 +0200

    Optimise ‘pageMeta.kt’

--- lawnchair/src/app/lawnchair/util/pageMeta.kt
+++ lawnchair/src/app/lawnchair/util/pageMeta.kt
@@ -3,12 +3,11 @@ package app.lawnchair.util
 class MetaState(val title: String, val topBarFloating: Boolean)
 class Meta(val title: String? = null, val topBarFloating: Boolean? = null)
 
-private fun <T> getFromPropsList(propsList: List<Meta>, defaultValue: T, extractor: (meta: Meta) -> T?): T {
-    return propsList
+private fun <T> getFromPropsList(propsList: List<Meta>, defaultValue: T, extractor: (meta: Meta) -> T?): T =
+    propsList
         .asSequence()
         .map(extractor)
         .lastOrNull { it != null } ?: defaultValue
-}
 
 val pageMeta = createSideEffect<MetaState, Meta> { propsList ->
     MetaState(
