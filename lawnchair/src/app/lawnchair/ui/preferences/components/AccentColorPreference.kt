afebf1bdb7a79bac0bfb3f17d5262c09c17bc20e
Patryk Michalik <contact@patrykmichalik.com>
2021-05-18 18:40:21 +0200

    `AccentColorPreference`: Add Close icon

--- lawnchair/src/app/lawnchair/ui/preferences/components/AccentColorPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/AccentColorPreference.kt
@@ -5,9 +5,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
@@ -30,6 +30,7 @@ import kotlinx.coroutines.launch
 @Composable
 fun AccentColorPreference(showDivider: Boolean = true) {
     val scope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
     var accentColor by preferenceManager().accentColor.getAdapter()
 
     val presets = listOf(
@@ -47,11 +48,32 @@ fun AccentColorPreference(showDivider: Boolean = true) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .navigationBarsPadding()
-                    .padding(16.dp)
             ) {
-                Text(text = stringResource(id = R.string.accent_color), style = MaterialTheme.typography.h6)
-                Spacer(modifier = Modifier.requiredHeight(16.dp))
-                Row {
+                Row(
+                    modifier = Modifier
+                        .height(56.dp)
+                        .padding(start = 8.dp, end = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .width(40.dp)
+                            .height(40.dp)
+                            .clip(CircleShape)
+                            .clickable { scope.launch { sheetState.hide() } },
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.Close,
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onSurface
+                        )
+                    }
+                    Spacer(modifier = Modifier.requiredWidth(8.dp))
+                    Text(text = stringResource(id = R.string.accent_color), style = MaterialTheme.typography.h6)
+                }
+                Spacer(modifier = Modifier.requiredHeight(8.dp))
+                Row(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
                     presets.forEachIndexed { index, it ->
                         ColorSwatch(
                             color = it,
@@ -63,7 +85,8 @@ fun AccentColorPreference(showDivider: Boolean = true) {
                     }
                 }
             }
-        }
+        },
+        sheetState = sheetState
     ) { showSheet ->
         ClickablePreference(
             label = stringResource(id = R.string.accent_color),
