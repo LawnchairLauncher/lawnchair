0e25d2eba84c9691471fc3899c6f19958e90793c
Wale Ogunwale <ogunwale@google.com>
2021-06-01 22:07:22 +0000

    Revert "Add haptic feedback to Launcher recent apps scrolling"
    This reverts commit b1ef5e5c55ee4e86124afb554fc48a1cd8791ea4.
    
    Reason for revert: b/182382085#comment4
    
    Change-Id: I001944da64dc05324b14cc9dd3db5921faf8806c
    Bug: 182382085

--- src/com/android/launcher3/PagedView.java
+++ src/com/android/launcher3/PagedView.java
@@ -479,11 +479,9 @@ public abstract class PagedView<T extends View & PageIndicator> extends ViewGrou
                 if (newPos < mMinScroll && oldPos >= mMinScroll) {
                     mEdgeGlowLeft.onAbsorb((int) mScroller.getCurrVelocity());
                     mScroller.abortAnimation();
-                    onEdgeAbsorbingScroll();
                 } else if (newPos > mMaxScroll && oldPos <= mMaxScroll) {
                     mEdgeGlowRight.onAbsorb((int) mScroller.getCurrVelocity());
                     mScroller.abortAnimation();
-                    onEdgeAbsorbingScroll();
                 }
             }
 
@@ -1333,13 +1331,6 @@ public abstract class PagedView<T extends View & PageIndicator> extends ViewGrou
     }
 
     protected void onNotSnappingToPageInFreeScroll() { }
-
-    /**
-     * Called when the view edges absorb part of the scroll. Subclasses can override this
-     * to provide custom behavior during animation.
-     */
-    protected void onEdgeAbsorbingScroll() {
-    }
 
     protected boolean shouldFlingForVelocity(int velocity) {
         float threshold = mAllowEasyFling ? mEasyFlingThresholdVelocity : mFlingThresholdVelocity;
