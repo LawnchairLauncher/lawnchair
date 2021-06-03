4866a73ff41e9c631ad95f705bc60c5be457785a
Patryk Michalik <contact@patrykmichalik.com>
2021-06-03 21:10:11 +0200

    Settings: Show active icon pack name in Icon Pack list entry

--- lawnchair/src/app/lawnchair/ui/preferences/GeneralPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/GeneralPreferences.kt
@@ -56,7 +56,10 @@ fun GeneralPreferences() {
             NavigationActionPreference(
                 label = stringResource(id = R.string.icon_pack),
                 destination = subRoute(name = GeneralRoutes.ICON_PACK),
-                showDivider = false
+                showDivider = false,
+                subtitle =
+                LocalPreferenceInteractor.current.getIconPacks()
+                    .find { it.packageName == preferenceManager().iconPackPackage.get() }?.name
             )
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
