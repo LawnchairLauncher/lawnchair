2e6df793332fe56b8666b1f9d971cfb1771f86cd
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-06-05 10:19:29 +0700

    Send push notification to chat

--- send_changelog.py
+++ send_changelog.py
@@ -4,17 +4,18 @@ import requests
 
 github_event_before = os.getenv('GITHUB_EVENT_BEFORE')
 github_sha = os.getenv('GITHUB_SHA')
+github_repo = os.getenv('GITHUB_REPO')
 telegram_ci_bot_token = os.getenv('TELEGRAM_CI_BOT_TOKEN')
 telegram_ci_channel_id = os.getenv('TELEGRAM_CI_CHANNEL_ID')
 
 repository = git.Repo('.')
-commits = repository.iter_commits(f'{github_event_before}...{github_sha}')
-message = ''
+commits_range = f'{github_event_before}...{github_sha}'
+commits = list(repository.iter_commits(commits_range))
+message = f'🔨 [{len(commits)} new commits](https://github.com/{github_repo}/compare/{commits_range}) to lawnchair:11-dev:\n'
 
-for index, commit in enumerate(commits):
+for commit in commits:
   commit_message = commit.message.split('\n')[0].replace('_', '\\_')
-  if (index != 0):
-    message += '\n'
-  message += f'''• [{repository.git.rev_parse(commit.hexsha, short=7)}](https://github.com/LawnchairLauncher/lawnchair/commit/{commit.hexsha}): {commit_message}'''
+  message += f'''\n• [{repository.git.rev_parse(commit.hexsha, short=7)}](https://github.com/LawnchairLauncher/lawnchair/commit/{commit.hexsha}): {commit_message}'''
 
-requests.get(f'''https://api.telegram.org/bot{telegram_ci_bot_token}/sendMessage?chat_id={telegram_ci_channel_id}&parse_mode=Markdown&text={message}&disable_web_page_preview=true''')
+print(message)
+# requests.get(f'''https://api.telegram.org/bot{telegram_ci_bot_token}/sendMessage?chat_id={telegram_ci_channel_id}&parse_mode=Markdown&text={message}&disable_web_page_preview=true''')
