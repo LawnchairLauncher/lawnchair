e6b291e64819ee2228a33d94923983538f91df53
Patryk Michalik <contact@patrykmichalik.com>
2021-06-05 21:18:23 +0200

    ‘send_changelog.py’: Escape hashtags

--- send_changelog.py
+++ send_changelog.py
@@ -15,7 +15,7 @@ commits = list(repository.iter_commits(commits_range))
 message = f'''**🔨 [{len(commits)} new {'commit' if len(commits) == 1 else 'commits'}](https://github.com/{github_repo}/compare/{commits_range}) to `lawnchair:{branch}`:**\n'''
 
 for commit in commits:
-  commit_message = commit.message.split('\n')[0].replace('_', '\\_')
+  commit_message = commit.message.split('\n')[0].replace('_', '\\_').replace('#', '\\#')
   message += f'''\n• [{repository.git.rev_parse(commit.hexsha, short=7)}](https://github.com/LawnchairLauncher/lawnchair/commit/{commit.hexsha}): {commit_message}'''
 
 requests.get(f'''https://api.telegram.org/bot{telegram_ci_bot_token}/sendMessage?chat_id={telegram_ci_channel_id}&parse_mode=Markdown&text={message}&disable_web_page_preview=true''')
