b31b245b6ee569afeb33f9184d396093af09a35c
Patryk Michalik <contact@patrykmichalik.com>
2021-06-05 19:58:53 +0200

    ‘Build release APK’ Action: Create changelog for each build

--- send_changelog.py
+++ send_changelog.py
@@ -7,11 +7,12 @@ github_sha = os.getenv('GITHUB_SHA')
 github_repo = os.getenv('GITHUB_REPO')
 telegram_ci_bot_token = os.getenv('TELEGRAM_CI_BOT_TOKEN')
 telegram_ci_channel_id = os.getenv('TELEGRAM_CI_CHANNEL_ID')
+branch = os.getenv('BRANCH')
 
 repository = git.Repo('.')
 commits_range = f'{github_event_before}...{github_sha}'
 commits = list(repository.iter_commits(commits_range))
-message = f'''🔨 [{len(commits)} new {'commit' if len(commits) == 1 else 'commits'}](https://github.com/{github_repo}/compare/{commits_range}) to `lawnchair:11-dev`:\n'''
+message = f'''🔨 [{len(commits)} new {'commit' if len(commits) == 1 else 'commits'}](https://github.com/{github_repo}/compare/{commits_range}) to `lawnchair:{branch}`:\n'''
 
 for commit in commits:
   commit_message = commit.message.split('\n')[0].replace('_', '\\_')
