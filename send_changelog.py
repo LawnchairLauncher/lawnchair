03429420b6245075634f7619acf2aa6e2c944b5f
Patryk Michalik <contact@patrykmichalik.com>
2021-06-06 09:41:40 +0200

    ‘send_changelog.py’: Use percent-encoding

--- send_changelog.py
+++ send_changelog.py
@@ -1,6 +1,7 @@
 import git
 import os
 import requests
+import urllib
 
 github_event_before = os.getenv('GITHUB_EVENT_BEFORE')
 github_sha = os.getenv('GITHUB_SHA')
@@ -15,7 +16,7 @@ commits = list(repository.iter_commits(commits_range))
 message = f'''**🔨 [{len(commits)} new {'commit' if len(commits) == 1 else 'commits'}](https://github.com/{github_repo}/compare/{commits_range}) to `lawnchair:{branch}`:**\n'''
 
 for commit in commits:
-  commit_message = commit.message.split('\n')[0].replace('_', '\\_').replace('#', '\\#').replace('&', '\\&')
+  commit_message = urllib.parse.quote(commit.message.split('\n')[0].replace('_', '\\_'), safe='')
   message += f'''\n• [{repository.git.rev_parse(commit.hexsha, short=7)}](https://github.com/LawnchairLauncher/lawnchair/commit/{commit.hexsha}): {commit_message}'''
 
 requests.get(f'''https://api.telegram.org/bot{telegram_ci_bot_token}/sendMessage?chat_id={telegram_ci_channel_id}&parse_mode=Markdown&text={message}&disable_web_page_preview=true''')
