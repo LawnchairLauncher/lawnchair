665edcf38f6ed1ffe838bec724749459e16b143e
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-06-06 14:56:04 +0700

    Send changelog as HTML in the request body

--- send_changelog.py
+++ send_changelog.py
@@ -1,7 +1,7 @@
 import git
+import html
 import os
 import requests
-import urllib
 
 github_event_before = os.getenv('GITHUB_EVENT_BEFORE')
 github_sha = os.getenv('GITHUB_SHA')
@@ -13,10 +13,23 @@ branch = os.getenv('BRANCH')
 repository = git.Repo('.')
 commits_range = f'{github_event_before}...{github_sha}'
 commits = list(repository.iter_commits(commits_range))
-message = f'''**🔨 [{len(commits)} new {'commit' if len(commits) == 1 else 'commits'}](https://github.com/{github_repo}/compare/{commits_range}) to `lawnchair:{branch}`:**\n'''
+
+overview_link = f'https://github.com/{github_repo}/compare/{commits_range}'
+overview_link_tag = f'''<a href="{overview_link}">{len(commits)} new {'commit' if len(commits) == 1 else 'commits'}</a>'''
+message = f'''<b>🔨 {overview_link_tag} to <code>lawnchair:{branch}</code>:</b>\n'''
 
 for commit in commits:
-  commit_message = urllib.parse.quote(commit.message.split('\n')[0].replace('_', '\\_'), safe='')
-  message += f'''\n• [{repository.git.rev_parse(commit.hexsha, short=7)}](https://github.com/LawnchairLauncher/lawnchair/commit/{commit.hexsha}): {commit_message}'''
+  commit_message = commit.message.split('\n')[0]
+  commit_link = f'https://github.com/{github_repo}/commit/{commit.hexsha}'
+  commit_link_tag = f'<a href="{commit_link}">{repository.git.rev_parse(commit.hexsha, short=7)}</a>'
+  encoded_message = html.escape(commit_message)
+  message += f'\n• {commit_link_tag}: {encoded_message}'
 
-requests.get(f'''https://api.telegram.org/bot{telegram_ci_bot_token}/sendMessage?chat_id={telegram_ci_channel_id}&parse_mode=Markdown&text={message}&disable_web_page_preview=true''')
+data = {
+  'chat_id': telegram_ci_channel_id,
+  'parse_mode': 'HTML',
+  'text': message,
+  'disable_web_page_preview': 'true',
+  'disable_notification': 'true'
+}
+r = requests.post(f'https://api.telegram.org/bot{telegram_ci_bot_token}/sendMessage', data)
