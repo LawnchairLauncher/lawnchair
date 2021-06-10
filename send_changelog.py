7f71b7d47cb1bea7c83559edb309dce7c43a4f00
Patryk Michalik <contact@patrykmichalik.com>
2021-06-10 18:26:46 +0200

    Remove redundant environment variable declarations from Workflows

--- send_changelog.py
+++ send_changelog.py
@@ -5,7 +5,7 @@ import requests
 
 github_event_before = os.getenv('GITHUB_EVENT_BEFORE')
 github_sha = os.getenv('GITHUB_SHA')
-github_repo = os.getenv('GITHUB_REPO')
+github_repository = os.getenv('GITHUB_REPOSITORY')
 telegram_ci_bot_token = os.getenv('TELEGRAM_CI_BOT_TOKEN')
 telegram_ci_channel_id = os.getenv('TELEGRAM_CI_CHANNEL_ID')
 branch = os.getenv('BRANCH')
@@ -14,13 +14,13 @@ repository = git.Repo('.')
 commits_range = f'{github_event_before}...{github_sha}'
 commits = list(repository.iter_commits(commits_range))
 
-overview_link = f'https://github.com/{github_repo}/compare/{commits_range}'
+overview_link = f'https://github.com/{github_repository}/compare/{commits_range}'
 overview_link_tag = f'''<a href="{overview_link}">{len(commits)} new commit{'s' if len(commits) > 1 else ''}</a>'''
 message = f'''<b>🔨 {overview_link_tag} to <code>lawnchair:{branch}</code>:</b>\n'''
 
 for commit in commits:
     commit_message = commit.message.split('\n')[0]
-    commit_link = f'https://github.com/{github_repo}/commit/{commit.hexsha}'
+    commit_link = f'https://github.com/{github_repository}/commit/{commit.hexsha}'
     commit_link_tag = f'<a href="{commit_link}">{repository.git.rev_parse(commit.hexsha, short=7)}</a>'
     encoded_message = html.escape(commit_message)
     message += f'\n• {commit_link_tag}: {encoded_message}'
