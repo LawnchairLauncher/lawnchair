e92329fc2d38d3d00b38fcb7c6af434c05476d7a
Patryk Michalik <contact@patrykmichalik.com>
2021-06-25 19:29:56 +0200

    `AnimatedCheck`: Use `Crossfade`

--- lawnchair/src/app/lawnchair/ui/preferences/components/AnimatedCheck.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/AnimatedCheck.kt
@@ -1,9 +1,6 @@
 package app.lawnchair.ui.preferences.components
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
+import androidx.compose.animation.*
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
@@ -19,16 +16,14 @@ import androidx.compose.ui.unit.dp
 @ExperimentalAnimationApi
 fun AnimatedCheck(visible: Boolean, tint: Color = MaterialTheme.colors.primary) {
     Box(modifier = Modifier.size(24.dp, 24.dp)) {
-        AnimatedVisibility(
-            visible = visible,
-            enter = fadeIn(),
-            exit = fadeOut()
-        ) {
-            Icon(
-                imageVector = Icons.Rounded.Check,
-                contentDescription = null,
-                tint = tint
-            )
+        Crossfade(targetState = visible) {
+            if (it) {
+                Icon(
+                    imageVector = Icons.Rounded.Check,
+                    contentDescription = null,
+                    tint = tint
+                )
+            }
         }
     }
 }
