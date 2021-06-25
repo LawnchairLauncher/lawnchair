b08e9ef7d3b62e028eb7276912bafebcc1dfb685
Patryk Michalik <contact@patrykmichalik.com>
2021-06-25 19:44:54 +0200

    Simplify `Modifier#addIf`

--- lawnchair/src/app/lawnchair/ui/util/modifierIf.kt
+++ lawnchair/src/app/lawnchair/ui/util/modifierIf.kt
@@ -2,6 +2,5 @@ package app.lawnchair.ui.util
 
 import androidx.compose.ui.Modifier
 
-inline fun Modifier.addIf(condition: Boolean, crossinline factory: Modifier.() -> Modifier): Modifier {
-    return if (condition) factory(this) else this
-}
+inline fun Modifier.addIf(condition: Boolean, crossinline factory: Modifier.() -> Modifier): Modifier =
+    if (condition) factory() else this
