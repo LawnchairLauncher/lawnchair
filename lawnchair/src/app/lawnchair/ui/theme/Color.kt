a477cf5a16240bd569c71ad67b93e3b277a3008e
Patryk Michalik <contact@patrykmichalik.com>
2021-06-25 16:56:48 +0200

    `Context#getSystemAccent`: Use `Theme_DeviceDefault_DayNight`

--- lawnchair/src/app/lawnchair/ui/theme/Color.kt
+++ lawnchair/src/app/lawnchair/ui/theme/Color.kt
@@ -45,8 +45,8 @@ fun Context.getSystemAccent(darkTheme: Boolean): Int {
         res.getColor(colorId)
     } else {
         val typedValue = TypedValue()
-        val contextWrapper = ContextThemeWrapper(this, R.style.AppTheme)
+        val contextWrapper = ContextThemeWrapper(this, android.R.style.Theme_DeviceDefault_DayNight)
         contextWrapper.theme.resolveAttribute(android.R.attr.colorAccent, typedValue, true)
-        if (darkTheme) lightenColor(typedValue.data) else typedValue.data
+        typedValue.data
     }
 }
