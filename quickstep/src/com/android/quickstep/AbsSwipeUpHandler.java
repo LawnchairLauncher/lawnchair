ffa6652c25f0d32df3939fc24d574d5293cb45a0
Evan Rosky <erosky@google.com>
2021-07-02 17:21:21 -0700

    Include shell+stage-split in legacy recents animations
    This just builds the infrastructure and then, just as
    an example/proof-of-concept, uses the infra to show/hide
    the divider-bar when it makes sense.
    
    Bug: 192292305
    Test: enable SPLIT_SELECT, enter split, use recents and observe
          the divider bar appropriately showing/hiding
    Change-Id: Iea0704b3f7821c3056a1675f11e671e81305831f

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -740,6 +740,8 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
             mRecentsAnimationStartCallbacks.clear();
         }
 
+        TaskViewUtils.setDividerBarShown(mRecentsAnimationTargets.nonApps, false);
+
         // Only add the callback to enable the input consumer after we actually have the controller
         mStateCallback.runOnceAtState(STATE_APP_CONTROLLER_RECEIVED | STATE_GESTURE_STARTED,
                 mRecentsAnimationController::enableInputConsumer);
@@ -753,6 +755,8 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
         ActiveGestureLog.INSTANCE.addLog("cancelRecentsAnimation");
         mActivityInitListener.unregister();
         mStateCallback.setStateOnUiThread(STATE_GESTURE_CANCELLED | STATE_HANDLER_INVALIDATED);
+
+        TaskViewUtils.setDividerBarShown(mRecentsAnimationTargets.nonApps, true);
 
         // Defer clearing the controller and the targets until after we've updated the state
         mRecentsAnimationController = null;
@@ -882,6 +886,7 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
                 break;
             case LAST_TASK:
                 mStateCallback.setState(STATE_RESUME_LAST_TASK);
+                TaskViewUtils.setDividerBarShown(mRecentsAnimationTargets.nonApps, true);
                 break;
         }
         ActiveGestureLog.INSTANCE.addLog("onSettledOnEndTarget " + endTarget);
@@ -1723,6 +1728,9 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
 
     @Override
     public void onRecentsAnimationFinished(RecentsAnimationController controller) {
+        if (!controller.getFinishTargetIsLauncher()) {
+            TaskViewUtils.setDividerBarShown(mRecentsAnimationTargets.nonApps, true);
+        }
         mRecentsAnimationController = null;
         mRecentsAnimationTargets = null;
         if (mRecentsView != null) {
