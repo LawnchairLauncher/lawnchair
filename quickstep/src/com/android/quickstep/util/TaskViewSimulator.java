145bd7474935a4cde1e2965e7a34943695463151
Tracy Zhou <tracyzhou@google.com>
2021-07-20 15:58:19 -0700

    [DO NOT MERGE] Disable live tile in split mode for S
    Recall link: https://recall.googleplex.com/projects/debb8388-5e38-42c5-99ab-20bdfb92d25e/sessions/655af962-b4bd-4dc4-93aa-178852c0d701
    
    Bug: 193991436
    Test: enter split screen and swipe up
    Change-Id: I7adffcdefb12d225db207542716a436f29df0377

--- quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
+++ quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
@@ -56,6 +56,7 @@ public class TaskViewSimulator implements TransformParams.BuilderProxy {
 
     private final Context mContext;
     private final BaseActivityInterface mSizeStrategy;
+    private final boolean mIsForLiveTile;
 
     @NonNull
     private RecentsOrientedState mOrientationState;
@@ -93,8 +94,14 @@ public class TaskViewSimulator implements TransformParams.BuilderProxy {
     private int mOrientationStateId;
 
     public TaskViewSimulator(Context context, BaseActivityInterface sizeStrategy) {
+        this(context, sizeStrategy, false);
+    }
+
+    public TaskViewSimulator(Context context, BaseActivityInterface sizeStrategy,
+            boolean isForLiveTile) {
         mContext = context;
         mSizeStrategy = sizeStrategy;
+        mIsForLiveTile = isForLiveTile;
 
         // TODO(b/187074722): Don't create this per-TaskViewSimulator
         mOrientationState = TraceHelper.allowIpcs("",
@@ -302,7 +309,8 @@ public class TaskViewSimulator implements TransformParams.BuilderProxy {
                 .withWindowCrop(mTmpCropRect)
                 .withCornerRadius(getCurrentCornerRadius());
 
-        if (ENABLE_QUICKSTEP_LIVE_TILE.get() && params.getRecentsSurface() != null) {
+        if (ENABLE_QUICKSTEP_LIVE_TILE.get() && mIsForLiveTile
+                && params.getRecentsSurface() != null) {
             // When relativeLayer = 0, it reverts the surfaces back to the original order.
             builder.withRelativeLayerTo(params.getRecentsSurface(),
                     mDrawsBelowRecents ? Integer.MIN_VALUE : 0);
