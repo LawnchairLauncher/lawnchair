13f3ffed950ad1cf1956968fd9901ae4edb51297
Andy Wickham <awickham@google.com>
2021-07-30 16:00:20 -1000

    Adds initial Taskbar Edu implementation.
    Currently it just pops up with one placeholder image for splitscreen.
    The Next button doesn't do anything.
    
    Screenshots:
    Light: https://screenshot.googleplex.com/B27CwBcwobHTghN.png
    Dark: https://screenshot.googleplex.com/48EuJZv8evd5RGW.png
    
    Known issues:
     - Back gesture is picked up behind by the app behind the taskbar.
     - When the flag is enabled, the Edu shows up each time you open
       an app, rather than only the first time.
    
    Bug: 180605356
    Test: Manual
    Change-Id: Ifba9aa59abf1501b5f8ddebb3fcc13df85f6efaa

--- quickstep/src/com/android/launcher3/taskbar/LauncherTaskbarUIController.java
+++ quickstep/src/com/android/launcher3/taskbar/LauncherTaskbarUIController.java
@@ -32,6 +32,7 @@ import com.android.launcher3.LauncherState;
 import com.android.launcher3.QuickstepTransitionManager;
 import com.android.launcher3.R;
 import com.android.launcher3.anim.AnimatorListeners;
+import com.android.launcher3.config.FeatureFlags;
 import com.android.launcher3.util.MultiValueAlpha;
 import com.android.launcher3.util.MultiValueAlpha.AlphaProperty;
 import com.android.quickstep.AnimatedFloat;
@@ -259,6 +260,28 @@ public class LauncherTaskbarUIController extends TaskbarUIController {
      */
     public void forceHideBackground(boolean forceHide) {
         mTaskbarOverrideBackgroundAlpha.updateValue(forceHide ? 0 : 1);
+    }
+
+    /**
+     * Starts the taskbar education flow, if the user hasn't seen it yet.
+     */
+    public void showEdu() {
+        if (!FeatureFlags.ENABLE_TASKBAR_EDU.get()) {
+            return;
+        }
+
+        mControllers.taskbarEduController.showEdu();
+    }
+
+    /**
+     * Manually ends the taskbar education flow.
+     */
+    public void hideEdu() {
+        if (!FeatureFlags.ENABLE_TASKBAR_EDU.get()) {
+            return;
+        }
+
+        mControllers.taskbarEduController.hideEdu();
     }
 
     private final class TaskBarRecentsAnimationListener implements RecentsAnimationListener {
