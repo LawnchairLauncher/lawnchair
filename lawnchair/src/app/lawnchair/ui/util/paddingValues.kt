f3dbcafe756e63961e81d97792d5b26731683f5a
Patryk Michalik <contact@patrykmichalik.com>
2021-08-05 16:25:06 +0200

    Reformat ‘paddingValues.kt’

--- lawnchair/src/app/lawnchair/ui/util/paddingValues.kt
+++ lawnchair/src/app/lawnchair/ui/util/paddingValues.kt
@@ -14,8 +14,7 @@ fun rememberExtendPadding(
     top: Dp = 0.dp,
     end: Dp = 0.dp,
     bottom: Dp = 0.dp
-): PaddingValues {
-    return remember(start, top, end, bottom) {
+): PaddingValues = remember(start, top, end, bottom) {
         CombinePaddingValues(padding, PaddingValues(
             start = start,
             top = top,
@@ -23,22 +22,17 @@ fun rememberExtendPadding(
             bottom = bottom
         ))
     }
-}
 
 private class CombinePaddingValues(private val a: PaddingValues, private val b: PaddingValues) : PaddingValues {
-    override fun calculateLeftPadding(layoutDirection: LayoutDirection): Dp {
-        return a.calculateLeftPadding(layoutDirection) + b.calculateLeftPadding(layoutDirection)
-    }
+    override fun calculateLeftPadding(layoutDirection: LayoutDirection) =
+        a.calculateLeftPadding(layoutDirection) + b.calculateLeftPadding(layoutDirection)
 
-    override fun calculateTopPadding(): Dp {
-        return a.calculateTopPadding() + b.calculateTopPadding()
-    }
+    override fun calculateTopPadding() =
+        a.calculateTopPadding() + b.calculateTopPadding()
 
-    override fun calculateRightPadding(layoutDirection: LayoutDirection): Dp {
-        return a.calculateRightPadding(layoutDirection) + b.calculateRightPadding(layoutDirection)
-    }
+    override fun calculateRightPadding(layoutDirection: LayoutDirection) =
+        a.calculateRightPadding(layoutDirection) + b.calculateRightPadding(layoutDirection)
 
-    override fun calculateBottomPadding(): Dp {
-        return a.calculateBottomPadding() + b.calculateBottomPadding()
-    }
+    override fun calculateBottomPadding() =
+        a.calculateBottomPadding() + b.calculateBottomPadding()
 }
