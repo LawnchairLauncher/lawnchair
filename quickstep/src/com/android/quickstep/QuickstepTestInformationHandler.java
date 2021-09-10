35231f30418d0bf0c3c4b993f7abff89e0a8e828
Pat Manning <patmanning@google.com>
2021-09-10 09:44:19 +0000

    Add TAPL tests for hiding the actions view when scrolling away from focused task in overview.
    Test: TaplTestsQuickstep.java
    Bug: 197630182
    Change-Id: Ie44a5bd99d7c9ffbc5405973c0ba1950cd90665c

--- quickstep/src/com/android/quickstep/QuickstepTestInformationHandler.java
+++ quickstep/src/com/android/quickstep/QuickstepTestInformationHandler.java
@@ -2,6 +2,7 @@ package com.android.quickstep;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
 import android.os.Bundle;
 
 import com.android.launcher3.LauncherState;
@@ -56,6 +57,17 @@ public class QuickstepTestInformationHandler extends TestInformationHandler {
             case TestProtocol.REQUEST_OVERVIEW_SHARE_ENABLED: {
                 response.putBoolean(TestProtocol.TEST_INFO_RESPONSE_FIELD,
                         FeatureFlags.ENABLE_OVERVIEW_SHARE.get());
+                return response;
+            }
+
+            case TestProtocol.REQUEST_GET_FOCUSED_TASK_WIDTH_FOR_TABLET: {
+                if (!mDeviceProfile.isTablet) {
+                    return null;
+                }
+                Rect focusedTaskRect = new Rect();
+                LauncherActivityInterface.INSTANCE.calculateTaskSize(mContext, mDeviceProfile,
+                        focusedTaskRect);
+                response.putInt(TestProtocol.TEST_INFO_RESPONSE_FIELD, focusedTaskRect.width());
                 return response;
             }
         }
