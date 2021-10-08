ffa72ef5afc6472c242abb1d36a3482f6808ecdc
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-08 16:56:58 +0700

    Sync keyboard hiding animation with app drawer

--- lawnchair/src/app/lawnchair/util/PendingAnimation.kt
+++ lawnchair/src/app/lawnchair/util/PendingAnimation.kt
@@ -3,18 +3,18 @@ package app.lawnchair.util
 import android.animation.Animator
 import com.android.launcher3.anim.PendingAnimation
 
-fun PendingAnimation.runOnEnd(block: () -> Unit) {
+fun PendingAnimation.runOnEnd(block: (isSuccess: Boolean) -> Unit) {
     addListener(object : Animator.AnimatorListener {
         override fun onAnimationStart(animation: Animator) {
 
         }
 
         override fun onAnimationEnd(animation: Animator) {
-            block()
+            block(true)
         }
 
         override fun onAnimationCancel(animation: Animator) {
-
+            block(false)
         }
 
         override fun onAnimationRepeat(animation: Animator) {
