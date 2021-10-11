edc5e798abb660f1d0d2d31ff2d219518e1d4d1d
Patryk Michalik <contact@patrykmichalik.com>
2021-10-11 17:32:49 +0200

    Refine sizes and positions of icons in Dock search bar

--- lawnchair/src/app/lawnchair/qsb/AssistantIconView.kt
+++ lawnchair/src/app/lawnchair/qsb/AssistantIconView.kt
@@ -4,15 +4,15 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.util.AttributeSet
-import android.widget.ImageView
+import android.widget.ImageButton
 import com.android.launcher3.R
 import com.android.launcher3.qsb.QsbContainerView
 
 @SuppressLint("AppCompatCustomView")
-class AssistantIconView(context: Context, attrs: AttributeSet?) : ImageView(context, attrs) {
+class AssistantIconView(context: Context, attrs: AttributeSet?) : ImageButton(context, attrs) {
 
     init {
-        scaleType = ScaleType.CENTER
+        scaleType = ScaleType.FIT_CENTER
         setOnClickListener {
             val intent = Intent(Intent.ACTION_VOICE_COMMAND)
                 .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
