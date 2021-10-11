6982132b898dcf43796e81acb673bc829ee673c8
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-12 01:35:55 +0700

    Don't show mic icon if assistant is not available

--- lawnchair/src/app/lawnchair/qsb/AssistantIconView.kt
+++ lawnchair/src/app/lawnchair/qsb/AssistantIconView.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.util.AttributeSet
 import android.widget.ImageButton
+import androidx.core.view.isVisible
 import com.android.launcher3.R
 import com.android.launcher3.qsb.QsbContainerView
 
@@ -12,11 +13,15 @@ import com.android.launcher3.qsb.QsbContainerView
 class AssistantIconView(context: Context, attrs: AttributeSet?) : ImageButton(context, attrs) {
 
     init {
+        val intent = Intent(Intent.ACTION_VOICE_COMMAND)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            .setPackage(QsbContainerView.getSearchWidgetPackageName(context))
+        if (context.packageManager.resolveActivity(intent, 0) == null) {
+            isVisible = false
+        }
+
         scaleType = ScaleType.FIT_CENTER
         setOnClickListener {
-            val intent = Intent(Intent.ACTION_VOICE_COMMAND)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                .setPackage(QsbContainerView.getSearchWidgetPackageName(context))
             context.startActivity(intent)
         }
     }
