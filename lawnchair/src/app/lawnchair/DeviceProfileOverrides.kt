99e82656fc6cfeb88d71fd330e46b7b8d1174887
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-12 11:05:58 +0700

    Refactor grid override logic

--- lawnchair/src/app/lawnchair/DeviceProfileOverrides.kt
+++ lawnchair/src/app/lawnchair/DeviceProfileOverrides.kt
@@ -9,21 +9,60 @@ class DeviceProfileOverrides(context: Context) {
     private val prefs = PreferenceManager.getInstance(context)
 
     fun apply(idp: InvariantDeviceProfile, defaultGrid: InvariantDeviceProfile.GridOption) {
+        val options = Options(prefs, defaultGrid)
+
         // apply grid size
-        idp.numShownHotseatIcons = prefs.hotseatColumns.get(defaultGrid)
-        idp.numRows = prefs.workspaceRows.get(defaultGrid)
-        idp.numColumns = prefs.workspaceColumns.get(defaultGrid)
-        idp.numAllAppsColumns = prefs.allAppsColumns.get(defaultGrid)
-        idp.numFolderRows = prefs.folderRows.get(defaultGrid)
-        idp.numFolderColumns = prefs.folderColumns.get(defaultGrid)
+        idp.numShownHotseatIcons = options.numHotseatColumns
+        idp.numRows = options.numRows
+        idp.numColumns = options.numColumns
+        idp.numAllAppsColumns = options.numAllAppsColumns
+        idp.numFolderRows = options.numFolderRows
+        idp.numFolderColumns = options.numFolderColumns
 
         // apply icon and text size
-        idp.iconSize *= prefs.iconSizeFactor.get()
-        idp.iconTextSize *= (if (prefs.showHomeLabels.get()) prefs.textSizeFactor.get() else 0.0F)
-        idp.allAppsIconSize *= prefs.allAppsIconSizeFactor.get()
-        idp.allAppsIconTextSize *= (if (prefs.allAppsIconLabels.get()) prefs.allAppsTextSizeFactor.get() else 0F)
+        idp.iconSize *= options.iconSizeFactor
+        idp.iconTextSize *= (if (options.enableIconText) options.iconTextSizeFactor else 0f)
+        idp.allAppsIconSize *= options.allAppsIconSizeFactor
+        idp.allAppsIconTextSize *= (if (options.enableAllAppsIconText) options.allAppsIconTextSizeFactor else 0f)
 
         idp.dbFile = "launcher_${idp.numRows}_${idp.numColumns}_${idp.numShownHotseatIcons}.db"
+    }
+
+    data class Options(
+        var numHotseatColumns: Int,
+        var numRows: Int,
+        var numColumns: Int,
+        var numAllAppsColumns: Int,
+        var numFolderRows: Int,
+        var numFolderColumns: Int,
+
+        var iconSizeFactor: Float,
+        var enableIconText: Boolean,
+        var iconTextSizeFactor: Float,
+
+        var allAppsIconSizeFactor: Float,
+        var enableAllAppsIconText: Boolean,
+        var allAppsIconTextSizeFactor: Float
+    ) {
+        constructor(
+            prefs: PreferenceManager,
+            defaultGrid: InvariantDeviceProfile.GridOption,
+        ) : this(
+            numHotseatColumns = prefs.hotseatColumns.get(defaultGrid),
+            numRows = prefs.workspaceRows.get(defaultGrid),
+            numColumns = prefs.workspaceColumns.get(defaultGrid),
+            numAllAppsColumns = prefs.allAppsColumns.get(defaultGrid),
+            numFolderRows = prefs.folderRows.get(defaultGrid),
+            numFolderColumns = prefs.folderColumns.get(defaultGrid),
+
+            iconSizeFactor = prefs.iconSizeFactor.get(),
+            enableIconText = prefs.showHomeLabels.get(),
+            iconTextSizeFactor = prefs.textSizeFactor.get(),
+
+            allAppsIconSizeFactor = prefs.allAppsIconSizeFactor.get(),
+            enableAllAppsIconText = prefs.allAppsIconLabels.get(),
+            allAppsIconTextSizeFactor = prefs.allAppsTextSizeFactor.get()
+        )
     }
 
     companion object {
