26d9e7d5d999a250c848d92e16f7a12f31c841cc
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-15 00:01:07 +0700

    Apply accent color to search bar

--- lawnchair/src/app/lawnchair/allapps/FallbackSearchInputView.kt
+++ lawnchair/src/app/lawnchair/allapps/FallbackSearchInputView.kt
@@ -5,7 +5,11 @@ import android.content.Context
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.Gravity
+import androidx.core.graphics.ColorUtils
+import app.lawnchair.theme.color.ColorTokens
 import app.lawnchair.theme.drawable.DrawableTokens
+import app.lawnchair.util.EditTextExtensions.setCursorColor
+import app.lawnchair.util.EditTextExtensions.setTextSelectHandleColor
 import com.android.launcher3.ExtendedEditText
 import com.android.launcher3.allapps.AllAppsContainerView
 
@@ -23,6 +27,11 @@ class FallbackSearchInputView(context: Context, attrs: AttributeSet?) : Extended
 
     init {
         background = bg
+
+        val accentColor = ColorTokens.ColorAccent.resolveColor(context)
+        setCursorColor(accentColor)
+        setTextSelectHandleColor(accentColor)
+        highlightColor = ColorUtils.setAlphaComponent(accentColor, 82)
     }
 
     fun initialize(appsView: AllAppsContainerView) {
