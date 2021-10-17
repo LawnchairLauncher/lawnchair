21ab212c0edbbfb2c0e7c7368cd0bd6d48a327cf
Patryk Michalik <contact@patrykmichalik.com>
2021-10-17 17:16:45 +0200

    App Drawer: Draw navigation bar scrim only when necessary

--- src/com/android/launcher3/allapps/AllAppsContainerView.java
+++ src/com/android/launcher3/allapps/AllAppsContainerView.java
@@ -71,6 +71,7 @@ import com.android.launcher3.views.RecyclerViewFastScroller;
 import java.util.ArrayList;
 
 import app.lawnchair.allapps.LawnchairAlphabeticalAppsList;
+import app.lawnchair.preferences.PreferenceManager;
 import app.lawnchair.ui.AllAppsStretchLayout;
 
 /**
@@ -388,7 +389,8 @@ public class AllAppsContainerView extends AllAppsStretchLayout implements DragSo
     public void draw(@NonNull Canvas canvas) {
         super.draw(canvas);
 
-        if (mNavBarScrimHeight > 0) {
+        boolean shouldDrawScrim = PreferenceManager.getInstance(getContext()).getDrawerOpacity().get() > 0.3F;
+        if (mNavBarScrimHeight > 0 && shouldDrawScrim) {
             canvas.drawRect(0, getHeight() - mNavBarScrimHeight, getWidth(), getHeight(),
                     mNavBarScrimPaint);
         }
