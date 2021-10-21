26f44911a60fca6a4dc7d244e0ebc580fa23e042
Thales Lima <tsuharesu@google.com>
2021-10-21 15:34:28 +0100

    add kotlin support for launcher
    Bug: 193432925
    Test: click on the app icon and check logcat
    Change-Id: I2ca4d57105fe4e1b1dfc6c968a7c1bbd3cbd2151

---
+++ quickstep/src/com/android/quickstep/views/TaskMenuViewWithArrow.kt
@@ -0,0 +1,14 @@
+package com.android.quickstep.views
+
+import android.util.Log
+
+// TODO(http://b/193432925)
+class TaskMenuViewWithArrow {
+    companion object {
+        const val TAG = "TaskMenuViewWithArrow"
+
+        fun logSomething() {
+            Log.d(TAG, "It worked!")
+        }
+    }
+}
