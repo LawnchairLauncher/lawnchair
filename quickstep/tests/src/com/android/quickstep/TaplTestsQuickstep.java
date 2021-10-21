ae3482748fd297aa92c7ef5a53b14916a1e63e0d
Alex Chau <alexchau@google.com>
2021-10-21 22:22:22 +0100

    Ignore testOverviewForTablet until root cause of cuttlefish failure is found.
    Test: TaplTestQuickstep.java
    Bug: 203781041
    Change-Id: I9b6f658ab8f55beafaf784d7a374e2780a591089

--- quickstep/tests/src/com/android/quickstep/TaplTestsQuickstep.java
+++ quickstep/tests/src/com/android/quickstep/TaplTestsQuickstep.java
@@ -43,6 +43,7 @@ import com.android.quickstep.views.RecentsView;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
