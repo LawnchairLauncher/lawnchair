f4c06d05163e7afbd6f8cdd88a216cc7ef6d6f9f
Patryk Michalik <contact@patrykmichalik.com>
2021-10-24 16:20:43 +0200

    Settings: Use accent color for icons on home page

--- lawnchair/src/app/lawnchair/ui/preferences/components/PreferenceCategory.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/PreferenceCategory.kt
@@ -17,12 +17,18 @@
 package app.lawnchair.ui.preferences.components
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import app.lawnchair.ui.preferences.LocalNavController
@@ -50,11 +56,20 @@ fun PreferenceCategory(
             }
         },
         startWidget = {
-            Image(
-                painter = painterResource(id = iconResource),
-                contentDescription = null,
-                modifier = Modifier.size(32.dp)
-            )
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(32.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colors.primary)
+            ) {
+                Icon(
+                    painter = painterResource(id = iconResource),
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp),
+                    tint = MaterialTheme.colors.onPrimary
+                )
+            }
         },
     )
 }
