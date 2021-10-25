d5d16c0cc24e988c09f19e33ce4c249a42925274
Patryk Michalik <contact@patrykmichalik.com>
2021-10-25 17:38:58 +0200

    `ScrimView`: Fix handling of custom App Drawer opacity

--- src/com/android/launcher3/views/ScrimView.java
+++ src/com/android/launcher3/views/ScrimView.java
@@ -31,7 +31,11 @@ import androidx.core.graphics.ColorUtils;
 
 import com.android.launcher3.BaseActivity;
 import com.android.launcher3.Insettable;
+import com.android.launcher3.R;
 import com.android.launcher3.util.SystemUiController;
+import com.android.launcher3.util.Themes;
+
+import app.lawnchair.preferences.PreferenceManager;
 
 /**
  * Simple scrim which draws a flat color
@@ -44,10 +48,15 @@ public class ScrimView extends View implements Insettable {
     private ScrimDrawingController mDrawingController;
     private int mBackgroundColor;
     private boolean mIsVisible = true;
+    private final float drawerOpacity;
+    private final boolean workspaceTextDark;
 
     public ScrimView(Context context, AttributeSet attrs) {
         super(context, attrs);
         setFocusable(false);
+        PreferenceManager preferenceManager = PreferenceManager.INSTANCE.get(context);
+        drawerOpacity = preferenceManager.getDrawerOpacity().get();
+        workspaceTextDark = Themes.getAttrBoolean(context, R.attr.isWorkspaceDarkText);
     }
 
     @Override
@@ -102,7 +111,7 @@ public class ScrimView extends View implements Insettable {
         final float threshold = STATUS_BAR_COLOR_FORCE_UPDATE_THRESHOLD;
         boolean forceChange = getVisibility() == VISIBLE
                 && getAlpha() > threshold
-                && (Color.alpha(mBackgroundColor) / 255f) > threshold;
+                && (Color.alpha(mBackgroundColor) / (255f * drawerOpacity)) > threshold;
         if (forceChange) {
             getSystemUiController().updateUiState(UI_STATE_SCRIM_VIEW, !isScrimDark());
         } else {
@@ -123,8 +132,11 @@ public class ScrimView extends View implements Insettable {
                     "ScrimView must have a ColorDrawable background, this one has: "
                             + getBackground());
         }
-        return ColorUtils.calculateLuminance(
-                ((ColorDrawable) getBackground()).getColor()) < 0.5f;
+        if (drawerOpacity <= 0.3f) {
+            return !workspaceTextDark;
+        } else {
+            return ColorUtils.calculateLuminance(((ColorDrawable) getBackground()).getColor()) < 0.5f;
+        }
     }
 
     /**
