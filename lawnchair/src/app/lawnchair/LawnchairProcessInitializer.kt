02a1ad7a746763463e44e343972a08c2e5441f6f
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-27 14:41:34 +0700

    Override themed icon colors

--- lawnchair/src/app/lawnchair/LawnchairProcessInitializer.kt
+++ lawnchair/src/app/lawnchair/LawnchairProcessInitializer.kt
@@ -2,7 +2,11 @@ package app.lawnchair
 
 import android.content.Context
 import androidx.annotation.Keep
+import androidx.arch.core.util.Function
 import app.lawnchair.bugreport.LawnchairBugReporter
+import app.lawnchair.theme.color.ColorTokens
+import com.android.launcher3.Utilities
+import com.android.launcher3.icons.ThemedIconDrawable
 import com.android.quickstep.QuickstepProcessInitializer
 
 @Keep
@@ -11,5 +15,18 @@ class LawnchairProcessInitializer(context: Context) : QuickstepProcessInitialize
     override fun init(context: Context) {
         super.init(context)
         LawnchairBugReporter.INSTANCE.get(context)
+        ThemedIconDrawable.COLORS_LOADER = Function {
+            if (Utilities.isDarkTheme(it)) {
+                intArrayOf(
+                    ColorTokens.Neutral1_800.resolveColor(it),
+                    ColorTokens.Accent1_100.resolveColor(it),
+                )
+            } else {
+                intArrayOf(
+                    ColorTokens.Accent1_100.resolveColor(it),
+                    ColorTokens.Neutral2_700.resolveColor(it),
+                )
+            }
+        }
     }
 }
