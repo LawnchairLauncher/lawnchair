95d0a65bd449b46ce0938cbaa3d7951adcfdfcc4
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-10-28 22:26:40 +0700

    Migrate font preference to material3

--- lawnchair/src/app/lawnchair/ui/preferences/FontSelectionPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/FontSelectionPreference.kt
@@ -7,6 +7,11 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowDropDown
 import androidx.compose.material.icons.rounded.Clear
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -179,7 +184,7 @@ private fun VariantDropdown(
 
     TextButton(
         onClick = { showVariants = true },
-        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.onBackground),
+        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.onBackground),
         contentPadding = VariantButtonContentPadding
     ) {
         AndroidText(
