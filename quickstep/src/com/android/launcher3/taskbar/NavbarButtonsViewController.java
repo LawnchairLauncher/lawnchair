6e906f3436e01354bf99336a5bc9a9da6a22ba02
Vinit Nayak <peanutbutter@google.com>
2021-11-01 13:08:14 -0700

    Align OverviewActionsView for 3 button taskbar
    Bug: 189807374
    Change-Id: I3c7ec5f8c7b588032ff83ca7ab461d2aac7bfa35

--- quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
@@ -229,6 +229,14 @@ public class NavbarButtonsViewController {
         mPropertyHolders.forEach(StatePropertyHolder::endAnimation);
     }
 
+    public void onDestroy() {
+        mPropertyHolders.clear();
+        mControllers.rotationButtonController.unregisterListeners();
+        if (mFloatingRotationButton != null) {
+            mFloatingRotationButton.hide();
+        }
+    }
+
     private void initButtons(ViewGroup navContainer, ViewGroup endContainer,
             TaskbarNavButtonController navButtonController) {
 
@@ -449,14 +457,6 @@ public class NavbarButtonsViewController {
     public void onConfigurationChanged(@Config int configChanges) {
         if (mFloatingRotationButton != null) {
             mFloatingRotationButton.onConfigurationChanged(configChanges);
-        }
-    }
-
-    public void onDestroy() {
-        mPropertyHolders.clear();
-        mControllers.rotationButtonController.unregisterListeners();
-        if (mFloatingRotationButton != null) {
-            mFloatingRotationButton.hide();
         }
     }
 
--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -1129,7 +1129,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
             for (int i = 0; i < taskCount; i++) {
                 View v = getTaskViewAt(i);
                 if (!(v instanceof GroupedTaskView)) {
-                    return;
+                    continue;
                 }
                 GroupedTaskView gtv = (GroupedTaskView) v;
                 gtv.onTaskListVisibilityChanged(false);
