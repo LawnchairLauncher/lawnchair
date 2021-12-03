149acef8d7e2fa111f7418a0b215a88fd3a19eb5
Alex Chau <alexchau@google.com>
2021-12-03 15:14:49 +0000

    Revert "Align OverviewActionsView for 3 button taskbar"
    This reverts commit 6e906f3436e01354bf99336a5bc9a9da6a22ba02.
    
    Reason for revert: b/204891006
    Bug: 189807374
    Change-Id: I1b3f93d44b1691aa3d2cc90222ac93067a5cf6b4

--- quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
@@ -229,14 +229,6 @@ public class NavbarButtonsViewController {
         mPropertyHolders.forEach(StatePropertyHolder::endAnimation);
     }
 
-    public void onDestroy() {
-        mPropertyHolders.clear();
-        mControllers.rotationButtonController.unregisterListeners();
-        if (mFloatingRotationButton != null) {
-            mFloatingRotationButton.hide();
-        }
-    }
-
     private void initButtons(ViewGroup navContainer, ViewGroup endContainer,
             TaskbarNavButtonController navButtonController) {
 
@@ -457,6 +449,14 @@ public class NavbarButtonsViewController {
     public void onConfigurationChanged(@Config int configChanges) {
         if (mFloatingRotationButton != null) {
             mFloatingRotationButton.onConfigurationChanged(configChanges);
+        }
+    }
+
+    public void onDestroy() {
+        mPropertyHolders.clear();
+        mControllers.rotationButtonController.unregisterListeners();
+        if (mFloatingRotationButton != null) {
+            mFloatingRotationButton.hide();
         }
     }
 
