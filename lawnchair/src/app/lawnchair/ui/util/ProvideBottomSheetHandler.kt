c3b979ba208ac7da9c47183e99c2519108aa55b9
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-01-11 20:30:26 +0700

    Bring back bottom sheet statusbar offset

--- lawnchair/src/app/lawnchair/ui/util/ProvideBottomSheetHandler.kt
+++ lawnchair/src/app/lawnchair/ui/util/ProvideBottomSheetHandler.kt
@@ -17,11 +17,22 @@
 package app.lawnchair.ui.util
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.LocalWindowInsets
 import kotlinx.coroutines.launch
+import kotlin.math.max
 
 internal val LocalBottomSheetHandler = staticCompositionLocalOf { BottomSheetHandler() }
 
@@ -57,7 +68,9 @@ fun ProvideBottomSheetHandler(
                 bottomSheetHandler.hide()
             }
             CompositionLocalProvider(LocalContentColor provides MaterialTheme.colorScheme.onSurface) {
-                bottomSheetContent.content()
+                StatusBarOffset {
+                    bottomSheetContent.content()
+                }
             }
         },
         sheetState = bottomSheetState
@@ -72,3 +85,32 @@ data class BottomSheetHandler(
     val show: (@Composable () -> Unit) -> Unit = {},
     val hide: () -> Unit = {}
 )
+
+@Composable
+fun StatusBarOffset(content: @Composable () -> Unit) {
+    val windowInsets = LocalWindowInsets.current
+    val statusBarHeight = max(windowInsets.statusBars.top, windowInsets.displayCutout.top)
+    val topOffset = statusBarHeight + with(LocalDensity.current) { 8.dp.roundToPx() }
+
+    Box(
+        modifier = Modifier
+            .layout { measurable, constraints ->
+                val newConstraints = Constraints(
+                    minWidth = constraints.minWidth,
+                    maxWidth = constraints.maxWidth,
+                    minHeight = constraints.minHeight,
+                    maxHeight = when (constraints.maxHeight) {
+                        Constraints.Infinity -> Constraints.Infinity
+                        else -> constraints.maxHeight - topOffset
+                    }
+                )
+                val placeable = measurable.measure(newConstraints)
+
+                layout(placeable.width, placeable.height) {
+                    placeable.placeRelative(0, 0)
+                }
+            }
+    ) {
+        content()
+    }
+}
