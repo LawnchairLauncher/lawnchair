73659377ee2456e7c7ddc09af5c78c13066a9ac7
Dylan Roussel <mail@evowizz.dev>
2022-02-22 11:51:38 +0100

    Disable GoogleGo if app isn't installed

--- lawnchair/src/app/lawnchair/ui/preferences/components/QsbProviderPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/QsbProviderPreference.kt
@@ -3,8 +3,12 @@ package app.lawnchair.ui.preferences.components
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import app.lawnchair.preferences2.PreferenceCollectorScope
 import app.lawnchair.preferences2.PreferenceManager2
+import app.lawnchair.qsb.QsbLayout
+import app.lawnchair.qsb.providers.GoogleGo
 import app.lawnchair.qsb.providers.QsbSearchProvider
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -13,8 +17,14 @@ fun PreferenceCollectorScope.QsbProviderPreference(
     value: QsbSearchProvider,
     edit: suspend PreferenceManager2.(QsbSearchProvider) -> Unit,
 ) {
+    val context = LocalContext.current
     val entries = remember {
-        QsbSearchProvider.values().map { ListPreferenceEntry2(it) { it.name } }
+        QsbSearchProvider.values().map {
+            // Enabled is true if provider is anything but Google Go,
+            // or if it is Google Go and the intent can be resolved.
+            val enabled = it != GoogleGo || QsbLayout.resolveIntent(context, it.createSearchIntent())
+            ListPreferenceEntry2(it, enabled) { stringResource(id = it.name) }
+        }
     }
 
     ListPreference2(
