b562b08e21354b7ac9d7d0c59e0607d28f366096
Dylan Roussel <9381261+evowizz@users.noreply.github.com>
2022-02-23 10:29:43 +0100

    Replace AppSearch's id with `app_search`
    Co-authored-by: Patryk Michalik <contact@patrykmichalik.com>

--- lawnchair/src/app/lawnchair/qsb/providers/AppSearch.kt
+++ lawnchair/src/app/lawnchair/qsb/providers/AppSearch.kt
@@ -4,7 +4,7 @@ import app.lawnchair.qsb.ThemingMethod
 import com.android.launcher3.R
 
 object AppSearch : QsbSearchProvider(
-    id = "app-search",
+    id = "app_search",
     name = R.string.search_provider_app_search,
     icon = R.drawable.ic_qsb_search,
     themingMethod = ThemingMethod.TINT,
