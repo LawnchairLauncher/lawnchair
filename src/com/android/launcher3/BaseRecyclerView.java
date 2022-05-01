3c5a08ada14741b01844f8deb40b82dc0431f692
Sunny Goyal <sunnygoyal@google.com>
2022-05-01 12:18:14 -0700

    Implementing support for item diffing instead of creating out the complete UI on every update
    Bug: 229860311
    Test: Verified locally
    Change-Id: I5712b5d76878a0ed72cc1392ede59b3778b7a1dc

--- src/com/android/launcher3/BaseRecyclerView.java
+++ src/com/android/launcher3/BaseRecyclerView.java
@@ -23,7 +23,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityNodeInfo;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.launcher3.compat.AccessibilityManagerCompat;
@@ -196,13 +195,6 @@ public abstract class BaseRecyclerView extends RecyclerView  {
     public void scrollToTop() {
         if (mScrollbar != null) {
             mScrollbar.reattachThumbToScroll();
-        }
-        if (getLayoutManager() instanceof LinearLayoutManager) {
-            LinearLayoutManager layoutManager = (LinearLayoutManager) getLayoutManager();
-            if (layoutManager.findFirstCompletelyVisibleItemPosition() == 0) {
-                // We are at the top, so don't scrollToPosition (would cause unnecessary relayout).
-                return;
-            }
         }
         scrollToPosition(0);
     }
