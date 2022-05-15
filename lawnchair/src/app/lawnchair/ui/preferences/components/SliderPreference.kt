55128a428bf2fafea4b8d2760274f29337a7eed9
Patryk Michalik <contact@patrykmichalik.com>
2022-05-15 20:23:13 +0200

    `SliderPreference`: Use M3 `Slider`

--- lawnchair/src/app/lawnchair/ui/preferences/components/SliderPreference.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/SliderPreference.kt
@@ -16,9 +16,24 @@
 
 package app.lawnchair.ui.preferences.components
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material3.Slider
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
