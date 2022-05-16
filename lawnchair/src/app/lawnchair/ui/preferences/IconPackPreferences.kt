7f95b4f220b6353416286f1d9de16af068891274
Suphon Thanakornpakapong <paphonb@gmail.com>
2022-05-16 16:02:36 +0700

    Migrate icon pack preferences to LazyVerticalGrid

--- lawnchair/src/app/lawnchair/ui/preferences/IconPackPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/IconPackPreferences.kt
@@ -20,7 +20,10 @@ import android.content.res.Configuration
 import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -31,7 +34,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -41,7 +43,6 @@ import app.lawnchair.preferences.PreferenceAdapter
 import app.lawnchair.preferences.getAdapter
 import app.lawnchair.preferences.preferenceManager
 import app.lawnchair.ui.preferences.components.*
-import app.lawnchair.ui.util.LazyGridLayout
 import app.lawnchair.util.Constants
 import app.lawnchair.util.isPackageInstalled
 import com.android.launcher3.R
@@ -107,72 +108,69 @@ fun IconPackPreferences() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IconPackGrid(adapter: PreferenceAdapter<String>) {
     val iconPacks by LocalPreferenceInteractor.current.iconPacks.collectAsState()
 
-    val density = LocalDensity.current
-    val gridLayout = remember {
-        LazyGridLayout(
-            minWidth = 72.dp,
-            gapWidth = 16.dp,
-            density = density
-        )
-    }
-    val listState = rememberLazyListState()
-    val numColumns by gridLayout.numColumns
-    PreferenceLazyColumn(
-        modifier = Modifier
-            .fillMaxWidth()
-            .then(gridLayout.onSizeChanged()),
-        state = listState,
-        isChild = true
-    ) {
-        verticalGridItems(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            items = iconPacks,
-            numColumns = numColumns,
-            horizontalGap = 16.dp,
-            verticalGap = 4.dp
-        ) { _, item ->
-            val wasSelected = remember { mutableStateOf(false) }
-            val selected = item.packageName == adapter.state.value
-            LaunchedEffect(selected) {
-                if (wasSelected.value != selected) {
-                    wasSelected.value = selected
-                    if (selected) {
-                        listState.scrollToThisItem()
+    NestedScrollStretch {
+        val state = rememberLazyGridState()
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 72.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp)
+        ) {
+            itemsIndexed(iconPacks, { _, item -> item.packageName }) { index, item ->
+                val wasSelected = remember { mutableStateOf(false) }
+                val selected = item.packageName == adapter.state.value
+                LaunchedEffect(selected) {
+                    if (wasSelected.value != selected) {
+                        wasSelected.value = selected
+                        if (selected) {
+                            state.animateScrollToItem(index)
+                        }
                     }
                 }
-            }
-            Surface(
-                onClick = { adapter.onChange(item.packageName) },
-                shape = MaterialTheme.shapes.large,
-                tonalElevation = if (selected) 1.dp else 0.dp
-            ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp, horizontal = 8.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Image(
-                        modifier = Modifier
-                            .size(56.dp)
-                            .padding(bottom = 8.dp),
-                        painter = rememberDrawablePainter(drawable = item.icon),
-                        contentDescription = null
-                    )
-                    Text(
-                        text = item.name,
-                        textAlign = TextAlign.Center,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 2,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
+                IconPackItem(item = item, selected = selected) {
+                    adapter.onChange(item.packageName)
                 }
             }
+        }
+        }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun IconPackItem(
+    item: IconPackInfo,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    Surface(
+        onClick = onClick,
+        shape = MaterialTheme.shapes.large,
+        tonalElevation = if (selected) 1.dp else 0.dp
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp, horizontal = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(56.dp)
+                    .padding(bottom = 8.dp),
+                painter = rememberDrawablePainter(drawable = item.icon),
+                contentDescription = null
+            )
+            Text(
+                text = item.name,
+                textAlign = TextAlign.Center,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
