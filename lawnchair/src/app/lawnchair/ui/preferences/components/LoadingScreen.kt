3f9b8a61e59720e74b0f3c188ec9bf81c1c3a3ac
Patryk Michalik <contact@patrykmichalik.com>
2022-05-23 10:50:38 +0200

    Simplify `LoadingScreen`

--- lawnchair/src/app/lawnchair/ui/preferences/components/LoadingScreen.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/LoadingScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
@@ -31,11 +32,9 @@ fun LoadingScreen(isLoading: Boolean, content: @Composable () -> Unit) {
     Crossfade(targetState = isLoading) {
         if (it) {
             Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(),
+                modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 CircularProgressIndicator()
             }
