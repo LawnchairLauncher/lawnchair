acb7c3a54b4e54f4772b5a262d86d1b19570f055
Yasan Ghaffarian <y.ghafarian@dariahamrah.ir>
2022-05-29 12:11:19 +0430

    Disable `addIconToHome` when home screen is locked
    Co-authored-by: Daria Hamrah Paytakht <info@dariarnd.ir>

--- src/com/android/launcher3/SessionCommitReceiver.java
+++ src/com/android/launcher3/SessionCommitReceiver.java
@@ -31,6 +31,9 @@ import com.android.launcher3.logging.FileLog;
 import com.android.launcher3.model.ItemInstallQueue;
 import com.android.launcher3.pm.InstallSessionHelper;
 import com.android.launcher3.util.Executors;
+import com.patrykmichalik.preferencemanager.PreferenceExtensionsKt;
+
+import app.lawnchair.preferences2.PreferenceManager2;
 
 /**
  * BroadcastReceiver to handle session commit intent.
@@ -81,6 +84,7 @@ public class SessionCommitReceiver extends BroadcastReceiver {
     }
 
     public static boolean isEnabled(Context context) {
+        if (PreferenceExtensionsKt.firstBlocking(PreferenceManager2.getInstance(context).getLockHomeScreen())) return false;
         return Utilities.getPrefs(context).getBoolean(ADD_ICON_PREFERENCE_KEY, true);
     }
 }
