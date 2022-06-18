684fb85964562b7a3335e54d3d0b5fcab0c792d0
Patryk Michalik <contact@patrykmichalik.com>
2022-06-18 17:37:25 +0200

    Use expression bodies for type converter functions

--- lawnchair/src/app/lawnchair/data/Converters.kt
+++ lawnchair/src/app/lawnchair/data/Converters.kt
@@ -4,13 +4,10 @@ import androidx.room.TypeConverter
 import com.android.launcher3.util.ComponentKey
 
 class Converters {
-    @TypeConverter
-    fun fromComponentKey(value: ComponentKey?): String? {
-        return value?.toString()
-    }
 
     @TypeConverter
-    fun toComponentKey(value: String?): ComponentKey? {
-        return value?.let { ComponentKey.fromString(it) }
-    }
+    fun fromComponentKey(value: ComponentKey?) = value?.toString()
+
+    @TypeConverter
+    fun toComponentKey(value: String?) = value?.let { ComponentKey.fromString(it) }
 }
