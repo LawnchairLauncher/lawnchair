83302627565e6d663cf946013f17f3724e32f91b
Yasan <y.ghafarian@dariahamrah.ir>
2022-08-13 13:34:43 +0430

    Icon Shape Preview (#2824)
    Co-authored-by: Fateme Maleki <f.maleki@dariahamrah.ir>
    Co-authored-by: Daria Hamrah Paytakht <info@dariarnd.ir>

--- lawnchair/src/app/lawnchair/ui/preferences/GeneralPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/GeneralPreferences.kt
@@ -29,7 +29,7 @@ import app.lawnchair.preferences2.asState
 import app.lawnchair.preferences2.preferenceManager2
 import app.lawnchair.ui.preferences.components.ExpandAndShrink
 import app.lawnchair.ui.preferences.components.FontPreference
-import app.lawnchair.ui.preferences.components.IconShapePreference
+import app.lawnchair.ui.preferences.components.IconShapePreview
 import app.lawnchair.ui.preferences.components.NavigationActionPreference
 import app.lawnchair.ui.preferences.components.NotificationDotsPreference
 import app.lawnchair.ui.preferences.components.PreferenceGroup
@@ -38,27 +38,33 @@ import app.lawnchair.ui.preferences.components.SliderPreference
 import app.lawnchair.ui.preferences.components.SwitchPreference
 import app.lawnchair.ui.preferences.components.ThemePreference
 import app.lawnchair.ui.preferences.components.colorpreference.ColorPreference
+import app.lawnchair.ui.preferences.components.iconShapeEntries
+import app.lawnchair.ui.preferences.components.iconShapeGraph
 import app.lawnchair.ui.preferences.components.notificationDotsEnabled
 import app.lawnchair.ui.preferences.components.notificationServiceEnabled
 import com.android.launcher3.R
 
 object GeneralRoutes {
     const val ICON_PACK = "iconPack"
+    const val ICON_SHAPE = "iconShape"
 }
 
 fun NavGraphBuilder.generalGraph(route: String) {
     preferenceGraph(route, { GeneralPreferences() }) { subRoute ->
         iconPackGraph(route = subRoute(GeneralRoutes.ICON_PACK))
+        iconShapeGraph(route = subRoute(GeneralRoutes.ICON_SHAPE))
     }
 }
 
 @Composable
 fun GeneralPreferences() {
+    val context = LocalContext.current
     val prefs = preferenceManager()
     val prefs2 = preferenceManager2()
     val iconPacks by LocalPreferenceInteractor.current.iconPacks.collectAsState()
     val themedIconsAdapter = prefs.themedIcons.getAdapter()
     val drawerThemedIconsAdapter = prefs.drawerThemedIcons.getAdapter()
+    val iconShapeAdapter = prefs2.iconShape.getAdapter()
 
     val currentIconPackName = iconPacks
         .find { it.packageName == preferenceManager().iconPackPackage.get() }
@@ -74,6 +80,9 @@ fun GeneralPreferences() {
             stringResource(id = R.string.themed_icon_title),
         )
     } else currentIconPackName
+    val iconShapeSubtitle = iconShapeEntries(context)
+        .firstOrNull { it.value == iconShapeAdapter.state.value }
+        ?.label?.invoke()
 
     PreferenceLayout(label = stringResource(id = R.string.general_label)) {
         PreferenceGroup {
@@ -87,7 +96,14 @@ fun GeneralPreferences() {
                 destination = subRoute(name = GeneralRoutes.ICON_PACK),
                 subtitle = iconStyleSubtitle,
             )
-            IconShapePreference()
+            NavigationActionPreference(
+                label = stringResource(id = R.string.icon_shape_label),
+                destination = subRoute(name = GeneralRoutes.ICON_SHAPE),
+                subtitle = iconShapeSubtitle,
+                endWidget = {
+                    IconShapePreview(iconShape = iconShapeAdapter.state.value)
+                }
+            )
             val enableFontSelection = prefs2.enableFontSelection.asState().value
             if (enableFontSelection) {
                 FontPreference(
@@ -97,7 +113,6 @@ fun GeneralPreferences() {
             }
         }
         PreferenceGroup(heading = stringResource(id = R.string.notification_dots)) {
-            val context = LocalContext.current
             val enabled by remember { notificationDotsEnabled(context) }.collectAsState(initial = false)
             val serviceEnabled = notificationServiceEnabled()
             NotificationDotsPreference(enabled = enabled, serviceEnabled = serviceEnabled)
