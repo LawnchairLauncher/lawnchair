b0fb5096ba0dd30ae435cc5d0775529480f231a5
Holly Sun <jiuyu@google.com>
2022-09-19 14:53:32 -0700

    Update gBoard icon to Go/Search depending on suggestions.
    If there is a highlight item, show Go (->) icon. Otherwise, show Search (magnifying glass) icon.
    
    Implement maybeUpdateEnterKeyIcon() in NexusLauncher because it's gBoard specific.
    
    Test: manual. Verify icon changes according to states.
    Bug: 183467628
    Bug: 210661150
    Change-Id: I5dbcb54a37f9d37ae31bf3eaef241ea8a3ede510

--- src/com/android/launcher3/config/FeatureFlags.java
+++ src/com/android/launcher3/config/FeatureFlags.java
@@ -96,6 +96,10 @@ public final class FeatureFlags {
             "ENABLE_QUICK_LAUNCH_V2", false, "Use quick launch v2 "
             + "behavior. Quick search and quick launch v1 would be unavailable if this is enabled");
 
+    public static final BooleanFlag GBOARD_UPDATE_ENTER_KEY = new DeviceFlag(
+            "GBOARD_UPDATE_ENTER_KEY", false, "Update gBoard enter key "
+            + "icon dynamically based on top search content for Quick Launch V2");
+
     public static final BooleanFlag ENABLE_HIDE_HEADER = new DeviceFlag("ENABLE_HIDE_HEADER",
             true, "Hide header on keyboard before typing in all apps");
 
