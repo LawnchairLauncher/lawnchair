30b87aad7d5d91a7ab2a8931805d7f18cae123dd
Andy Wickham <awickham@google.com>
2022-09-21 17:04:48 -0700

    Fix the case of the missing header.
    We should not override the tabs based on the search state now
    that they are hidden (but still active) during the animation.
    
    For an explanation of why this fixes the issue, please see
    https://b.corp.google.com/issues/247615711#comment15.
    
    Fix: 247615711
    Test: Manually with predicted apps being updated every second
    (part of the way I found to repro the bug).
    
    Change-Id: I5ba45c42726c34f471a7743c06aecc05c89f6105

--- src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
+++ src/com/android/launcher3/allapps/ActivityAllAppsContainerView.java
@@ -179,11 +179,6 @@ public class ActivityAllAppsContainerView<T extends Context & AppLauncher
     }
 
     @Override
-    protected boolean shouldShowTabs() {
-        return super.shouldShowTabs() && !isSearching();
-    }
-
-    @Override
     public boolean isSearching() {
         return mIsSearching;
     }
