899b1b7d6798cf0f4ca674229cb7a51526009279
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2022-10-12 21:55:51 +0800

    Fix crashes from #3018 by un-`!` the comparison operator
    Apparently I didn't fully read the docs:  `check` and`require` error out on `false`, instead of `true`. This commit fixes that by removing the `!` on the comparison operators.

--- lawnchair/src/app/lawnchair/icons/IconEntry.kt
+++ lawnchair/src/app/lawnchair/icons/IconEntry.kt
@@ -6,7 +6,7 @@ data class IconEntry(
     val type: IconType
 ) {
     fun resolveDynamicCalendar(day: Int): IconEntry {
-        check (type != IconType.Calendar) { "type is not calendar" }
+        check (type == IconType.Calendar) { "type is not calendar" }
         return IconEntry(packPackageName, "$name${day + 1}", IconType.Normal)
     }
 }
