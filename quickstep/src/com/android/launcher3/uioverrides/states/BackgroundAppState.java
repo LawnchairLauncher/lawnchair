47a07bc704193ea36aabc53642b4c760ce48defe
Alex Chau <alexchau@google.com>
2022-10-14 17:45:41 +0100

    Decouple taskbar background from overveiw scrim if taskbar is in overview
    - Removed all forceHideBackground calls if taskbar would be in overview
    - Make overview scrim transparent regardless of stash state if taskbar would be in overview
    
    Fix: 234139692
    Test: Go to overview from app with ENABLE_TASKBAR_IN_OVERVIEW on/off and taskbar stashed/unstashed
    Change-Id: I29e811cb93894e6c9e7a75c0ea493719f15eeccb

--- quickstep/src/com/android/launcher3/uioverrides/states/BackgroundAppState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/BackgroundAppState.java
@@ -26,6 +26,7 @@ import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.R;
 import com.android.launcher3.allapps.AllAppsTransitionController;
+import com.android.launcher3.config.FeatureFlags;
 import com.android.quickstep.util.LayoutUtils;
 import com.android.quickstep.views.RecentsView;
 
@@ -96,7 +97,7 @@ public class BackgroundAppState extends OverviewState {
     @Override
     public int getWorkspaceScrimColor(Launcher launcher) {
         DeviceProfile dp = launcher.getDeviceProfile();
-        if (dp.isTaskbarPresentInApps) {
+        if (dp.isTaskbarPresentInApps && !FeatureFlags.ENABLE_TASKBAR_IN_OVERVIEW.get()) {
             return launcher.getColor(R.color.taskbar_background);
         }
         return Color.TRANSPARENT;
