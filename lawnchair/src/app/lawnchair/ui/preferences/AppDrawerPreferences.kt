fb71210aaa6fb41a372c53e231081f6a2ae1699c
Naveen <naveen.ccmsd@gmail.com>
2022-10-14 17:55:54 +0530

    Update lawnchair/src/app/lawnchair/ui/preferences/AppDrawerPreferences.kt
    Co-authored-by: SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>

--- lawnchair/src/app/lawnchair/ui/preferences/AppDrawerPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/AppDrawerPreferences.kt
@@ -64,7 +64,7 @@ fun AppDrawerPreferences() {
         val deviceSearchEnabled = LawnchairSearchAlgorithm.isDeviceSearchEnabled(LocalContext.current)
         val showDrawerSearchBar = !prefs2.hideAppDrawerSearchBar.getAdapter()
         val showHiddenAppsInSearch = prefs2.showHiddenAppsInSearch.getAdapter()
-        val enableSmartHide = prefs2.enableSmartHide.getAdapter()
+val enableSmartHide = prefs2.enableSmartHide.getAdapter()
         PreferenceGroup(heading = stringResource(id = R.string.pref_category_search)) {
             SwitchPreference(
                 label = stringResource(id = R.string.show_app_search_bar),
