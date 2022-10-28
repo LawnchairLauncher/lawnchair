10856068bc31a36db900bf8822e539e8e9d21dc2
Mady Mellor <madym@google.com>
2022-10-27 17:00:43 -0700

    Remove floating task button from taskbar
    Not POR to have a button in taskbar for this so removing this bit
    of the prototype.
    
    Test: manual - enable floating tasks on tablet and check if there's
                   a button in taskbar (there shouldn't be).
    Bug: 237678727
    Change-Id: Ifb57f6528946f89592649afda84101e4f9cbb198

--- quickstep/src/com/android/quickstep/SystemUiProxy.java
+++ quickstep/src/com/android/quickstep/SystemUiProxy.java
@@ -59,7 +59,6 @@ import com.android.systemui.shared.system.smartspace.ISysuiUnlockAnimationContro
 import com.android.systemui.shared.system.smartspace.SmartspaceState;
 import com.android.wm.shell.back.IBackAnimation;
 import com.android.wm.shell.desktopmode.IDesktopMode;
-import com.android.wm.shell.floating.IFloatingTasks;
 import com.android.wm.shell.onehanded.IOneHanded;
 import com.android.wm.shell.pip.IPip;
 import com.android.wm.shell.pip.IPipAnimationListener;
@@ -90,7 +89,6 @@ public class SystemUiProxy implements ISystemUiProxy {
     private IPip mPip;
     private ISysuiUnlockAnimationController mSysuiUnlockAnimationController;
     private ISplitScreen mSplitScreen;
-    private IFloatingTasks mFloatingTasks;
     private IOneHanded mOneHanded;
     private IShellTransitions mShellTransitions;
     private IStartingWindow mStartingWindow;
@@ -168,7 +166,7 @@ public class SystemUiProxy implements ISystemUiProxy {
     }
 
     public void setProxy(ISystemUiProxy proxy, IPip pip, ISplitScreen splitScreen,
-            IFloatingTasks floatingTasks, IOneHanded oneHanded, IShellTransitions shellTransitions,
+            IOneHanded oneHanded, IShellTransitions shellTransitions,
             IStartingWindow startingWindow, IRecentTasks recentTasks,
             ISysuiUnlockAnimationController sysuiUnlockAnimationController,
             IBackAnimation backAnimation, IDesktopMode desktopMode) {
@@ -176,7 +174,6 @@ public class SystemUiProxy implements ISystemUiProxy {
         mSystemUiProxy = proxy;
         mPip = pip;
         mSplitScreen = splitScreen;
-        mFloatingTasks = floatingTasks;
         mOneHanded = oneHanded;
         mShellTransitions = shellTransitions;
         mStartingWindow = startingWindow;
@@ -210,7 +207,7 @@ public class SystemUiProxy implements ISystemUiProxy {
     }
 
     public void clearProxy() {
-        setProxy(null, null, null, null, null, null, null, null, null, null, null);
+        setProxy(null, null, null, null, null, null, null, null, null, null);
     }
 
     // TODO(141886704): Find a way to remove this
@@ -693,20 +690,6 @@ public class SystemUiProxy implements ISystemUiProxy {
             }
         }
         return null;
-    }
-
-    //
-    // Floating tasks
-    //
-
-    public void showFloatingTask(Intent intent) {
-        if (mFloatingTasks != null) {
-            try {
-                mFloatingTasks.showTask(intent);
-            } catch (RemoteException e) {
-                Log.w(TAG, "Launcher: Failed call showFloatingTask", e);
-            }
-        }
     }
 
     //
