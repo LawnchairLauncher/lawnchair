75506f748a0adeec98a27b20ea85becfd907ef55
Goooler <wangzongler@gmail.com>
2022-10-31 11:01:08 +0800

    Remove imports

--- lawnchair/src/app/lawnchair/util/core.kt
+++ lawnchair/src/app/lawnchair/util/core.kt
@@ -2,7 +2,5 @@ package app.lawnchair.util
 
 import android.content.Context
 import androidx.core.content.getSystemService
-import kotlin.contracts.ExperimentalContracts
-import kotlin.contracts.contract
 
 inline fun <reified T : Any> Context.requireSystemService(): T = checkNotNull(getSystemService())
