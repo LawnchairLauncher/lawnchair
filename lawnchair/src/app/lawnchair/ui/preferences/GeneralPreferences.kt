fde964cfe5d85a8c3fbffdadf93454bb7a132ccd
Naveen <naveen.ccmsd@gmail.com>
2022-11-10 02:48:07 +0530

    Hide Icon shape only when ThemedIcons enabled

--- lawnchair/src/app/lawnchair/ui/preferences/GeneralPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/GeneralPreferences.kt
@@ -124,7 +124,7 @@ fun GeneralPreferences() {
                     description = stringResource(id = R.string.transparent_background_icons_description),
                 )
             }
-            ExpandAndShrink(visible = !transparentIconBackground.state.value) {
+            ExpandAndShrink(visible = !transparentIconBackground.state.value || !themedIconsEnabled) {
                 DividerColumn {
                     NavigationActionPreference(
                         label = stringResource(id = R.string.icon_shape_label),
