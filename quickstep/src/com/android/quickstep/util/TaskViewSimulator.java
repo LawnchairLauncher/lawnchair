3b1f5849ce8edb67a3f86356a92e552f440d42dd
Vinit Nayak <peanutbutter@google.com>
2022-11-14 21:10:11 -0800

    Update for stashed taskbar animation
    * Strangely if you swipe up with a split pair
    and then tap on the live tile to go back to
    fullscreen this animates fairly correctly.
    However, if you drag the focused pair back to fullscreen
    the bottom app does not unclip as expected
    
    Bug: 219411750
    Test: Swipe up w/ stashed taskbar in portrait.
    Thumbnail cutout doesn't appear when dragging to
    fullscreen
    
    Change-Id: Ia6e7aae10389d9fefa4adbfce540171bbe0a9180

--- quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
+++ quickstep/src/com/android/quickstep/util/TaskViewSimulator.java
@@ -320,6 +320,7 @@ public class TaskViewSimulator implements TransformParams.BuilderProxy {
 
             // mIsRecentsRtl is the inverse of TaskView RTL.
             boolean isRtlEnabled = !mIsRecentsRtl;
+            mPositionHelper.setTaskbarInApp(mDp.isTaskbarPresentInApps);
             mPositionHelper.updateThumbnailMatrix(
                     mThumbnailPosition, mThumbnailData, mTaskRect.width(), mTaskRect.height(),
                     mDp.widthPx, mDp.heightPx, mDp.taskbarSize, mDp.isTablet,
