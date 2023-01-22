4a34a32e497ca9b088aaba1e7a67bf5c983fd3b1
Goooler <wangzongler@gmail.com>
2023-01-22 12:32:49 +0800

    CustomTextView extends AppCompatTextView

--- lawnchair/src/app/lawnchair/views/CustomTextView.kt
+++ lawnchair/src/app/lawnchair/views/CustomTextView.kt
@@ -2,12 +2,12 @@ package app.lawnchair.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.TextView
+import androidx.appcompat.widget.AppCompatTextView
 import app.lawnchair.font.FontManager
 
 open class CustomTextView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
-) : TextView(context, attrs) {
+) : AppCompatTextView(context, attrs) {
 
     init {
         @Suppress("LeakingThis")
