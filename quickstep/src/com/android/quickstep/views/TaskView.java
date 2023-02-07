97ef7f005273d83b8fe52bfad94e0396b639aaaa
Pat Manning <patmanning@google.com>
2023-02-07 15:49:08 +0000

    Refresh thumbnail splash when task icon changes.
    This is usually a result of TaskIconCache updating in the background.
    
    Fix: 267744363
    Test: manual.
    Change-Id: I83620f3774def7ffb07906a6d45b64e9aad6de71

--- quickstep/src/com/android/quickstep/views/TaskView.java
+++ quickstep/src/com/android/quickstep/views/TaskView.java
@@ -1184,6 +1184,10 @@ public class TaskView extends FrameLayout implements Reusable {
         mSnapshotView.setSplashAlpha(mTaskThumbnailSplashAlpha);
     }
 
+    protected void refreshTaskThumbnailSplash() {
+        mSnapshotView.refreshSplashView();
+    }
+
     private void setSplitSelectTranslationX(float x) {
         mSplitSelectTranslationX = x;
         applyTranslationX();
