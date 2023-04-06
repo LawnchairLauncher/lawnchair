0b69d3878750817721d9151577249af1628b49f0
Tony Huang <tonyychuang@google.com>
2023-04-06 08:08:17 +0000

    Revert "Implement two intents or shortcuts to active split"
    Revert submission 22349283-intents-split
    
    Reason for revert: Cause master branch build break
    
    Reverted changes: /q/submissionid:22349283-intents-split
    
    Change-Id: I1030f801385b6f17e8565f2f64f71ba5229aed73

--- quickstep/src/com/android/quickstep/util/SplitSelectStateController.java
+++ quickstep/src/com/android/quickstep/util/SplitSelectStateController.java
@@ -298,10 +298,9 @@ public class SplitSelectStateController {
                         getOppositeStagePosition(stagePosition), splitRatio, remoteTransition,
                         shellInstanceId);
             } else {
-                mSystemUiProxy.startIntents(getPendingIntent(intent1), getShortcutInfo(intent1),
-                        options1.toBundle(), getPendingIntent(intent2), getShortcutInfo(intent2),
-                        null /* options2 */, stagePosition, splitRatio, remoteTransition,
-                        shellInstanceId);
+                mSystemUiProxy.startIntents(getPendingIntent(intent1), options1.toBundle(),
+                        getPendingIntent(intent2), null /* options2 */, stagePosition,
+                        splitRatio, remoteTransition, shellInstanceId);
             }
         } else {
             final RemoteSplitLaunchAnimationRunner animationRunner =
