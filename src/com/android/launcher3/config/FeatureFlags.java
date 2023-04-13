ff8b89cea4a75e324c2f3bcfc3645e54537652a7
fbaron <fbaron@google.com>
2023-04-13 14:56:10 -0700

    Turn on flag for download app ux V3
    Bug: 267272201
    Test: Verify that when the flag is on the new app download animation is visible
    Change-Id: Ic4c5cd9678d6eb5dd9d66114e82eb9d93d48fd46

--- src/com/android/launcher3/config/FeatureFlags.java
+++ src/com/android/launcher3/config/FeatureFlags.java
@@ -272,7 +272,7 @@ public final class FeatureFlags {
                     + " to have better visuals");
 
     public static final BooleanFlag ENABLE_DOWNLOAD_APP_UX_V3 = getDebugFlag(270395186,
-            "ENABLE_DOWNLOAD_APP_UX_V3", DISABLED, "Updates the download app UX"
+            "ENABLE_DOWNLOAD_APP_UX_V3", ENABLED, "Updates the download app UX"
                     + " to have better visuals, improve contrast, and color");
 
     public static final BooleanFlag FORCE_PERSISTENT_TASKBAR = getDebugFlag(270395077,
