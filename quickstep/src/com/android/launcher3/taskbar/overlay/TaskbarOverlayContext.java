29fcd9a6365ada87e9010d282977fa381b43529d
Jagrut Desai <jagrutdesai@google.com>
2023-05-04 10:50:38 -0700

    Taskbar All Apps Icon Shortcuts Popup Accesiiblity
    Before we were only applying the icon.clearAccessibilityFocus() only on normal popup and not material u popups, since the flag is turned on for udc, we can move it outside.
    
    There seems to be a bug associated with either nested abstract floating views or the accessibility service where upon opening a nested abstract floating view from another the icon get the focus. This change make the bug littl less visible to use but users will still see this bug if they open the shortcut pop agian withought closing taskbar all apps.
    
    The accessibility delegate was always null when using the TaskbarOverlayContext, now we use TaskbarActivityContext Delegate when in TaskbarOverlayContext.
    
    Test: Manual
    Bug: 280657266
    Flag: not needed
    Change-Id: I631e3aa6e54748c4c8ea2ac7daf5473b198e47d6

--- quickstep/src/com/android/launcher3/taskbar/overlay/TaskbarOverlayContext.java
+++ quickstep/src/com/android/launcher3/taskbar/overlay/TaskbarOverlayContext.java
@@ -86,6 +86,11 @@ public class TaskbarOverlayContext extends BaseTaskbarContext {
     }
 
     @Override
+    public View.AccessibilityDelegate getAccessibilityDelegate() {
+        return mTaskbarContext.getAccessibilityDelegate();
+    }
+
+    @Override
     public TaskbarDragController getDragController() {
         return mDragController;
     }
