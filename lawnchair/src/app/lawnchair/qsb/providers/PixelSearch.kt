597bfc667f090a4e958bf4b097f77ba56ec31fac
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2023-05-09 17:15:46 +0800

    `PixelSearch.kt` - use google logo

--- lawnchair/src/app/lawnchair/qsb/providers/PixelSearch.kt
+++ lawnchair/src/app/lawnchair/qsb/providers/PixelSearch.kt
@@ -6,8 +6,9 @@ import com.android.launcher3.R
 object PixelSearch : QsbSearchProvider(
     id = "pixel_search",
     name = R.string.search_provider_pixel_search,
-    icon = R.drawable.ic_qsb_search,
-    themingMethod = ThemingMethod.TINT,
+    // Use same style as Google Search
+    icon = R.drawable.ic_super_g_color,
+    themingMethod = ThemingMethod.THEME_BY_LAYER_ID,
     packageName = "rk.android.app.pixelsearch",
     website = "https://play.google.com/store/apps/details?id=rk.android.app.pixelsearch",
     type = QsbSearchProviderType.APP,
