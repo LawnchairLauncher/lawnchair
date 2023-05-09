b8e491eecd4cb151f0195eec0474077365d69ebd
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2023-05-09 19:18:49 +0800

    Tweak Pixel Search QSB UI (#3457)
    * `PixelSearch.kt` - use google logo
    
    * `LawnQsbLayout.kt` -  Add Pixel Search to providers with mic/lens support
    
    Pixel Search app technically needs Google to do the tasks, so this is added.
    
    * `LawnQsbLayout.kt` - import PixelSearch
    
    why did i forgor
    
    * Update PixelSearch.kt
    
    * reorganize providers in QsbSearchProvider.kt
    
    * PixelSearch.kt: use voice command intent
    
    * Update PixelSearch.kt

--- lawnchair/src/app/lawnchair/qsb/LawnQsbLayout.kt
+++ lawnchair/src/app/lawnchair/qsb/LawnQsbLayout.kt
@@ -23,6 +23,7 @@ import app.lawnchair.preferences2.subscribeBlocking
 import app.lawnchair.qsb.providers.AppSearch
 import app.lawnchair.qsb.providers.Google
 import app.lawnchair.qsb.providers.GoogleGo
+import app.lawnchair.qsb.providers.PixelSearch
 import app.lawnchair.qsb.providers.QsbSearchProvider
 import app.lawnchair.util.pendingIntent
 import app.lawnchair.util.recursiveChildren
@@ -67,8 +68,8 @@ class LawnQsbLayout(context: Context, attrs: AttributeSet?) : FrameLayout(contex
         clipIconRipples()
 
         val searchProvider = getSearchProvider(context, preferenceManager2)
-        val isGoogle = searchProvider == Google || searchProvider == GoogleGo
-        val supportsLens = searchProvider == Google
+        val isGoogle = searchProvider == Google || searchProvider == GoogleGo || searchProvider == PixelSearch
+        val supportsLens = searchProvider == Google || searchProvider == PixelSearch
 
         preferenceManager2.themedHotseatQsb.subscribeBlocking(scope = viewAttachedScope) { themed ->
             setUpBackground(themed)
