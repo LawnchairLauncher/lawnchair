a6b441bdeeec1a64dd283d11aab681bf74217e94
Vinit Nayak <peanutbutter@google.com>
2023-07-06 13:38:50 -0700

    Allow pressing home button on recents if command queue not empty
    Fixes: 290252373
    Test: Go home from overview, works
    Flag: None
    Change-Id: Ie40eb1891f38b1561eda89ad0f7987cdecd9da16

--- quickstep/src/com/android/quickstep/views/LauncherRecentsView.java
+++ quickstep/src/com/android/quickstep/views/LauncherRecentsView.java
@@ -90,8 +90,8 @@ public class LauncherRecentsView extends RecentsView<QuickstepLauncher, Launcher
     }
 
     @Override
-    protected boolean isCommandQueueEmpty() {
-        return mActivity.isCommandQueueEmpty();
+    protected boolean canStartHomeSafely() {
+        return mActivity.canStartHomeSafely();
     }
 
     @Override
