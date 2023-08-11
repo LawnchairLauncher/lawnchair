1b481e19733463ab96b0876909fdf5dcbefbb2ad
Sam Dubey <dubeyshubham@google.com>
2023-08-11 11:52:10 +0000

    Revert "Fix kotlin nullable errors in Launcher3"
    This reverts commit 7943b35a714af7e0fdc3f8bfd1ff0ee03b732cc0.
    
    Reason for revert: Seems to be breaking flicker tests b/295447665, will verify before submitting. This change could be WAI (or surfacing issues)
    
    Change-Id: Ic240c29539f41490cf7232bd6a7a944a868f3870

--- quickstep/src/com/android/launcher3/taskbar/TaskbarEduTooltip.kt
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarEduTooltip.kt
@@ -97,8 +97,8 @@ constructor(
     override fun onFinishInflate() {
         super.onFinishInflate()
 
-        content = requireViewById(R.id.content)
-        arrow = requireViewById(R.id.arrow)
+        content = findViewById(R.id.content)
+        arrow = findViewById(R.id.arrow)
         arrow.background =
             RoundedArrowDrawable(
                 arrowWidth,
