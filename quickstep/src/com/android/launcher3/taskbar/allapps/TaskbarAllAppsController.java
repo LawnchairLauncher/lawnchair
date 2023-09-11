91a4aca5a59a7550a22602940aa8786d283c08ed
Brian Isganitis <brianji@google.com>
2023-09-11 19:46:47 +0000

    Have backup TouchController of Taskbar All Apps AFV.
    BaseDragLayer only lets the top AFV intercpet touch events, otherwise it
    moves onto its TouchController instances. Taskbar AllApps still needs to
    intercept touch events sometimes when an AFV above it cannot (e.g. a
    snackbar). This CL adds a TouchController to TaskbarOverlayDragLayer
    that delegates to TaskbarAllAppsSlideInView if it is open.
    
    Test: Manual
    Fix: 299165179
    Flag: No
    Change-Id: I9b73c55f698de5b60b463f91140a6717a7053cd3

--- quickstep/src/com/android/launcher3/taskbar/allapps/TaskbarAllAppsController.java
+++ quickstep/src/com/android/launcher3/taskbar/allapps/TaskbarAllAppsController.java
@@ -161,6 +161,9 @@ public final class TaskbarAllAppsController {
 
         mSlideInView = (TaskbarAllAppsSlideInView) mOverlayContext.getLayoutInflater().inflate(
                 R.layout.taskbar_all_apps_sheet, mOverlayContext.getDragLayer(), false);
+        // Ensures All Apps gets touch events in case it is not the top floating view. Floating
+        // views above it may not be able to intercept the touch, so All Apps should try to.
+        mOverlayContext.getDragLayer().addTouchController(mSlideInView);
         mSlideInView.addOnCloseListener(() -> {
             mControllers.getSharedState().allAppsVisible = false;
             cleanUpOverlay();
@@ -195,6 +198,7 @@ public final class TaskbarAllAppsController {
             mSearchSessionController = null;
         }
         if (mOverlayContext != null) {
+            mOverlayContext.getDragLayer().removeTouchController(mSlideInView);
             mOverlayContext.setSearchSessionController(null);
             mOverlayContext = null;
         }
