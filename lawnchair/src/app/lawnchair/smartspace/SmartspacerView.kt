68824ff865d46cf706ea2a1a5275034bcb68c741
Kieron Quinn <quinny898@gmail.com>
2023-11-14 20:14:10 +0000

    - Update Smartspacer client SDK to 1.0.4 (contains fixes for popup handling) - Added options for dismiss, about & feedback, make them all optional and use the provided launchIntent method as it provides a fallback for invalid intents - Use original Customise option as fallback if provided options are empty

--- lawnchair/src/app/lawnchair/smartspace/SmartspacerView.kt
+++ lawnchair/src/app/lawnchair/smartspace/SmartspacerView.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.graphics.Rect
 import android.graphics.RectF
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import app.lawnchair.launcher
 import app.lawnchair.ui.preferences.PreferenceActivity
@@ -17,6 +16,7 @@ import com.kieronquinn.app.smartspacer.sdk.client.views.BcSmartspaceView
 import com.kieronquinn.app.smartspacer.sdk.client.views.popup.Popup
 import com.kieronquinn.app.smartspacer.sdk.client.views.popup.PopupFactory
 import com.kieronquinn.app.smartspacer.sdk.model.SmartspaceTarget
+import com.kieronquinn.app.smartspacer.sdk.client.R as SmartspacerR
 
 class SmartspacerView(context: Context, attrs: AttributeSet?) : BcSmartspaceView(context, attrs) {
     init {
@@ -36,21 +36,93 @@ class SmartspacerView(context: Context, attrs: AttributeSet?) : BcSmartspaceView
                 val launcher = context.launcher
                 val pos = Rect()
                 launcher.dragLayer.getDescendantRectRelativeToSelf(anchorView, pos)
-                OptionsPopupView.show(launcher, RectF(pos), listOf(getCustomizeOption()), true)
+                val options = listOfNotNull(
+                    getDismissOption(target, dismissAction),
+                    getAboutOption(launchIntent, aboutIntent),
+                    getFeedbackOption(launchIntent, feedbackIntent),
+                    getCustomizeOption(launchIntent, settingsIntent)
+                ).ifEmpty { listOf(getCustomizeOptionFallback()) }
+                val popup = OptionsPopupView
+                    .show(launcher, RectF(pos), options, true)
                 return object : Popup {
                     override fun dismiss() {
-
+                        popup.close(true)
                     }
                 }
             }
         }
     }
 
-    private fun getCustomizeOption() = OptionsPopupView.OptionItem(
+    private fun getDismissOption(
+        target: SmartspaceTarget,
+        dismissAction: ((SmartspaceTarget) -> Unit)?
+    ): OptionsPopupView.OptionItem? {
+        if(dismissAction == null) return null
+        return OptionsPopupView.OptionItem(
+            context,
+            SmartspacerR.string.smartspace_long_press_popup_dismiss,
+            SmartspacerR.drawable.ic_smartspace_long_press_dismiss,
+            StatsLogManager.LauncherEvent.IGNORE
+        ) {
+            dismissAction.invoke(target)
+            true
+        }
+    }
+
+    private fun getAboutOption(
+        launchIntent: (Intent?) -> Unit,
+        aboutIntent: Intent?
+    ): OptionsPopupView.OptionItem? {
+        if(aboutIntent == null) return null
+        return OptionsPopupView.OptionItem(
+            context,
+            SmartspacerR.string.smartspace_long_press_popup_about,
+            SmartspacerR.drawable.ic_smartspace_long_press_about,
+            StatsLogManager.LauncherEvent.IGNORE
+        ) {
+            launchIntent(aboutIntent)
+            true
+        }
+    }
+
+    private fun getFeedbackOption(
+        launchIntent: (Intent?) -> Unit,
+        feedbackIntent: Intent?
+    ): OptionsPopupView.OptionItem? {
+        if(feedbackIntent == null) return null
+        return OptionsPopupView.OptionItem(
+            context,
+            SmartspacerR.string.smartspace_long_press_popup_feedback,
+            SmartspacerR.drawable.ic_smartspace_long_press_feedback,
+            StatsLogManager.LauncherEvent.IGNORE
+        ) {
+            launchIntent(feedbackIntent)
+            true
+        }
+    }
+
+    private fun getCustomizeOption(
+        launchIntent: (Intent?) -> Unit,
+        settingsIntent: Intent?
+    ): OptionsPopupView.OptionItem? {
+        if(settingsIntent == null) return null
+        return OptionsPopupView.OptionItem(
+            context,
+            R.string.customize_button_text,
+            R.drawable.ic_setting,
+            StatsLogManager.LauncherEvent.IGNORE
+        ) {
+            launchIntent(settingsIntent)
+            true
+        }
+    }
+
+    private fun getCustomizeOptionFallback() = OptionsPopupView.OptionItem(
         context, R.string.customize_button_text, R.drawable.ic_setting,
         StatsLogManager.LauncherEvent.IGNORE
     ) {
         context.startActivity(PreferenceActivity.createIntent(context, "/${Routes.SMARTSPACE}/"))
         true
     }
+
 }
