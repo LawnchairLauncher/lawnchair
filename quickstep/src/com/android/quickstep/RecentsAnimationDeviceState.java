84d59b50e5c83943cba583f26b58e38558817baa
Rafael de Moura Dev <93414086+Rafael2616@users.noreply.github.com>
2023-12-01 21:52:32 -0300

    fix: avoid NPE in mExclusionListener due to it being null under Android 10

--- quickstep/src/com/android/quickstep/RecentsAnimationDeviceState.java
+++ quickstep/src/com/android/quickstep/RecentsAnimationDeviceState.java
@@ -260,6 +260,10 @@ public class RecentsAnimationDeviceState implements DisplayInfoChangeListener {
             mMode = info.navigationMode;
             mNavBarPosition = new NavBarPosition(mMode, info);
 
+            if (mExclusionListener == null) {
+                return
+            }
+
             if (mMode == NO_BUTTON) {
                 mExclusionListener.register();
             } else {
