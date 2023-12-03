1c918aa17247d6f5ebd152320c1c96daffc42461
MrSluffy <werdna.jac@gmail.com>
2023-12-03 14:54:08 +0800

    Update Workspace.java

--- src/com/android/launcher3/Workspace.java
+++ src/com/android/launcher3/Workspace.java
@@ -410,12 +410,9 @@ public class Workspace<T extends View & PageIndicator> extends PagedView<T>
         } else {
             lp.leftMargin = lp.rightMargin = 0;
             lp.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
-            boolean isDisabledHotseat;
-                if (Utilities.ATLEAST_Q) {
-                    isDisabledHotseat = getHotseat() != null && getHotseat().getQsb().getSourceLayoutResId() == R.layout.empty_view;
-                } else {
-                    isDisabledHotseat = true;
-                }
+            boolean isDisabledHotseat = Utilities.ATLEAST_Q &&
+                    getHotseat() != null &&
+                    getHotseat().getQsb().getSourceLayoutResId() == R.layout.empty_view;
             lp.bottomMargin = (padding.bottom) + grid.hotseatBarBottomSpacePx - (isDisabledHotseat ? grid.workspaceCellPaddingXPx * 3 : 0);
         }
         mPageIndicator.setLayoutParams(lp);
