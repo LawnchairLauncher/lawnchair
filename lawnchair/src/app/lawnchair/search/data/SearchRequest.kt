d5a70dada3fc06bef6e721a84fd727bb5715020c
MrSluffy <werdna.jac@gmail.com>
2023-12-06 06:51:03 +0800

    Surround try catch to give proper error logs
    java.lang.NullPointerException
    	at app.lawnchair.search.data.SearchRequestKt$findContactsByName$2.invokeSuspend(SearchRequest.kt:124)
    	at app.lawnchair.search.data.SearchRequestKt$findContactsByName$2.invoke(Unknown Source:8)
    	at app.lawnchair.search.data.SearchRequestKt$findContactsByName$2.invoke(Unknown Source:4)
    	at kotlinx.coroutines.intrinsics.UndispatchedKt.startUndispatchedOrReturn(Undispatched.kt:78)
    	at kotlinx.coroutines.BuildersKt__Builders_commonKt.withContext(Builders.common.kt:167)
    	at kotlinx.coroutines.BuildersKt.withContext(Unknown Source:1)
    	at app.lawnchair.search.data.SearchRequestKt.findContactsByName(SearchRequest.kt:68)
    	at app.lawnchair.search.LawnchairSearchAlgorithm$performDeviceWideSearch$2.invokeSuspend(LawnchairSearchAlgorithm.kt:152)
    	at kotlin.coroutines.jvm.internal.BaseContinuationImpl.resumeWith(ContinuationImpl.kt:33)

--- lawnchair/src/app/lawnchair/search/data/SearchRequest.kt
+++ lawnchair/src/app/lawnchair/search/data/SearchRequest.kt
@@ -34,115 +34,119 @@ private val retrofit = Retrofit.Builder()
 val startPageService: StartPageService = retrofit.create()
 
 suspend fun getStartPageSuggestions(query: String, max: Int): List<String> {
-    if (query.isEmpty() || query.isBlank() || max <= 0) return emptyList()
-    val exceptionHandler = CoroutineExceptionHandler { _, e ->
-        Log.e("Exception", "Error during suggestion retrieval: ${e.message}")
-    }
-    return withContext(Dispatchers.IO + exceptionHandler) {
-        val response: Response<ResponseBody> = startPageService.getStartPageSuggestions(
-            query = query,
-            segment = "startpage.lawnchair",
-            partner = "lawnchair",
-            format = "opensearch",
-        )
-
-        if (response.isSuccessful) {
-            val responseBody = response.body()?.string()
-
-            return@withContext JSONArray(responseBody).optJSONArray(1)?.let { array ->
-                (0 until array.length()).take(max).map { array.getString(it) }
-            } ?: emptyList()
-        } else {
-            Log.d("Failed to retrieve suggestions", ": ${response.code()}")
+    try {
+        if (query.isEmpty() || query.isBlank() || max <= 0) return emptyList()
+        return withContext(Dispatchers.IO) {
+            val response: Response<ResponseBody> = startPageService.getStartPageSuggestions(
+                query = query,
+                segment = "startpage.lawnchair",
+                partner = "lawnchair",
+                format = "opensearch",
+            )
+            if (response.isSuccessful) {
+                val responseBody = response.body()?.string()
+                return@withContext JSONArray(responseBody).optJSONArray(1)?.let { array ->
+                    (0 until array.length()).take(max).map { array.getString(it) }
+                } ?: emptyList()
+            } else {
+                Log.d("Failed to retrieve suggestions", ": ${response.code()}")
+            }
+            return@withContext emptyList()
         }
-
-        return@withContext emptyList()
+    } catch (e: Exception) {
+        Log.e("Exception", "Error during suggestion retrieval: ${e.message}")
+        return emptyList()
     }
 }
 
 suspend fun findContactsByName(context: Context, query: String, max: Int): List<ContactInfo> {
-    if (query.isEmpty() || query.isBlank()) return emptyList()
-    val exceptionHandler = CoroutineExceptionHandler { _, e ->
-        Log.e("ContactSearch", "Something went wrong ", e)
-    }
-    return withContext(Dispatchers.IO + exceptionHandler) {
-        val contactMap = HashMap<String, ContactInfo>()
+    try {
+        if (query.isEmpty() || query.isBlank()) return emptyList()
+        val exceptionHandler = CoroutineExceptionHandler { _, e ->
+            Log.e("ContactSearch", "Something went wrong ", e)
+        }
+        return withContext(Dispatchers.IO + exceptionHandler) {
+            val contactMap = HashMap<String, ContactInfo>()
 
-        val projection = arrayOf(
-            ContactsContract.Data._ID,
-            ContactsContract.Data.CONTACT_ID,
-            ContactsContract.Data.DISPLAY_NAME,
-            ContactsContract.Data.DATA1,
-            ContactsContract.Data.DATA3,
-            ContactsContract.Data.DATA5,
-            "phonebook_label",
-            "account_type",
-            "account_name",
-            ContactsContract.Data.MIMETYPE,
-            ContactsContract.Data.PHOTO_URI,
-        )
+            val projection = arrayOf(
+                ContactsContract.Data._ID,
+                ContactsContract.Data.CONTACT_ID,
+                ContactsContract.Data.DISPLAY_NAME,
+                ContactsContract.Data.DATA1,
+                ContactsContract.Data.DATA3,
+                ContactsContract.Data.DATA5,
+                "phonebook_label",
+                "account_type",
+                "account_name",
+                ContactsContract.Data.MIMETYPE,
+                ContactsContract.Data.PHOTO_URI,
+            )
 
-        val selection = ContactsContract.Data.DISPLAY_NAME + " LIKE ?"
-        val selectionArgs = arrayOf("%$query%")
+            val selection = ContactsContract.Data.DISPLAY_NAME + " LIKE ?"
+            val selectionArgs = arrayOf("%$query%")
 
-        context.contentResolver.query(
-            ContactsContract.Data.CONTENT_URI,
-            projection,
-            selection,
-            selectionArgs,
-            null,
-        )?.use {
-            while (it.moveToNext() && contactMap.size <= max) {
-                val contactIdIndex = it.getColumnIndex(ContactsContract.Data.CONTACT_ID)
-                val displayNameIndex = it.getColumnIndex(ContactsContract.Data.DISPLAY_NAME)
-                val data1Index = it.getColumnIndex(ContactsContract.Data.DATA1)
-                val data3Index = it.getColumnIndex(ContactsContract.Data.DATA3)
-                val data5Index = it.getColumnIndex(ContactsContract.Data.DATA5)
-                val phonebookLabelIndex = it.getColumnIndex("phonebook_label")
-                val accountTypeIndex = it.getColumnIndex("account_type")
-                val accountNameIndex = it.getColumnIndex("account_name")
-                val mimeTypeIndex = it.getColumnIndex(ContactsContract.Data.MIMETYPE)
-                val photoUriIndex = it.getColumnIndex(ContactsContract.Data.PHOTO_URI)
-                val contactId = it.getString(contactIdIndex)
-                val displayName = it.getString(displayNameIndex)
-                val data1 = it.getString(data1Index)
-                val data3 = it.getString(data3Index)
-                val data5 = it.getString(data5Index)
-                val phonebookLabel = it.getString(phonebookLabelIndex)
-                val accountType = it.getString(accountTypeIndex)
-                val accountName = it.getString(accountNameIndex)
-                val mimeType = it.getString(mimeTypeIndex)
-                val photoUri = it.getString(photoUriIndex)
-                val phoneNumber = data3 ?: data5 ?: data1
-                val key = contactId ?: phoneNumber
-                val imageUri = photoUri ?: ""
-                val phonebookLabel2 = phonebookLabel ?: ""
-                if (key != null && !EXCLUDED_MIME_TYPES.contains(mimeType)) {
-                    contactMap[key] = ContactInfo(
-                        contactId,
-                        displayName,
-                        phoneNumber,
-                        phonebookLabel2,
-                        imageUri,
-                        JSONArray().toString(),
-                    )
-                } else {
-                    if (contactMap.containsKey(contactId)) {
-                        val existingContact = contactMap[contactId]
-                        val jsonArray = JSONArray(existingContact?.packages ?: "")
-                        val jsonObject = JSONObject()
-                        jsonObject.put(CONTACT_ACCOUNT_ID, key)
-                        jsonObject.put(CONTACT_ACCOUNT_TITLE, data5)
-                        jsonObject.put(CONTACT_ACCOUNT_NAME, accountName)
-                        jsonObject.put(CONTACT_ACCOUNT_TYPE, accountType)
-                        jsonObject.put(CONTACT_ACCOUNT_MIME, mimeType)
-                        jsonArray.put(jsonObject)
-                        existingContact?.packages = jsonArray.toString()
+            context.contentResolver.query(
+                ContactsContract.Data.CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+            )?.use {
+                while (it.moveToNext() && contactMap.size <= max) {
+                    val contactIdIndex = it.getColumnIndex(ContactsContract.Data.CONTACT_ID)
+                    val displayNameIndex = it.getColumnIndex(ContactsContract.Data.DISPLAY_NAME)
+                    val data1Index = it.getColumnIndex(ContactsContract.Data.DATA1)
+                    val data3Index = it.getColumnIndex(ContactsContract.Data.DATA3)
+                    val data5Index = it.getColumnIndex(ContactsContract.Data.DATA5)
+                    val phonebookLabelIndex = it.getColumnIndex("phonebook_label")
+                    val accountTypeIndex = it.getColumnIndex("account_type")
+                    val accountNameIndex = it.getColumnIndex("account_name")
+                    val mimeTypeIndex = it.getColumnIndex(ContactsContract.Data.MIMETYPE)
+                    val photoUriIndex = it.getColumnIndex(ContactsContract.Data.PHOTO_URI)
+                    val contactId = it.getString(contactIdIndex)
+                    val displayName = it.getString(displayNameIndex)
+                    val data1 = it.getString(data1Index)
+                    val data3 = it.getString(data3Index)
+                    val data5 = it.getString(data5Index)
+                    val phonebookLabel = it.getString(phonebookLabelIndex)
+                    val accountType = it.getString(accountTypeIndex)
+                    val accountName = it.getString(accountNameIndex)
+                    val mimeType = it.getString(mimeTypeIndex)
+                    val photoUri = it.getString(photoUriIndex)
+                    val phoneNumber = data3 ?: data5 ?: data1
+                    val key = contactId ?: phoneNumber
+                    val imageUri = photoUri ?: ""
+                    val phonebookLabel2 = phonebookLabel ?: ""
+                    if (key != null && !EXCLUDED_MIME_TYPES.contains(mimeType)) {
+                        contactMap[key] = ContactInfo(
+                            contactId,
+                            displayName,
+                            phoneNumber,
+                            phonebookLabel2,
+                            imageUri,
+                            JSONArray().toString(),
+                        )
+                    } else {
+                        if (contactMap.containsKey(contactId)) {
+                            val existingContact = contactMap[contactId]
+                            val jsonArray = JSONArray(existingContact?.packages ?: "")
+                            val jsonObject = JSONObject()
+                            jsonObject.put(CONTACT_ACCOUNT_ID, key)
+                            jsonObject.put(CONTACT_ACCOUNT_TITLE, data5)
+                            jsonObject.put(CONTACT_ACCOUNT_NAME, accountName)
+                            jsonObject.put(CONTACT_ACCOUNT_TYPE, accountType)
+                            jsonObject.put(CONTACT_ACCOUNT_MIME, mimeType)
+                            jsonArray.put(jsonObject)
+                            existingContact?.packages = jsonArray.toString()
+                        }
                     }
                 }
             }
+            contactMap.values.toList()
         }
-        contactMap.values.toList()
+    } catch (e : Exception) {
+        Log.e("ContactSearch", "Something went wrong ", e)
+        return emptyList()
     }
 }
 
