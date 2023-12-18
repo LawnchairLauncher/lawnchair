d506f23d334680497719220f22002a45c4d9df2c
Goooler <wangzongler@gmail.com>
2023-12-18 10:22:02 +0800

    Fix authority in LawnchairRecentSuggestionProvider

--- lawnchair/src/app/lawnchair/search/LawnchairRecentSuggestionProvider.kt
+++ lawnchair/src/app/lawnchair/search/LawnchairRecentSuggestionProvider.kt
@@ -1,10 +1,11 @@
 package app.lawnchair.search
 
 import android.content.SearchRecentSuggestionsProvider
+import com.android.launcher3.BuildConfig
 
 class LawnchairRecentSuggestionProvider : SearchRecentSuggestionsProvider() {
     companion object {
-        const val AUTHORITY = "app.lawnchair.search.LawnchairRecentSuggestionProvider"
+        const val AUTHORITY = BuildConfig.APPLICATION_ID + ".search.LawnchairRecentSuggestionProvider"
         const val MODE = DATABASE_MODE_QUERIES
     }
 
