e921a7b183bd2f70aab8600f9aceee984586fd78
Tony Wickham <twickham@google.com>
2024-01-19 20:04:59 +0000

    Revert to passing mIsStashed to StashedHandleViewController again
    - Fixes case where handle is initially invisible but will be stashed
      and visible, e.g. when launching an app from home screen
    - To fix the original issue of sampling happening unnecessarily for
      three button nav, also check supportsVisualStashing()
    
    Fixes: 321257120
    Flag: None
    Test: Launch an app from home in gesture nav mode
    Test: testThreeButtonsTaskbarBoundsAfterConfigChangeDuringIme
    (cherry picked from commit c4df8c2ec68d443716dcb004f77b7bda8a5aaf6a)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c5f03fd1bcfc24ec63bdd62b0606452f28d15664)
    Merged-In: I1f26436de3081fd00f75a5921010e361e9a9f34e
    Change-Id: I1f26436de3081fd00f75a5921010e361e9a9f34e

--- quickstep/src/com/android/launcher3/taskbar/TaskbarStashController.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarStashController.java
@@ -600,7 +600,7 @@ public class TaskbarStashController implements TaskbarControllers.LoggableTaskba
             mAnimator.addListener(AnimatorListeners.forEndCallback(() -> {
                 mAnimator = null;
                 mIsStashed = isStashed;
-                onIsStashedChanged();
+                onIsStashedChanged(mIsStashed);
             }));
             return;
         }
@@ -615,7 +615,7 @@ public class TaskbarStashController implements TaskbarControllers.LoggableTaskba
             @Override
             public void onAnimationStart(Animator animation) {
                 mIsStashed = isStashed;
-                onIsStashedChanged();
+                onIsStashedChanged(mIsStashed);
 
                 cancelTimeoutIfExists();
             }
@@ -840,9 +840,10 @@ public class TaskbarStashController implements TaskbarControllers.LoggableTaskba
                 .setDuration(TASKBAR_HINT_STASH_DURATION).start();
     }
 
-    private void onIsStashedChanged() {
+    private void onIsStashedChanged(boolean isStashed) {
         mControllers.runAfterInit(() -> {
-            mControllers.stashedHandleViewController.onIsStashedChanged();
+            mControllers.stashedHandleViewController.onIsStashedChanged(
+                    isStashed && supportsVisualStashing());
             mControllers.taskbarInsetsController.onTaskbarOrBubblebarWindowHeightOrInsetsChanged();
         });
     }
