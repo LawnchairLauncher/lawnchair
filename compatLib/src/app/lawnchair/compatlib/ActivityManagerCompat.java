6542987d5fcca9265aa23d0a2912bb345f5a4566
MrSluffy <werdna.jac@gmail.com>
2024-01-22 14:50:09 +0800

    Revert "Convert compatlib java to kt" as causes gesture issue in oneui
    This reverts commit 73c3ab1bce00c3c72a6b2dc459519f539235efac.

---
+++ compatLib/src/app/lawnchair/compatlib/ActivityManagerCompat.java
@@ -0,0 +1,38 @@
+package app.lawnchair.compatlib;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Intent;
+import android.window.TaskSnapshot;
+
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public abstract class ActivityManagerCompat {
+
+    public static final int NUM_RECENT_ACTIVITIES_REQUEST = 3;
+
+    public abstract void invalidateHomeTaskSnapshot(final Activity homeActivity);
+
+    /**
+     *  Called only in S+ platform
+     * @param taskId
+     * @param isLowResolution
+     * @param takeSnapshotIfNeeded
+     * @return
+     */
+    @Nullable
+    public TaskSnapshot getTaskSnapshot(int taskId, boolean isLowResolution,
+                                        boolean takeSnapshotIfNeeded) {
+        return null;
+    }
+
+    public abstract void startRecentsActivity(Intent intent, long eventTime, RecentsAnimationRunnerCompat runnerCompat);
+
+    public abstract ActivityManager.RunningTaskInfo getRunningTask(boolean filterOnlyVisibleRecents);
+
+    public abstract List<ActivityManager.RecentTaskInfo> getRecentTasks(int numTasks, int userId);
+
+    public abstract ActivityManager.RunningTaskInfo[] getRunningTasks(boolean filterOnlyVisibleRecents);
+}
---
+++ compatLib/src/app/lawnchair/compatlib/QuickstepCompatFactory.java
@@ -0,0 +1,16 @@
+package app.lawnchair.compatlib;
+
+import androidx.annotation.NonNull;
+
+public abstract class QuickstepCompatFactory {
+
+    @NonNull
+    public abstract ActivityManagerCompat getActivityManagerCompat();
+
+    @NonNull
+    public abstract ActivityOptionsCompat getActivityOptionsCompat();
+
+    @NonNull
+    public abstract RemoteTransitionCompat getRemoteTransitionCompat();
+
+}
