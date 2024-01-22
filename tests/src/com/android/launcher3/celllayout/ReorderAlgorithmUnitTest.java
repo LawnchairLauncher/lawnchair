13fc236b8773139050bf54861e47817ef89e37f6
Sebastián Franco <fransebas@google.com>
2024-01-22 18:43:42 +0000

    Revert "Adding ReorderParameters to ReorderAlgorithm"
    This reverts commit 25f8e406a738e1274e6eeaa8858ddfd54d9fed51.
    
    Reason for revert: b/318417510
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:93715f140b818d1b2294a79b5d24f2a3cbd2dac1)
    Merged-In: I175a49b6cf7db82eeec71fa24930736b58f8ed00
    Change-Id: I175a49b6cf7db82eeec71fa24930736b58f8ed00

--- tests/src/com/android/launcher3/celllayout/ReorderAlgorithmUnitTest.java
+++ tests/src/com/android/launcher3/celllayout/ReorderAlgorithmUnitTest.java
@@ -191,21 +191,9 @@ public class ReorderAlgorithmUnitTest {
 
         int[] testCaseXYinPixels = new int[2];
         cl.regionToCenterPoint(x, y, spanX, spanY, testCaseXYinPixels);
-        ItemConfiguration configuration = new ItemConfiguration();
-        cl.copyCurrentStateToSolution(configuration);
-        ItemConfiguration solution = cl.createReorderAlgorithm()
-                .calculateReorder(
-                        new ReorderParameters(
-                                testCaseXYinPixels[0],
-                                testCaseXYinPixels[1],
-                                spanX,
-                                spanY,
-                                minSpanX,
-                                minSpanY,
-                                null,
-                                configuration
-                        )
-                );
+        ItemConfiguration solution = cl.createReorderAlgorithm().calculateReorder(
+                testCaseXYinPixels[0], testCaseXYinPixels[1], minSpanX, minSpanY, spanX, spanY,
+                null);
         if (solution == null) {
             solution = new ItemConfiguration();
             solution.isSolution = false;
