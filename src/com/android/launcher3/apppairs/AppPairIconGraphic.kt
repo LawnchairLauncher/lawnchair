cfcf2255dc5abbddcafae334d42ca5e0790183a1
Jeremy Sim <jeremysim@google.com>
2024-01-30 17:30:51 +0800

    Add error logging and better crash safety for app pairs
    This causes app pairs to log an error message if creation or icon inflation is attempted with contents != 2. App pairs with wrong numbers of members should not be created anymore.
    
    Fixes: 317283244
    Fixes: 322892793
    Test: Manual
    Flag: ACONFIG com.android.wm.shell.enable_app_pairs TEAMFOOD
    Change-Id: I5e454042719bb67187f900c546380bb44cfc34ac

--- src/com/android/launcher3/apppairs/AppPairIconGraphic.kt
+++ src/com/android/launcher3/apppairs/AppPairIconGraphic.kt
@@ -93,7 +93,7 @@ class AppPairIconGraphic @JvmOverloads constructor(context: Context, attrs: Attr
     private fun applyIcons(contents: ArrayList<WorkspaceItemInfo>) {
         // App pair should always contain 2 members; if not 2, return to avoid a crash loop
         if (contents.size != 2) {
-            Log.w(TAG, "AppPair contents not 2, size: " + contents.size, Throwable())
+            Log.wtf(TAG, "AppPair contents not 2, size: " + contents.size, Throwable())
             return
         }
 
@@ -111,7 +111,6 @@ class AppPairIconGraphic @JvmOverloads constructor(context: Context, attrs: Attr
         appIcon1?.setBounds(0, 0, memberIconSize.toInt(), memberIconSize.toInt())
         appIcon2?.setBounds(0, 0, memberIconSize.toInt(), memberIconSize.toInt())
     }
-
 
     /** Gets this icon graphic's bounds, with respect to the parent icon's coordinate system. */
     fun getIconBounds(outBounds: Rect) {
