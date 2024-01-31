1121f8b5b954fe2c65b64acf217ad51853799de3
David Saff <saff@google.com>
2024-01-31 19:35:40 +0000

    Assert keyguard gone at beginning and end of PortraitLandscapeRunner
    Should help distinguish if we have tests leaving things in a bad state,
    or the intervening infrastructure.
    
    Bug: 285935417
    Change-Id: I5569472a9804cb21ee0646c72decb5f51be9ab01

--- tests/multivalentTests/src/com/android/launcher3/ui/PortraitLandscapeRunner.java
+++ tests/multivalentTests/src/com/android/launcher3/ui/PortraitLandscapeRunner.java
@@ -44,7 +44,9 @@ public class PortraitLandscapeRunner implements TestRule {
             @Override
             public void evaluate() throws Throwable {
                 try {
+                    // we expect to begin unlocked...
                     AbstractLauncherUiTest.verifyKeyguardInvisible();
+
                     mTest.mDevice.pressHome();
                     mTest.waitForLauncherCondition("Launcher activity wasn't created",
                             launcher -> launcher != null);
@@ -67,6 +69,9 @@ public class PortraitLandscapeRunner implements TestRule {
                         }
                     });
                     mTest.mLauncher.setExpectedRotation(Surface.ROTATION_0);
+
+                    // and end unlocked...
+                    AbstractLauncherUiTest.verifyKeyguardInvisible();
                 }
             }
 
