c0a78600456d8e7b2d17e5e9c92a0e9793496861
MrSluffy <werdna.jac@gmail.com>
2024-02-09 07:45:04 +0800

    Fix conflicts

--- quickstep/src/com/android/quickstep/interaction/AllSetActivity.java
+++ quickstep/src/com/android/quickstep/interaction/AllSetActivity.java
@@ -270,9 +270,9 @@ public class AllSetActivity extends Activity {
 
     private void onTISConnected(TISBinder binder) {
         setSetupUIVisible(isResumed());
-        mBinder.setSwipeUpProxy(isResumed() ? this::createSwipeUpProxy : null);
-        mBinder.setOverviewTargetChangeListener(mBinder::preloadOverviewForSUWAllSet);
-        mBinder.preloadOverviewForSUWAllSet();
+        binder.setSwipeUpProxy(isResumed() ? this::createSwipeUpProxy : null);
+        binder.setOverviewTargetChangeListener(binder::preloadOverviewForSUWAllSet);
+        binder.preloadOverviewForSUWAllSet();
         TaskbarManager taskbarManager = binder.getTaskbarManager();
         if (taskbarManager != null) {
             mLauncherStartAnim = taskbarManager.createLauncherStartFromSuwAnim(MAX_SWIPE_DURATION);
