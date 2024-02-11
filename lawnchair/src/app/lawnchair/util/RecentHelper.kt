861f4439e4d0f8d15c459b4191b2aedf297cad93
MrSluffy <werdna.jac@gmail.com>
2024-02-11 09:28:18 +0800

    Fixed clear all action in A10
    error : java.lang.NoSuchMethodError: No static method getInstance()Landroid/app/ActivityTaskManager; in class Landroid/app/ActivityTaskManager; or its super classes (declaration of 'android.app.ActivityTaskManager' appears in /system/framework/framework.jar)
    	at t7.g0.a(Unknown Source:33)
    	at com.android.quickstep.views.RecentsView.lambda$createAllTasksDismissAnimation$29(Unknown Source:4)
    	at com.android.quickstep.views.RecentsView.T(Unknown Source:0)

--- lawnchair/src/app/lawnchair/util/RecentHelper.kt
+++ lawnchair/src/app/lawnchair/util/RecentHelper.kt
@@ -1,12 +1,11 @@
 package app.lawnchair.util
 
-import android.app.ActivityManager
-import android.app.ActivityTaskManager
 import android.content.Context
 import android.os.Process
 import android.os.UserHandle
+import app.lawnchair.LawnchairLauncher
+import app.lawnchair.launcher
 import com.android.launcher3.BuildConfig
-import com.android.launcher3.Launcher
 import com.android.quickstep.views.RecentsView
 import com.android.systemui.shared.recents.model.Task
 import com.android.systemui.shared.system.ActivityManagerWrapper
@@ -15,14 +14,14 @@ object RecentHelper {
 
     fun clearAllTaskStacks(context: Context) {
         try {
-            val launcher = Launcher.getLauncher(context)
-            val recentsView = launcher.getOverviewPanel<RecentsView<Launcher, *>>()
+            val launcher = context.launcher
+            val recentsView = launcher.getOverviewPanel<RecentsView<LawnchairLauncher, *>>()
             val taskViewCount = recentsView.getTaskViewCount()
             val currentUserId = Process.myUserHandle().identifier
             for (i in 0..taskViewCount) {
                 try {
-                    val rawTasks = ActivityTaskManager.getInstance()
-                        .getRecentTasks(i, ActivityManager.RECENT_IGNORE_UNAVAILABLE, currentUserId)
+                    val rawTasks = ActivityManagerWrapper.getInstance()
+                        .getRecentTasks(i, currentUserId)
                     for (recentTaskInfo in rawTasks) {
                         var packageName = recentTaskInfo.baseIntent.component?.packageName
                         val taskKey = Task.TaskKey(recentTaskInfo)
