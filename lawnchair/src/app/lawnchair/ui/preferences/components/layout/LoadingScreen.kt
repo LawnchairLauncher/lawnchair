7f45d4ce5fe1d10943d65d6964a2f7bf49e444d7
Goooler <wangzongler@gmail.com>
2024-02-22 17:51:36 +0800

    Migrate to M3 CircularProgressIndicator

--- lawnchair/src/app/lawnchair/ui/preferences/components/layout/LoadingScreen.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/layout/LoadingScreen.kt
@@ -20,7 +20,7 @@ import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
