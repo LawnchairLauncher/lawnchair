9aee3f651827a92de7ce937d331e5cdb6c78f0fe
Goooler <wangzongler@gmail.com>
2024-02-22 17:57:05 +0800

    Remove imports

--- lawnchair/src/app/lawnchair/ui/preferences/components/layout/PreferenceSearchScaffold.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/layout/PreferenceSearchScaffold.kt
@@ -33,9 +33,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
