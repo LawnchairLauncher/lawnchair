c94b2a7045eb3ebcf0ab1fc2b4834cc5647aad21
Vadim Tryshev <vadimt@google.com>
2024-02-22 16:17:40 -0800

    Add tracing for a bug where current RecentsActivity is null
    Bug: 321775748
    Test: presubmit
    Flag: N/A
    Change-Id: I46fde7a73a3bfe02ed6276b4ac5f3e6c33bda387

--- tests/multivalentTests/shared/com/android/launcher3/testing/shared/TestProtocol.java
+++ tests/multivalentTests/shared/com/android/launcher3/testing/shared/TestProtocol.java
@@ -172,6 +172,7 @@ public final class TestProtocol {
     public static final String OVERVIEW_OVER_HOME = "b/279059025";
     public static final String UIOBJECT_STALE_ELEMENT = "b/319501259";
     public static final String ACTIVITY_NOT_RESUMED_AFTER_BACK = "b/322823209";
+    public static final String GET_FROM_RECENTS_FAILURE = "b/321775748";
 
     public static final String REQUEST_EMULATE_DISPLAY = "emulate-display";
     public static final String REQUEST_STOP_EMULATE_DISPLAY = "stop-emulate-display";
