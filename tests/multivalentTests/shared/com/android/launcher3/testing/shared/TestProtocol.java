dae8b65dea6bceafbcf225ffb691f2ec6ab281d0
Vadim Tryshev <vadimt@google.com>
2024-02-26 17:48:51 -0800

    Removing logging for verifying internal Launcher state from tests
    Bug: 322823209
    Test: presubmit
    Flag: N/A
    Change-Id: I25afcc30446d429171445f8e1b6033b277166725

--- tests/multivalentTests/shared/com/android/launcher3/testing/shared/TestProtocol.java
+++ tests/multivalentTests/shared/com/android/launcher3/testing/shared/TestProtocol.java
@@ -171,7 +171,6 @@ public final class TestProtocol {
     public static final String ICON_MISSING = "b/282963545";
     public static final String OVERVIEW_OVER_HOME = "b/279059025";
     public static final String UIOBJECT_STALE_ELEMENT = "b/319501259";
-    public static final String ACTIVITY_NOT_RESUMED_AFTER_BACK = "b/322823209";
     public static final String GET_FROM_RECENTS_FAILURE = "b/321775748";
 
     public static final String REQUEST_EMULATE_DISPLAY = "emulate-display";
