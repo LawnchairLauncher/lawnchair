6ce297a2ff1583e49cf3e2e1e89d1ead7f83dac0
Andreas Agvard <agvard@google.com>
2024-03-13 18:31:51 +0100

    Removes recovery
    Flag: NONE
    Bug: 326143814
    Test: Unit
    Change-Id: Icbebeade552cb101eb4b8bae4965b4d6c972f22e

--- quickstep/src/com/android/quickstep/util/AssistStateManager.java
+++ quickstep/src/com/android/quickstep/util/AssistStateManager.java
@@ -52,11 +52,6 @@ public class AssistStateManager implements ResourceBasedOverride {
         return Optional.empty();
     }
 
-    /** Whether search recovery is available. */
-    public boolean isVisRecoveryEnabled() {
-        return false;
-    }
-
     /** Return {@code true} if the Settings toggle is enabled. */
     public boolean isSettingsNavHandleEnabled() {
         return false;
