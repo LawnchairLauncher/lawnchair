4f8fa6b825a407c72bfc0308b6a9a3d9fadb6b1a
SuperDragonXD <70206496+SuperDragonXD@users.noreply.github.com>
2024-04-03 19:53:51 +0800

    Fix pref_searchResultApps

--- lawnchair/src/app/lawnchair/preferences/PreferenceManager.kt
+++ lawnchair/src/app/lawnchair/preferences/PreferenceManager.kt
@@ -80,7 +80,7 @@ class PreferenceManager private constructor(private val context: Context) : Base
     val searchResultPixelTips = BoolPref("pref_searchResultPixelTips", false)
     val searchResultSettings = BoolPref("pref_searchResultSettings", false)
 
-    val searchResultApps = BoolPref("pref_searchResultFiles", true, recreate)
+    val searchResultApps = BoolPref("pref_searchResultApps", true, recreate)
     val searchResultFiles = BoolPref("pref_searchResultFiles", false, recreate)
     val searchResultStartPageSuggestion = BoolPref("pref_searchResultStartPageSuggestion", true, recreate)
     val searchResultSettingsEntry = BoolPref("pref_searchResultSettingsEntry", false, recreate)
