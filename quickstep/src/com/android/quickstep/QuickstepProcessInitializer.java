17a32be72de814ab753f7bab6eae9cdeee4c3778
Pun Butrach <pun.butrach@gmail.com>
2024-04-27 15:05:20 +0700

    Don't elevate priority for devices below Q
    EGL_CONTEXT_PRIORITY_HIGH_IMG doesn't exist for Android below Q and
    Lawnchair Launcher don't support QuickSwitch for Android Q anyway,
    the effect should have no effect on daily usage.
    
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- quickstep/src/com/android/quickstep/QuickstepProcessInitializer.java
+++ quickstep/src/com/android/quickstep/QuickstepProcessInitializer.java
@@ -64,7 +64,7 @@ public class QuickstepProcessInitializer extends MainProcessInitializer {
 
                 // Elevate GPU priority for Quickstep and Remote animations.
                 try {
-                        ThreadedRenderer.setContextPriority(
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) ThreadedRenderer.setContextPriority(
                                         ThreadedRenderer.EGL_CONTEXT_PRIORITY_HIGH_IMG);
                 } catch (Exception e) {
                         Log.e(TAG, "init: " + e);
