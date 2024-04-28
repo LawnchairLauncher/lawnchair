920b9e2db64aa01e61d3d62805d8943bb30e75d9
Pun Butrach <pun.butrach@gmail.com>
2024-04-28 16:57:07 +0700

    Don't elevate priority for devices below P
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    
    (amend 752c3757b1ad9d25ceb3d2c1b6510d6cc9cee331)
    
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- quickstep/src/com/android/quickstep/QuickstepProcessInitializer.java
+++ quickstep/src/com/android/quickstep/QuickstepProcessInitializer.java
@@ -65,7 +65,7 @@ public class QuickstepProcessInitializer extends MainProcessInitializer {
 
                 // Elevate GPU priority for Quickstep and Remote animations.
                 try {
-                        if (!Utilities.ATLEAST_Q) return;
+                        if (!Utilities.ATLEAST_P) return;
                         ThreadedRenderer.setContextPriority(
                                         ThreadedRenderer.EGL_CONTEXT_PRIORITY_HIGH_IMG);
                 } catch (Exception e) {
