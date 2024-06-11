4a4b7a80592896055727d25416b3f23092a43625
Jon Miranda <jonmiranda@google.com>
2024-06-11 14:17:20 -0700

    Delay taskbar background fade in animation - Taskbar background will fade in based on   1. Velocity threshold   2. End target gesture destination
    Bug: 298089923
    Bug: 345768019
    Test: swipe up fast to go home, note no taskbar bg
          swipe up slow to reveal taskhome, note taskbar bg shows immediately
    Flag: com.android.launcher3.enable_scaling_reveal_home_animation DISABLED
    
    Change-Id: I2c16352e1c0c52a8afc49900a39b80383bacde62

--- quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
+++ quickstep/src/com/android/launcher3/taskbar/TaskbarActivityContext.java
@@ -1406,6 +1406,13 @@ public class TaskbarActivityContext extends BaseTaskbarContext {
     }
 
     /**
+     * Plays the taskbar background alpha animation if one is not currently playing.
+     */
+    public void playTaskbarBackgroundAlphaAnimation() {
+        mControllers.taskbarStashController.playTaskbarBackgroundAlphaAnimation();
+    }
+
+    /**
      * Called to start the taskbar translation spring to its settled translation (0).
      */
     public void startTranslationSpring() {
