cf88c1941289439ec21bf3bf28737303fbbbaa5c
hotmule <ithr997@gmail.com>
2024-07-06 22:13:06 +0300

    Icon updated

--- lawnchair/src/app/lawnchair/qsb/providers/Yandex.kt
+++ lawnchair/src/app/lawnchair/qsb/providers/Yandex.kt
@@ -1,14 +1,12 @@
 package app.lawnchair.qsb.providers
 
 import android.content.Intent
-import app.lawnchair.qsb.ThemingMethod
 import com.android.launcher3.R
 
 data object Yandex : QsbSearchProvider(
     id = "yandex",
     name = R.string.search_provider_yandex,
     icon = R.drawable.ic_yandex,
-    themingMethod = ThemingMethod.THEME_BY_LAYER_ID,
     packageName = "com.yandex.searchapp",
     className = "ru.yandex.searchplugin.MainActivity",
     supportVoiceIntent = true,
