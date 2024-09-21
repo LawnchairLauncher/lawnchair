85a9ec8708767efd4839c744d63138764e6c788a
Pun Butrach <pun.butrach@gmail.com>
2024-09-22 01:50:02 +0700

    fix build

--- lawnchair/src/app/lawnchair/smartspace/model/SmartspaceScores.kt
+++ lawnchair/src/app/lawnchair/smartspace/model/SmartspaceScores.kt
@@ -5,6 +5,7 @@ object SmartspaceScores {
     const val SCORE_PERSONALITY = 1f
     const val SCORE_BATTERY = 2f
     const val SCORE_MEDIA = 3f
+    const val SCORE_CALENDAR = 4f
 
     // Critical Information that must be displayed before anything else.
     const val SCORE_LOW_BATTERY = 10f
