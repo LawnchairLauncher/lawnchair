a8e1ce83ff32b682f07a82c3ae23fd438b420cf1
Rafael <rafaelmoura135790@gmail.com>
2024-12-05 01:03:13 -0300

    fix NoClassDefFound attempting to create new folder in lower apis

--- quickstep/src/com/android/launcher3/model/AppEventProducer.java
+++ quickstep/src/com/android/launcher3/model/AppEventProducer.java
@@ -268,9 +268,13 @@ public class AppEventProducer implements StatsLogConsumer {
 
 
     private AppTarget createTempFolderTarget() {
-        return new AppTarget.Builder(new AppTargetId("folder:" + SystemClock.uptimeMillis()),
+        try {
+            return new AppTarget.Builder(new AppTargetId("folder:" + SystemClock.uptimeMillis()),
                 mContext.getPackageName(), Process.myUserHandle())
                 .build();
+        } catch (NoClassDefFoundError e) {
+            return null;
+        }
     }
 
     private String getContainer(LauncherAtom.ItemInfo info) {
