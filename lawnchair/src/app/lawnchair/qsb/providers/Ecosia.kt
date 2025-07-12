116c28e23571063a8ced6193e3a3026ba81858d9
Zongle Wang <wangzongler@gmail.com>
2025-07-12 20:11:37 +0800

    Update lawnchair/src/app/lawnchair/qsb/providers/Ecosia.kt

--- lawnchair/src/app/lawnchair/qsb/providers/Ecosia.kt
+++ lawnchair/src/app/lawnchair/qsb/providers/Ecosia.kt
@@ -8,6 +8,5 @@ data object Ecosia : QsbSearchProvider(
     icon = R.drawable.ic_ecosia,
     packageName = "com.ecosia.android",
     website = "https://www.ecosia.org/",
-
     type = QsbSearchProviderType.APP_AND_WEBSITE
 )
