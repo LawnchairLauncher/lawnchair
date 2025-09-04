4ffe4f0fbb2b023fa384426f177cce405d2fe3ad
ELY M. <elymbmx@gmail.com>
2025-09-04 09:35:50 -0500

    scrolling

--- src/com/android/launcher3/PagedView.java
+++ src/com/android/launcher3/PagedView.java
@@ -1427,13 +1427,31 @@ public abstract class PagedView<T extends View & PageIndicator> extends ViewGrou
                                 ? mCurrentPage : mCurrentPage - getPanelCount();
                         runOnPageScrollsInitialized(
                                 () -> snapToPageWithVelocity(finalPage, velocity));
-                    } else if (((isSignificantMove && isDeltaLeft && !isFling) ||
-                            (isFling && isVelocityLeft)) &&
-                            mCurrentPage < getChildCount() - 1) {
-                        finalPage = returnToOriginalPage
-                                ? mCurrentPage : mCurrentPage + getPanelCount();
-                        runOnPageScrollsInitialized(
-                                () -> snapToPageWithVelocity(finalPage, velocity));
+					//elys mod//			
+                    } else if (((isSignificantMove && isDeltaLeft && !isFling) || (isFling && isVelocityLeft)) && mCurrentPage < getChildCount() - 1) {
+												
+                        finalPage = returnToOriginalPage ? mCurrentPage : mCurrentPage + getPanelCount();
+                        runOnPageScrollsInitialized(() -> snapToPageWithVelocity(finalPage, velocity));
+												
+						
+						Log.d("elys-log", "mCurrentPage: "+mCurrentPage);
+						Log.d("elys-log", "getPanelCount(): "+getPanelCount());	
+						Log.d("elys-log", "finalPage: "+finalPage);						
+						Log.d("elys-log", "isVelocityLeft: "+isVelocityLeft);	
+	
+						
+					} else if (mCurrentPage == getChildCount() - 1 && infiniteScroll) {
+                        finalPage = returnToOriginalPage ? mCurrentPage : 0;
+                        snapToPageWithVelocity(finalPage, velocity);
+						
+						
+                    } else if (mCurrentPage == 0 && infiniteScroll) {
+                        finalPage = returnToOriginalPage ? mCurrentPage : getChildCount() - 1;
+                        snapToPageWithVelocity(finalPage, velocity);						
+						
+						
+						
+						
                     } else {
                         runOnPageScrollsInitialized(this::snapToDestination);
                     }
