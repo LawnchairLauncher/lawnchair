d8ec692e89bf124d5d6e94b8f31c454b6abf7291
himanshu <garghimanshu0786@gmail.com>
2025-10-08 03:40:39 +0530

    Remove code comments from fixes
    - Clean up code by removing explanatory comments
    - Keep changes minimal and focused

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -378,9 +378,6 @@ public class DeviceProfile {
         mInfo = info;
         isTablet = info.isTablet(windowBounds);
         isPhone = !isTablet;
-        // Enable two-panel layout for multi-display devices (foldables)
-        // This ensures foldable devices like Pixel 9 Pro Fold use proper layout when unfolded
-        // even if they don't meet the traditional tablet width threshold (600dp)
         isTwoPanels = isMultiDisplay;
         boolean isTaskBarEnabled = PreferenceExtensionsKt.firstBlocking(preferenceManager2.getEnableTaskbarOnPhone());
         isTaskbarPresent = isTaskBarEnabled && (isTablet || (enableTinyTaskbar() && isGestureMode))
@@ -1413,8 +1410,6 @@ public class DeviceProfile {
      * Updates the iconSize for allApps* variants.
      */
     private void updateAllAppsIconSize(float scale, Resources res) {
-        // For scalable grids, don't apply home screen scale to app drawer border spacing
-        // This prevents app drawer layout from being affected by home screen icon size changes
         float borderScale = mIsScalableGrid ? 1.0f : scale;
         allAppsBorderSpacePx = new Point(
                 pxFromDp(inv.allAppsBorderSpaces[mTypeIndex].x, mMetrics, borderScale),
@@ -1426,13 +1421,10 @@ public class DeviceProfile {
         // but width is just the cell,
         // the border is added in #updateAllAppsContainerWidth
         if (mIsScalableGrid) {
-            // For scalable grids, don't apply the home screen scale to app drawer icons
-            // This prevents app drawer from being affected by home screen icon size changes
             allAppsIconSizePx = pxFromDp(inv.allAppsIconSize[mTypeIndex], mMetrics);
             allAppsIconTextSizePx = pxFromSp(inv.allAppsIconTextSize[mTypeIndex], mMetrics);
             allAppsIconTextSizePx *= mTextFactors.getAllAppsIconTextSizeFactor();
             allAppsIconDrawablePaddingPx = getNormalizedIconDrawablePadding();
-            // Also don't scale the cell width to maintain proper app drawer layout
             allAppsCellWidthPx = pxFromDp(inv.allAppsCellSize[mTypeIndex].x, mMetrics);
 
             if (allAppsCellWidthPx < allAppsIconSizePx) {
