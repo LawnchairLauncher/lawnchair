919a23e833589a9dd93cb846e6e1ba24e37bb659
himanshu <garghimanshu0786@gmail.com>
2025-10-08 03:41:17 +0530

    Remove code comments from foldable fix
    - Clean up code by removing explanatory comments
    - Keep changes minimal and focused

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -378,9 +378,6 @@ public class DeviceProfile {
         mInfo = info;
         isTablet = info.isTablet(windowBounds);
         isPhone = !isTablet;
-        // Enable two-panel layout for multi-display devices (foldables)
-        // This ensures foldable devices like Pixel 9 Pro Fold use proper layout when unfolded
-        // even if they don't meet the traditional tablet width threshold (600dp)
         isTwoPanels = isMultiDisplay;
         boolean isTaskBarEnabled = PreferenceExtensionsKt.firstBlocking(preferenceManager2.getEnableTaskbarOnPhone());
         isTaskbarPresent = isTaskBarEnabled && (isTablet || (enableTinyTaskbar() && isGestureMode))
