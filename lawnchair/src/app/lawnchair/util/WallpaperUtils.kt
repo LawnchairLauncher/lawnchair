f3fcb862c9ac3916affe0667822d5b9b14fb799b
VBansal99 <varunbansaldcsamca@gmail.com>
2025-10-11 21:32:51 +0530

    Fix: Implement WallpaperManagerCompat instead of Wallpaper Manager

--- lawnchair/src/app/lawnchair/util/WallpaperUtils.kt
+++ lawnchair/src/app/lawnchair/util/WallpaperUtils.kt
@@ -1,22 +1,17 @@
 package app.lawnchair.util
 
-import android.app.WallpaperColors
-import android.app.WallpaperManager
 import android.content.Context
 import android.graphics.Color
-import android.os.Build
+import app.lawnchair.wallpaper.WallpaperColorsCompat
+import app.lawnchair.wallpaper.WallpaperManagerCompat
 
 fun isWallpaperDark(context: Context): Boolean {
-    val wallpaperManager = WallpaperManager.getInstance(context)
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        val colors: WallpaperColors? =
-            wallpaperManager.getWallpaperColors(WallpaperManager.FLAG_SYSTEM)
-        colors?.primaryColor?.toArgb()?.let { argb ->
-            val darkness =
-                1 - (0.299 * Color.red(argb) + 0.587 * Color.green(argb) + 0.114 * Color.blue(argb)) / 255
-            darkness >= 0.5
-        } ?: false
-    } else {
-        false
-    }
+    val wallpaperManager = WallpaperManagerCompat.INSTANCE.get(context)
+    val colors: WallpaperColorsCompat? = wallpaperManager.wallpaperColors
+    return colors?.primaryColor?.let { argb ->
+        val darkness =
+            1 - (0.299 * Color.red(argb) + 0.587 * Color.green(argb) + 0.114 * Color.blue(argb)) / 255
+        darkness >= 0.5
+    } ?: false
 }
+
