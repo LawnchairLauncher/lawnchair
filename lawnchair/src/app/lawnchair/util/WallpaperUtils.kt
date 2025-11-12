da5c3eb0a99ce9a26c95ad48db25088e499fd8ec
VBansal99 <varunbansaldcsamca@gmail.com>
2025-11-13 00:16:09 +0530

    [BUG] Fix status bar and At a Glance text unreadable in light/dark mode (#6073)

--- lawnchair/src/app/lawnchair/util/WallpaperUtils.kt
+++ lawnchair/src/app/lawnchair/util/WallpaperUtils.kt
@@ -2,15 +2,15 @@ package app.lawnchair.util
 
 import android.content.Context
 import android.graphics.Color
-import app.lawnchair.wallpaper.WallpaperColorsCompat
 import app.lawnchair.wallpaper.WallpaperManagerCompat
 
 fun isWallpaperDark(context: Context): Boolean {
     val wallpaperManager = WallpaperManagerCompat.INSTANCE.get(context)
-    val colors: WallpaperColorsCompat? = wallpaperManager.wallpaperColors
-    return colors?.primaryColor?.let { argb ->
-        val darkness =
-            1 - (0.299 * Color.red(argb) + 0.587 * Color.green(argb) + 0.114 * Color.blue(argb)) / 255
-        darkness >= 0.5
-    } ?: false
+    val colors = wallpaperManager.wallpaperColors
+    val primaryColor = colors?.primaryColor ?: return false
+    val brightness = getBrightness(primaryColor)
+    return brightness < 0.5
+}
+private fun getBrightness(color: Int): Double {
+    return (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255.0
 }
