d97dea85d1a543ee4dcde3dc8a59090fafa18b7c
ELY M. <elymbmx@gmail.com>
2025-12-02 09:50:23 -0600

    scrolling preferences moved

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/HomeScreenPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/HomeScreenPreferences.kt
@@ -89,6 +89,11 @@ fun HomeScreenPreferences(
                 adapter = prefs2.doubleTapGestureHandler.getAdapter(),
                 label = stringResource(id = R.string.gesture_double_tap),
             )
+            SwitchPreference(
+                prefs.infiniteScrolling.getAdapter(),
+                label = stringResource(id = R.string.infinite_scrolling_label),
+                description = stringResource(id = R.string.infinite_scrolling_description),
+            )
         }
         PreferenceGroup(heading = stringResource(id = R.string.minus_one)) {
             val feedAvailable = OverlayCallbackImpl.minusOneAvailable(LocalContext.current)
@@ -125,12 +130,6 @@ fun HomeScreenPreferences(
             SwitchPreference(
                 adapter = prefs2.showTopShadow.getAdapter(),
                 label = stringResource(id = R.string.show_sys_ui_scrim),
-            )
-        }
-        PreferenceGroup(heading = stringResource(id = R.string.infinite_scrolling)) {
-            SwitchPreference(
-                prefs.infiniteScrolling.getAdapter(),
-                label = stringResource(id = R.string.infinite_scrolling_label),
             )
         }
         PreferenceGroup(heading = stringResource(id = R.string.layout)) {
