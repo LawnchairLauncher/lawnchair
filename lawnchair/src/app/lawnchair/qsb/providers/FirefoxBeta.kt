45ce92af5f036b228c40732f67ca880188d44993
Dharshika <dharshikaselvaraj2006@gmail.com>
2026-06-16 09:56:38 +0530

    Add Firefox Beta provider

---
+++ lawnchair/src/app/lawnchair/qsb/providers/FirefoxBeta.kt
@@ -0,0 +1,24 @@
+package app.lawnchair.qsb.providers
+
+import android.content.Intent
+import app.lawnchair.qsb.ThemingMethod
+import com.android.launcher3.R
+
+data object FirefoxBeta : QsbSearchProvider(
+    id = "FirefoxBeta",
+    name = R.string.search_provider_firefox_beta,
+    icon = R.drawable.ic_firefox_beta,
+    themedIcon = R.drawable.ic_firefox_beta_tinted,
+    themingMethod = ThemingMethod.TINT,
+    packageName = "org.mozilla.firefox_beta",
+    action = "org.mozilla.fenix.OPEN_TAB",
+    className = "org.mozilla.fenix.IntentReceiverActivity",
+    website = "https://play.google.com/store/apps/details?id=org.mozilla.firefox_beta",
+    type = QsbSearchProviderType.APP,
+    supportVoiceIntent = false,
+) {
+
+    override fun handleCreateVoiceIntent(): Intent = Intent(action)
+        .addFlags(INTENT_FLAGS)
+        .setClassName(packageName, "org.chromium.chrome.browser.VoiceSearchActivity")
+}
