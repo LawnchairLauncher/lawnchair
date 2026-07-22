bfca14f6851c4fde7cd07e97b588ce301ad781be
Pun Butrach <pun.butrach@gmail.com>
2026-07-22 23:06:38 +0700

    feat: Allow live information to be configured on/off and custom endpoint (#7026)
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/ui/preferences/destinations/GeneralPreferences.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/destinations/GeneralPreferences.kt
@@ -48,6 +48,7 @@ import app.lawnchair.ui.preferences.components.layout.PreferenceGroup
 import app.lawnchair.ui.preferences.components.layout.PreferenceLayout
 import app.lawnchair.ui.preferences.components.notificationDotsEnabled
 import app.lawnchair.ui.preferences.components.notificationServiceEnabled
+import app.lawnchair.ui.preferences.data.liveinfo.liveInformationManager
 import app.lawnchair.ui.preferences.navigation.GeneralIconPack
 import app.lawnchair.ui.preferences.navigation.GeneralIconShape
 import com.android.launcher3.BuildConfig
@@ -59,6 +60,7 @@ fun GeneralPreferences(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val prefs = preferenceManager()
     val prefs2 = preferenceManager2()
+    val liveInfoManager = liveInformationManager()
     val iconPacks by LocalPreferenceInteractor.current.iconPacks.collectAsStateWithLifecycle()
     val themedIconsAdapter = prefs.themedIcons.getAdapter()
     val drawerThemedIconsAdapter = prefs.drawerThemedIcons.getAdapter()
@@ -97,14 +99,19 @@ fun GeneralPreferences(modifier: Modifier = Modifier) {
                 description = stringResource(id = R.string.home_screen_rotation_description),
             )
         }
-        if (BuildConfig.APPLICATION_ID.contains("nightly")) {
-            PreferenceGroup(heading = stringResource(id = R.string.updater)) {
+        PreferenceGroup(heading = stringResource(id = R.string.updater)) {
+            if (BuildConfig.APPLICATION_ID.contains("nightly")) {
                 SwitchPreference(
                     adapter = prefs2.autoUpdaterNightly.getAdapter(),
                     label = stringResource(id = R.string.auto_updater_label),
                     description = stringResource(id = R.string.auto_updater_description),
                 )
             }
+            SwitchPreference(
+                adapter = liveInfoManager.enabled.getAdapter(),
+                label = stringResource(id = R.string.live_information_label),
+                description = stringResource(id = R.string.live_information_description),
+            )
         }
         ExpandAndShrink(visible = prefs2.enableFontSelection.asState().value) {
             PreferenceGroup(heading = stringResource(id = R.string.font_label)) {
