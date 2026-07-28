9c5f24520a02c486b7bf6c496999a1a83f0a3718
Pun Butrach <pun.butrach@gmail.com>
2026-07-28 21:41:54 +0700

    chore: Cleanup import
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/gestures/GestureController.kt
+++ lawnchair/src/app/lawnchair/gestures/GestureController.kt
@@ -25,7 +25,6 @@ import app.lawnchair.gestures.handlers.NoOpGestureHandler
 import app.lawnchair.preferences2.PreferenceManager2
 import com.android.launcher3.util.VibratorWrapper
 import com.patrykmichalik.opto.domain.Preference
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.distinctUntilChanged
