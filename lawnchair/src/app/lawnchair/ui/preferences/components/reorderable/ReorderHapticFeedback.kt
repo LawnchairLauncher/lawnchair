6b22cdb39a37529788afcf1f098521e9ac6c8907
Pun Butrach <pun.butrach@gmail.com>
2026-08-08 20:50:26 +0700

    feat: Support for below U for reorderable haptic
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/ui/preferences/components/reorderable/ReorderHapticFeedback.kt
+++ lawnchair/src/app/lawnchair/ui/preferences/components/reorderable/ReorderHapticFeedback.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import com.android.launcher3.Utilities
 import com.android.launcher3.util.MSDLPlayerWrapper
 import com.google.android.msdl.data.model.MSDLToken
 
@@ -28,16 +27,14 @@ fun rememberReorderHapticFeedback(): ReorderHapticFeedback {
     val reorderHapticFeedback = remember {
         object : ReorderHapticFeedback {
             override fun performHapticFeedback(type: ReorderHapticFeedbackType) {
-                if (Utilities.ATLEAST_U) {
-                    mMSDLPlayerWrapper.playToken(
-                        when (type) {
-                            ReorderHapticFeedbackType.START -> MSDLToken.START
-                            ReorderHapticFeedbackType.MOVE -> MSDLToken.DRAG_INDICATOR_DISCRETE
-                            ReorderHapticFeedbackType.END -> MSDLToken.STOP
-                            ReorderHapticFeedbackType.CANCEL -> MSDLToken.CANCEL
-                        },
-                    )
-                }
+                mMSDLPlayerWrapper.playToken(
+                    when (type) {
+                        ReorderHapticFeedbackType.START -> MSDLToken.START
+                        ReorderHapticFeedbackType.MOVE -> MSDLToken.DRAG_INDICATOR_DISCRETE
+                        ReorderHapticFeedbackType.END -> MSDLToken.STOP
+                        ReorderHapticFeedbackType.CANCEL -> MSDLToken.CANCEL
+                    },
+                )
             }
         }
     }
