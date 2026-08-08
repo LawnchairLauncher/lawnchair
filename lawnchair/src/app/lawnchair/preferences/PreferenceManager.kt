3ef5a39b773a692cd38f55b48c222315fd96b642
Pun Butrach <pun.butrach@gmail.com>
2026-08-08 18:25:40 +0700

    chore: Remove changes from local
    Signed-off-by: Pun Butrach <pun.butrach@gmail.com>

--- lawnchair/src/app/lawnchair/preferences/PreferenceManager.kt
+++ lawnchair/src/app/lawnchair/preferences/PreferenceManager.kt
@@ -18,7 +18,6 @@ package app.lawnchair.preferences
 
 import android.content.Context
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.ui.platform.LocalContext
 import app.lawnchair.LawnchairLauncher
 import app.lawnchair.font.FontCache
@@ -41,7 +40,6 @@ import com.android.launcher3.util.SafeCloseable
 import com.android.quickstep.RecentsModel
 import javax.inject.Inject
 
-@Stable
 @LauncherAppSingleton
 class PreferenceManager @Inject constructor(
     @ApplicationContext private val context: Context,
