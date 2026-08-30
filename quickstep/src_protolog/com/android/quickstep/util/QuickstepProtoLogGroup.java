5f65f736dc6ded0c9a121ecc0dccdefe94b01bce
Pun Butrach <pun.butrach@gmail.com>
2026-08-30 21:39:51 +0700

    feat: Add Quickstep Protolog for Baklava (#7217)

--- quickstep/src_protolog/com/android/quickstep/util/QuickstepProtoLogGroup.java
+++ quickstep/src_protolog/com/android/quickstep/util/QuickstepProtoLogGroup.java
@@ -51,6 +51,7 @@ public enum QuickstepProtoLogGroup implements IProtoLogGroup {
         return Variables.sIsInitialized;
     }
 
+    @RequiresApi(36)
     public static void initProtoLog() {
         if (Variables.sIsInitialized) {
             Log.e(Constants.TAG,
