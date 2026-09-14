6e0ce67dd187acfb7a2fc8fcce660980c23be078
wized2 <code@canvas.app>
2026-09-14 08:43:44 +0000

    style: single blank line before MojeekWebSearchProvider (spotless)

--- lawnchair/src/app/lawnchair/search/algorithms/engine/provider/web/BuiltInWebSearchProviders.kt
+++ lawnchair/src/app/lawnchair/search/algorithms/engine/provider/web/BuiltInWebSearchProviders.kt
@@ -421,7 +421,6 @@ object KagiWebSearchProvider : WebSearchProvider {
     override fun toString(): String = id
 }
 
-
 object MojeekWebSearchProvider : WebSearchProvider {
     override val label = R.string.search_provider_mojeek
     override val iconRes = R.drawable.ic_qsb_search
